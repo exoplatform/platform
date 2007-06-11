92258719bf495d595066cc4aa7a51415cbaa4974
Le Thu Thuy <thuy.le@exoplatform.com>
2007-06-11 03:02:19 +0000

    //

--- webui/portal/src/main/java/org/exoplatform/portal/application/PortalRequestHandler.java
+++ webui/portal/src/main/java/org/exoplatform/portal/application/PortalRequestHandler.java
@@ -4,18 +4,20 @@
  **************************************************************************/
 package org.exoplatform.portal.application;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.exoplatform.container.PortalContainer;
-import org.exoplatform.services.log.LogService;
 import org.exoplatform.web.WebAppController;
 import org.exoplatform.web.WebRequestHandler;
 import org.exoplatform.web.application.ApplicationLifecycle;
 import org.exoplatform.webui.application.WebuiRequestContext;
 import org.exoplatform.webui.component.UIApplication;
+import org.exoplatform.webui.component.UIComponent;
+import org.exoplatform.webui.event.Event.Phase;
 
 /**
  * Created by The eXo Platform SARL
@@ -29,16 +31,54 @@ public class PortalRequestHandler extends WebRequestHandler {
 
   public String[] getPath() { return PATHS ; }
 
+  @SuppressWarnings("unchecked")
   public void execute(WebAppController controller,  HttpServletRequest req, HttpServletResponse res) throws Exception {
     res.setHeader("Cache-Control", "no-cache");
+    
     PortalApplication app =  controller.getApplication(PortalApplication.PORTAL_APPLICATION_ID) ;
     WebuiRequestContext context = new  PortalRequestContext(app, req, res) ;  ;
     WebuiRequestContext.setCurrentInstance(context) ;
+    Map<String, String> parameters = new HashMap<String, String>();
+    
     List<ApplicationLifecycle> lifecycles = app.getApplicationLifecycle();
     try {
-      for(ApplicationLifecycle lifecycle :  lifecycles) lifecycle.onStartRequest(app, context) ;
+      for(ApplicationLifecycle lifecycle :  lifecycles) {
+        lifecycle.onStartRequest(app, context) ;
+      }
+      String url = req.getRequestURI().toString();
+     
       UIApplication uiApp = app.getStateManager().restoreUIRootComponent(context) ;
-      if(context.getUIApplication() != uiApp) context.setUIApplication(uiApp) ;
+      String requetsParams  = "";
+      if(url.split("/").length > 4) {requetsParams =  url.split("/")[4]; }
+      
+      if(requetsParams != null && requetsParams.length() > 0) { 
+        String[] params = requetsParams.split("&");
+        for(String param: params){
+          String[] temp = param.split("=");
+          if(temp.length < 1) continue;
+          System.out.println(temp[0] + "-" + temp[1]);
+          parameters.put(temp[0], temp[1]);
+        }
+      }
+      if(parameters.containsKey("portal:componentId") && parameters.containsKey("portal:action")){
+        UIComponent uicomponent = uiApp.findComponentById(parameters.get("portal:componentId"));
+        if(uicomponent != null) {
+          Phase phase = Phase.PROCESS;
+          if(parameters.containsKey("portal:phase")){
+            String phaseStr = parameters.get("portal:phase").toLowerCase();
+            if (phaseStr.equals("decode")) phase = Phase.DECODE;
+            else if(phaseStr.equals("render")) phase = Phase.RENDER;
+            else if(phaseStr.equals("init")) phase = Phase.INIT;
+            else if(phaseStr.equals("destroy")) phase = Phase.DESTROY;
+            else if(phaseStr.equals("restore")) phase = Phase.RESTORE;
+          }
+          System.out.println("\n\n\n-------------------Action: " + parameters.get("portal:action"));
+          uicomponent.createEvent(parameters.get("portal:action"), phase, context );
+//          uicomponent.processAction(context);
+        }
+      }
+      
+      if(context.getUIApplication() != uiApp){ context.setUIApplication(uiApp); }
       
       app.processDecode(uiApp, context) ;
       
