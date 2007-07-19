8ec99b6825b1c077740a4901d5c6afb8ad96fe7b
Le Thu Thuy <thuy.le@exoplatform.com>
2007-07-19 08:28:57 +0000

    //

---
+++ webui/eXo/src/main/java/org/exoplatform/web/command/handler/WidgetHandler.java
@@ -0,0 +1,30 @@
+/***************************************************************************
+ * Copyright 2001-2006 The eXo Platform SARL         All rights reserved.  *
+ * Please look at license.txt in info directory for more license detail.   *
+ **************************************************************************/
+package org.exoplatform.web.command.handler;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.exoplatform.web.WebAppController;
+import org.exoplatform.web.command.Command;
+
+/**
+ * Created by The eXo Platform SARL
+ * Author : Le Bien Thuy
+ *          thuy.le@exoplatform.com
+ * Dec 9, 2006  
+ */
+public class WidgetHandler extends Command {
+  
+  private String componentId ;
+  
+
+  @SuppressWarnings("unused")
+  public void execute(WebAppController controller,  HttpServletRequest req, HttpServletResponse res) throws Exception {
+    System.out.println("\n\n\n----------------------------->WidgetHandler");
+  }
+  
+}
