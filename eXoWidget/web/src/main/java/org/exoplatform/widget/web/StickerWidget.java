86844f5a7726661fde1399e4a514d6b81026a413
Le Thu Thuy <thuy.le@exoplatform.com>
2007-08-07 07:52:40 +0000

    //

--- eXoWidget/web/src/main/java/org/exoplatform/widget/web/StickerWidget.java
+++ eXoWidget/web/src/main/java/org/exoplatform/widget/web/StickerWidget.java
@@ -6,9 +6,11 @@ package org.exoplatform.widget.web;
 
 import java.io.Writer;
 
+import org.exoplatform.container.PortalContainer;
 import org.exoplatform.portal.application.PortalRequestContext;
 import org.exoplatform.portal.webui.application.UIWidget;
 import org.exoplatform.portal.webui.util.Util;
+import org.exoplatform.portal.widget.jcr.UserWidgetDataService;
 import org.exoplatform.web.application.mvc.MVCRequestContext;
 import org.exoplatform.web.application.widget.WidgetApplication;
 //import org.exoplatform.webui.config.annotation.ComponentConfig;
@@ -47,18 +49,30 @@ public class StickerWidget extends WidgetApplication<UIWidget> {
   public String getApplicationGroup() { return "eXoWidgetWeb"; }
   
   public void processRender(UIWidget uiWidget, Writer w) throws Exception {
+//    try{ int k = 3/0;
+//    }catch (Exception e) { e.printStackTrace();}
+//    
+    PortalContainer container  = PortalContainer.getInstance();
+    UserWidgetDataService service = 
+      (UserWidgetDataService)container.getComponentInstanceOfType(UserWidgetDataService.class) ;    
+
     PortalRequestContext pContext = Util.getPortalRequestContext();
     MVCRequestContext appReqContext = new MVCRequestContext(this, pContext) ;
     String instanceId = uiWidget.getApplicationInstanceId() ;
-//    System.out.println("\n\n\n-------------Id = " + instanceId);
+//    System.out.println("\n\n\n-------------Idne = " + uiWidget.getApplicationInstanceUniqueId());
     int posX = uiWidget.getProperties().getIntValue("locationX") ;
     int posY = uiWidget.getProperties().getIntValue("locationY") ;
     int zIndex = uiWidget.getProperties().getIntValue("zIndex") ;
+    String content = service.getWidgetData(pContext.getRemoteUser(), getApplicationName(), uiWidget.getApplicationInstanceUniqueId());
+    if(content == null ) content = "";
     
     w.append("<div id = 'UIStickerWidget' applicationId = '").
-      append(instanceId).append("' posX = '").append(String.valueOf(posX)). 
+      append(instanceId).
+      append("' posX = '").append(String.valueOf(posX)). 
       append("' posY = '").append(String.valueOf(posY)).
-      append("' zIndex = '").append(String.valueOf(zIndex)).append("'><span></span></div>") ;
+      append("' zIndex = '").append(String.valueOf(zIndex)).
+      append("' content = '").append(content).
+      append("'><span></span></div>") ;
     String script = 
       "eXo.portal.UIPortal.createJSApplication('eXo.widget.web.sticker.UIStickerWidget','UIStickerWidget','"+instanceId+"','/eXoWidgetWeb/javascript/');";
     appReqContext.getJavascriptManager().addCustomizedOnLoadScript(script) ;
--- eXoWidget/web/src/main/webapp/javascript/eXo/widget/web/sticker/UIStickerWidget.js
+++ eXoWidget/web/src/main/webapp/javascript/eXo/widget/web/sticker/UIStickerWidget.js
@@ -7,34 +7,37 @@ function UIStickerWidget() {
 	this.init("UIStickerWidget", "sticker");
 }
 
-UIStickerWidget.prototype.createAppDescriptor = function(appDescriptor, appElement) {
-	appDescriptor.widget = {
-		positionX : appElement.getAttribute('posX'),
-		positionY : appElement.getAttribute('posY'),
-		zIndex : appElement.getAttribute('zIndex'),
-		
-		uiWidget : {
-			temporaty : appElement,
-			appId : appElement.getAttribute('applicationId'),
-			stickerContent : (appElement.getAttribute('stickerContent'))?appElement.getAttribute('stickerContent'):""
-		}
-	};
-}
 
 UIStickerWidget.prototype.sendContent = function(object) {	
 	if (object.value == "")	return ;
 	var DOMUtil = eXo.core.DOMUtil ;
 	var uiWidgetContainer = DOMUtil.findAncestorByClass(object, "WidgetApplication") ;
+	var uiWidget = DOMUtil.findAncestorByClass(object, "UIWidget" );
 	containerBlockId = uiWidgetContainer.id;
 	var parent = uiWidgetContainer.parentNode ;
 	var params = [
-  	{name: "objectId", value : object.id} ,
-  	{name: "stickerContent", value : object.value}
+  	{name: "objectId", value : uiWidget.id} ,
+  	{name: "content", value : object.value}
   ] ;
+  alert("---------1-------" + uiWidget.id);
   var url = eXo.env.server.context + "/command?" ;
-  url += "type=org.exoplatform.web.command.handler.WidgetHandler&action=saveContent&componentId="+parent.id+"&content="+object.value ;
+  url += "type=org.exoplatform.web.command.handler.StickerWidgetHandler&action=saveContent&objectId="+uiWidget.id+"&content="+object.value ;
   ajaxAsyncGetRequest(url, false);
 } ;
+
+UIStickerWidget.prototype.createAppDescriptor = function(appDescriptor, appElement) {
+    appDescriptor.widget = {
+        positionX : appElement.getAttribute('posX'),
+        positionY : appElement.getAttribute('posY'),
+        zIndex : appElement.getAttribute('zIndex'),
+        
+        uiWidget : {
+            temporaty : appElement,
+            appId : appElement.getAttribute('applicationId'),
+            content : appElement.getAttribute('content')
+        }
+    };
+}
 
 if(eXo.widget.web == null) eXo.widget.web = {} ;
 if(eXo.widget.web.sticker == null) eXo.widget.web.sticker = {};
