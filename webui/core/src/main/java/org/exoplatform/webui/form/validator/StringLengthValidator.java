cdee0049c96119c34c622b9a08f7eb7b3381f75d
Le Thu Thuy <thuy.le@exoplatform.com>
2007-08-07 09:51:46 +0000

    //

---
+++ webui/core/src/main/java/org/exoplatform/webui/form/validator/StringLengthValidator.java
@@ -0,0 +1,45 @@
+/***************************************************************************
+ * Copyright 2001-2003 The eXo Platform SARL         All rights reserved.  *
+ * Please look at license.txt in info directory for more license detail.   *
+ **************************************************************************/
+package org.exoplatform.webui.form.validator;
+
+import javax.faces.validator.LengthValidator;
+
+import org.codehaus.groovy.runtime.NewInstanceMetaMethod;
+import org.exoplatform.web.application.ApplicationMessage;
+import org.exoplatform.webui.exception.MessageException;
+import org.exoplatform.webui.form.UIFormInput;
+
+/**
+ * Created by The eXo Platform SARL
+ * Author : Dang Van Minh
+ *          minhdv81@yahoo.com
+ * Jun 7, 2006
+ */
+public class StringLengthValidator implements Validator {
+  private Integer min_ = 0;
+  private Integer max_ = 0;
+  
+  public StringLengthValidator(Integer max) {
+    max_ = max;
+  }
+  public StringLengthValidator(Integer min, Integer max){
+   min_ = min;
+   max_ = max;
+  }
+  
+  public void validate(UIFormInput uiInput) throws Exception {
+    if((uiInput.getValue() != null) ){
+      int length = ((String)uiInput.getValue()).trim().length();
+      if(min_ <= length && max_ >= length) return ;
+    }    
+    String label = uiInput.getLabel();
+    if(label == null) label = uiInput.getName();
+    label = label.trim();
+    if(label.charAt(label.length() - 1) == ':') label = label.substring(0, label.length() - 1);
+    Object[]  args = {label,min_.toString(), max_.toString()} ;
+    throw new MessageException(new ApplicationMessage("StringLengthValidator.msg.length-invalid", args, 
+                                                      ApplicationMessage.WARNING)) ;
+  }
+}
