7512fb56e0d572b8e88b5dd9f0bba823e6c09de6
John Doe <noreply@exoplatform.com>
2007-10-05 08:19:21 +0000

    SVM: skipping changes 20458-20459 for svn://svn.forge.objectweb.org/svnroot/exoplatform/projects

--- web/eXoResources/src/main/webapp/javascript/eXo/webui/UIDropDownControl.js
+++ web/eXoResources/src/main/webapp/javascript/eXo/webui/UIDropDownControl.js
@@ -1,7 +1,7 @@
 function UIDropDownControl() {} ;
 
 UIDropDownControl.prototype.init = function(id) {
-	alert("param : " + id) ;
+//	alert("param : " + id) ;
 };
 
 UIDropDownControl.prototype.selectItem = function(clickedElemt, method, param) {
@@ -18,6 +18,11 @@ UIDropDownControl.prototype.show = function(obj, evt) {
 	
 	var DOMUtil = eXo.core.DOMUtil ;
 	var itemContainer = DOMUtil.findNextElementByTagName(obj, 'div') ;
+	var editIcon = DOMUtil.findPreviousElementByTagName(itemContainer.parentNode, 'div');
+	var uiPopupControl = DOMUtil.findFirstDescendantByClass(editIcon, 'div', 'UIPopupControl') ;
+	if(uiPopupControl.style.display == "block") {
+		uiPopupControl.style.display = "none" ;
+	}	
 	if (itemContainer) {
 		if (itemContainer.style.display == "none") itemContainer.style.display = "block" ;
 		else itemContainer.style.display = "none" ;
@@ -36,6 +41,39 @@ UIDropDownControl.prototype.onclickEvt = function(obj) {
 	var uiDropDownMiddleTitle = DOMUtil.findFirstDescendantByClass(uiDropDownTitle,'div','UIDropDownMiddleTitle') ;
 	uiDropDownMiddleTitle.firstChild.innerHTML = obj.innerHTML ;
 	uiDropDownAnchor.style.display = 'none' ;
+} ;
+
+/* show PopupControl at right edit Icon
+ * tungnd
+ * */
+UIDropDownControl.prototype.showPopupControl = function(obj, evt) {
+	if(!evt) evt = window.event ;
+	evt.cancelBubble = true ;
+	var DOMUtil = eXo.core.DOMUtil ;
+	var uiPopupControl = DOMUtil.findFirstDescendantByClass(obj, 'div', 'UIPopupControl') ;
+	var tmpAnchor = DOMUtil.findNextElementByTagName(obj, 'div') ;
+	var uiDropDownAnchor = DOMUtil.findFirstDescendantByClass(tmpAnchor, 'div', 'UIDropDownAnchor') ;
+	if(uiDropDownAnchor.style.display == "block")
+		uiDropDownAnchor.style.display = "none" ;
+	if (!uiPopupControl) return ;	
+	if(uiPopupControl.style.display == "none") {
+		eXo.webui.UIDropDownControl.hide() ;
+		uiPopupControl.style.display = "block" ;
+		eXo.core.DOMUtil.listHideElements(uiPopupControl) ;	
+	}	
+	else uiPopupControl.style.display = "none" ;
+} ;
+
+/* hide element when onclick
+ * tungnd
+ * */
+UIDropDownControl.prototype.hide = function() {
+	var ln = eXo.core.DOMUtil.hideElementList.length ;
+	if (ln > 0) {
+		for (var i = 0 ; i < ln ; i++) {
+			eXo.core.DOMUtil.hideElementList[i].style.display = "none" ;
+		}
+	}
 } ;
 
 eXo.webui.UIDropDownControl = new UIDropDownControl() ;
