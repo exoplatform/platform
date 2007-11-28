f75c67ca32af4a8b64f575b86d2d4af0279a5729
John Doe <noreply@exoplatform.com>
2007-11-28 08:02:58 +0000

    //

--- web/portal/src/main/webapp/WEB-INF/conf/uiconf/portal/webui/portal/PortalTemplateConfigOption.groovy
+++ web/portal/src/main/webapp/WEB-INF/conf/uiconf/portal/webui/portal/PortalTemplateConfigOption.groovy
@@ -5,15 +5,16 @@ import org.exoplatform.webui.core.model.SelectItemCategory;
 
 List options = new ArrayList();
 
-  SelectItemCategory guest = new SelectItemCategory("SitePortal");
+  SelectItemCategory guest = new SelectItemCategory("ClassicPortal");
   guest.addSelectItemOption(
-      new PortalTemplateConfigOption("", "site", "Site Portal", "SitePortal").addGroup("/guest")
+      //new PortalTemplateConfigOption("", "site", "Site Portal", "SitePortal").addGroup("/guest")
+      new PortalTemplateConfigOption("", "classic", "Classic Portal", "ClassicPortal").addGroup("/platform/guests")
   );
   options.add(guest);
 
   SelectItemCategory webos = new SelectItemCategory("WebOSPortal");
   webos.addSelectItemOption(
-    new PortalTemplateConfigOption("", "webos", "WebOS Portal", "WebOSPortal").addGroup("/guest")
+    new PortalTemplateConfigOption("", "webos", "WebOS Portal", "WebOSPortal").addGroup("/platform/guests")
   );
   options.add(webos);
   
