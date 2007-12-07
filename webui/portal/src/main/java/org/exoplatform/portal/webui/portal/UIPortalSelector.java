42c1aee0b5b2a4ae907cf20c6a333900084b5ae3
Do Thanh Tung <tung.do@exoplatform.com>
2007-12-07 08:08:53 +0000

    Fix bug PORTAL-1215

--- webui/portal/src/main/java/org/exoplatform/portal/webui/portal/UIPortalSelector.java
+++ webui/portal/src/main/java/org/exoplatform/portal/webui/portal/UIPortalSelector.java
@@ -41,7 +41,6 @@ import org.exoplatform.webui.core.UIGrid;
 
 public class UIPortalSelector extends UIContainer {
   
-  //public static String[] BEAN_FEILD = {"creator", "name", "skin", "factoryId"} ;
   public static String[] BEAN_FEILD = {"creator", "name", "skin"} ;
   public static String[] SELECT_ACTIONS = {"SelectPortal"} ;
   
