88827cf63bd38370e95e3b32d7f5de5acc71fb0b
John Doe <noreply@exoplatform.com>
2007-12-10 03:11:00 +0000

    SVM: skipping changes 22597-22598 for svn://svn.forge.objectweb.org/svnroot/exoplatform/projects

--- portlet/web/src/main/java/org/exoplatform/portal/webui/component/UIHomePagePortlet.java
+++ portlet/web/src/main/java/org/exoplatform/portal/webui/component/UIHomePagePortlet.java
@@ -4,15 +4,13 @@
  **************************************************************************/
 package org.exoplatform.portal.webui.component;
 
-import org.exoplatform.portal.webui.portal.PageNodeEvent;
-import org.exoplatform.portal.webui.portal.UIPortal;
-import org.exoplatform.portal.webui.util.Util;
+import javax.portlet.PortletRequest;
+
+import org.exoplatform.webui.application.WebuiRequestContext;
+import org.exoplatform.webui.application.portlet.PortletRequestContext;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
-import org.exoplatform.webui.config.annotation.EventConfig;
 import org.exoplatform.webui.core.UIPortletApplication;
 import org.exoplatform.webui.core.lifecycle.UIApplicationLifecycle;
-import org.exoplatform.webui.event.Event;
-import org.exoplatform.webui.event.EventListener;
 
 /**
  * Created by The eXo Platform SARL
@@ -23,25 +21,23 @@ import org.exoplatform.webui.event.EventListener;
 
 @ComponentConfig (
     lifecycle = UIApplicationLifecycle.class, 
-    template = "app:/groovy/portal/webui/component/UIHomePagePortlet.gtmpl",
-    events = {
-        @EventConfig(listeners = UIHomePagePortlet.LinkActionListener.class)        
-      }
+    template = "app:/groovy/portal/webui/component/UIHomePagePortlet.gtmpl"    
 )
 
 public class UIHomePagePortlet extends UIPortletApplication {
 
-  public UIHomePagePortlet() throws Exception {
-
-  }  
+  private static String DEFAULT_TEMPLATE = "app:/groovy/portal/webui/component/UIHomePagePortlet.gtmpl" ;  
   
-  static public class LinkActionListener extends EventListener<UIHomePagePortlet> {
-    public void execute(Event<UIHomePagePortlet> event) throws Exception {      
-      UIPortal uiPortal = Util.getUIPortal();
-      String uri = "group::portal/admin::ecm";
-      PageNodeEvent<UIPortal> pnevent ;
-      pnevent = new PageNodeEvent<UIPortal>(uiPortal, PageNodeEvent.CHANGE_PAGE_NODE, null, uri) ;      
-      uiPortal.broadcast(pnevent, Event.Phase.PROCESS) ;
-    }
+  public UIHomePagePortlet () throws  Exception {
+  
+  } 
+  
+  @Override
+  public String getTemplate() {
+    PortletRequestContext context = (PortletRequestContext) WebuiRequestContext.getCurrentInstance() ;
+    PortletRequest prequest = context.getRequest() ;    
+    String template =  prequest.getPreferences().getValue("template", DEFAULT_TEMPLATE) ; 
+    
+    return template;
   }
 }
