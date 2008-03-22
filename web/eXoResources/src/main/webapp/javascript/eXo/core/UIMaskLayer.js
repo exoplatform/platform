11045601c17ae45c753f4adac2e970fdbf165365
Nguyen Hong Minh <minh.nguyen@exoplatform.com>
2008-03-22 05:30:41 +0000

    fix bug manager widget in

--- web/eXoResources/src/main/webapp/javascript/eXo/core/UIMaskLayer.js
+++ web/eXoResources/src/main/webapp/javascript/eXo/core/UIMaskLayer.js
@@ -122,28 +122,21 @@ UIMaskLayer.prototype.createMask = function(blockContainerId, object, opacity, p
  * Tung.Pham added
  */
 //TODO: Temporary use
-UIMaskLayer.prototype.createMaskForFrame = function(blockContainerId, object, opacity, position) {
+UIMaskLayer.prototype.createMaskForFrame = function(blockContainerId, object, opacity) {
 	try {
 		var Browser = eXo.core.Browser ;
 		if(typeof(blockContainerId) == "string") blockContainerId = document.getElementById(blockContainerId) ;
 		var blockContainer = blockContainerId ;
 		var maskLayer = document.createElement("div") ;
-		
-		this.object = object ;
-		this.blockContainer = blockContainer ;
-		this.position = position ;
-		
 		blockContainer.appendChild(maskLayer) ;
-		
 		maskLayer.className = "MaskLayer" ;
 		maskLayer.id = object.id + "MaskLayer" ;
 		maskLayer.maxZIndex = 3 ;
-		maskLayer.style.width = "100%" ;
-		maskLayer.style.height = "100%" ;
+		maskLayer.style.width = blockContainer.offsetWidth + "px"  ;
+		maskLayer.style.height =  blockContainer.offsetHeight + "px"  ;
 		maskLayer.style.top = "0px" ;
 		maskLayer.style.left = "0px" ;
 		maskLayer.style.zIndex = maskLayer.maxZIndex ;
-	
 		if(opacity) {
 	    Browser.setOpacity(maskLayer, opacity) ;
 		}
@@ -152,27 +145,17 @@ UIMaskLayer.prototype.createMaskForFrame = function(blockContainerId, object, op
 			if(object.nextSibling) {
 			  maskLayer.nextSiblingOfObject = object.nextSibling ;
 			  maskLayer.parentOfObject = null ;
-			} else {
-			  maskLayer.nextSiblingOfObject = null ;
-			  maskLayer.parentOfObject = object.parentNode ;
-			}
-			
-			object.style.zIndex = maskLayer.maxZIndex + 1 ;
-			object.style.display = "block" ;
-			
-			blockContainer.appendChild(object) ;
-		
-			eXo.core.UIMaskLayer.setPosition() ;
-			
-			if((blockContainer.offsetWidth > object.offsetLeft + object.offsetWidth) && (position == "TOP-RIGHT") || (position == "BOTTOM-RIGHT")) {
-		    object.style.left = blockContainer.offsetWidth - object.offsetWidth + "px" ;
-			}
+				} else {
+				  maskLayer.nextSiblingOfObject = null ;
+				  maskLayer.parentOfObject = object.parentNode ;
+				}
+				
+				object.style.zIndex = maskLayer.maxZIndex + 1 ;
+				object.style.display = "block" ;
+				
+				blockContainer.appendChild(object) ;
 	  }
-		
-
-		}catch(err) {
-			alert(err) ;
-	}
+		}catch(err) {}
 	return maskLayer ;
 } ;
 
