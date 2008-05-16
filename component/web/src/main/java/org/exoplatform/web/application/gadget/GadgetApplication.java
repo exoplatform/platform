91def925e589d9cc42b527b93b2a945a49b37931
Nguyen Dang Tung <tung.dang@exoplatform.com>
2008-05-16 08:54:18 +0000

    add gadget architech and shindig to porttal

---
+++ component/web/src/main/java/org/exoplatform/web/application/gadget/GadgetApplication.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2003-2007 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.web.application.gadget;
+
+import java.io.Writer;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.exoplatform.web.application.Application;
+/**
+ * Created by The eXo Platform SAS
+ * Author : dang.tung
+ *          tungcnw@gmail.com
+ * May 06, 2008   
+ */
+abstract public class GadgetApplication<T> extends Application {
+  
+  public String getApplicationType() { return "eXoGadget" ; }
+  
+  abstract public void processRender(T uiGadget, Writer w) throws Exception ;
+  
+  public ResourceBundle getOwnerResourceBundle(String username, Locale locale) throws Exception {
+    throw new Exception("This method is not supported") ;
+  }
+
+  public ResourceBundle getResourceBundle(Locale locale) throws Exception {
+    throw new Exception("This method is not supported") ;
+  }
+}
