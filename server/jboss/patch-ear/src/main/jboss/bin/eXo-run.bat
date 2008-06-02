fa19d93dc129d7c2ff9fceb5637c000f2e0c8944
Alexey Zavizionov <alexey.zavizionov@exoplatform.com>
2008-06-02 09:51:18 +0000

    TOOLS-11

---
+++ server/jboss/patch-ear/src/main/jboss/bin/eXo-run.bat
@@ -0,0 +1,9 @@
+@echo off
+rem -------------------------------------------------------------------------
+rem eXo Bootstrap Script for Win32
+rem -------------------------------------------------------------------------
+
+set JAVA_OPTS=%JAVA_OPTS% -Dorg.apache.catalina.STRICT_SERVLET_COMPLIANCE=false
+
+call run.bat
+
---
+++ server/jboss/patch-ear/src/main/jboss/bin/eXo-run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+### ====================================================================== ###
+##                                                                          ##
+##  eXo Platform Bootstrap Script for Jboss                                    
+##                                                                          ##
+### ====================================================================== ###
+
+JAVA_OPTS="$JAVA_OPTS -Dorg.apache.catalina.STRICT_SERVLET_COMPLIANCE=false"
+
+export JAVA_OPTS
+
+./run.sh
