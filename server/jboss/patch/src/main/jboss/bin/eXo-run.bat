4480c659598fe59fa308bf31793ecba5812769e9
Alexey Zavizionov <alexey.zavizionov@exoplatform.com>
2008-06-26 10:13:28 +0000

    PC-258

--- server/jboss/patch/src/main/jboss/bin/eXo-run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem -------------------------------------------------------------------------
-rem eXo Bootstrap Script for Win32
-rem -------------------------------------------------------------------------
-
-set JAVA_OPTS=%JAVA_OPTS% -Dorg.apache.catalina.STRICT_SERVLET_COMPLIANCE=false
-
-call run.bat
-
