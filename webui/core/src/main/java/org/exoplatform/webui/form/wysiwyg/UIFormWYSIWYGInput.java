2f306ac37bf58a050e027a3bc133d4037df5ba08
Pham Xuan Hoa <hoa.pham@exoplatform.com>
2008-07-30 03:16:20 +0000

    PORTAL-1683

--- webui/core/src/main/java/org/exoplatform/webui/form/wysiwyg/UIFormWYSIWYGInput.java
+++ webui/core/src/main/java/org/exoplatform/webui/form/wysiwyg/UIFormWYSIWYGInput.java
@@ -29,6 +29,9 @@ import org.exoplatform.webui.form.UIFormInputBase;
  *          hoa.pham@exoplatform.com
  * Jun 23, 2008  
  */
+/**
+ * The Class UIFormWYSIWYGInput.
+ */
 public class UIFormWYSIWYGInput extends UIFormInputBase<String> {
   private final String BASIC_TOOLBAR = "Basic".intern();
   private final String DEFAULT_TOOLBAR = "Default".intern();
@@ -36,36 +39,61 @@ public class UIFormWYSIWYGInput extends UIFormInputBase<String> {
   private FCKEditorConfig fckConfig;  
   private boolean useBasicToolbar = true;  
 
+  /**
+   * Instantiates a new uI form wysiwyg input.
+   * 
+   * @param name the name
+   * @param bindingField the binding field
+   * @param value the value
+   * @param isBasic the is basic
+   */
   public UIFormWYSIWYGInput(String name, String bindingField, String value, boolean isBasic) {
     super(name, bindingField, String.class);
     useBasicToolbar = isBasic;    
     this.value_ = value;
   }
 
+  /**
+   * Gets the fCK config.
+   * 
+   * @return the fCK config
+   */
   public FCKEditorConfig getFCKConfig() { return fckConfig; }
+  
+  /**
+   * Sets the fCK config.
+   * 
+   * @param config the new fCK config
+   */
   public void setFCKConfig(FCKEditorConfig config) {this.fckConfig = config; }
 
+  /* (non-Javadoc)
+   * @see org.exoplatform.webui.form.UIFormInputBase#decode(java.lang.Object, org.exoplatform.webui.application.WebuiRequestContext)
+   */
   public void decode(Object input, WebuiRequestContext context) throws Exception {
     value_ = (String) input;
     if(value_ != null && value_.length() == 0) value_ = null ;
   }
 
+  /* (non-Javadoc)
+   * @see org.exoplatform.webui.core.UIComponent#processRender(org.exoplatform.webui.application.WebuiRequestContext)
+   */
   public void processRender(WebuiRequestContext context) throws Exception {    
     HttpServletRequest request = context.getRequest();           
     FCKEditor editor = new FCKEditor(request,getName());
     if(fckConfig == null) {
       FCKEditorConfig editorConfig = new FCKEditorConfig();
       localize(editorConfig);
-      editor.setConfig(editorConfig);
-      if(useBasicToolbar) {
-        editor.setToolbarSet(BASIC_TOOLBAR);
-      }else {
-        editor.setToolbarSet(DEFAULT_TOOLBAR);
-      }            
+      editor.setConfig(editorConfig);                  
     }else {
       localize(fckConfig);
       editor.setConfig(fckConfig);      
     }    
+    if(useBasicToolbar) {
+      editor.setToolbarSet(BASIC_TOOLBAR);
+    }else {
+      editor.setToolbarSet(DEFAULT_TOOLBAR);
+    }
     if (value_ == null) 
       value_ = "" ;
     editor.setValue(value_);
