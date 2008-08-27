f425389954b01f924a40bdd1bfe8f9c7180bb721
Pham Xuan Hoa <hoa.pham@exoplatform.com>
2008-08-27 01:52:20 +0000

    allow user set toolbar name and use source mode on startup

--- webui/core/src/main/java/org/exoplatform/webui/form/wysiwyg/UIFormWYSIWYGInput.java
+++ webui/core/src/main/java/org/exoplatform/webui/form/wysiwyg/UIFormWYSIWYGInput.java
@@ -20,6 +20,7 @@ import java.io.Writer;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang.StringUtils;
 import org.exoplatform.webui.application.WebuiRequestContext;
 import org.exoplatform.webui.form.UIFormInputBase;
 
@@ -33,80 +34,100 @@ import org.exoplatform.webui.form.UIFormInputBase;
  * The Class UIFormWYSIWYGInput.
  */
 public class UIFormWYSIWYGInput extends UIFormInputBase<String> {
-  private final String BASIC_TOOLBAR = "Basic".intern();
-  private final String DEFAULT_TOOLBAR = "Default".intern();
+	public static final String BASIC_TOOLBAR = "Basic".intern();
+	public static final String DEFAULT_TOOLBAR = "Default".intern();
 
-  private FCKEditorConfig fckConfig;  
-  private boolean useBasicToolbar = true;  
+	private FCKEditorConfig fckConfig;	
+	@Deprecated
+	private boolean useBasicToolbar = true;
+	private boolean sourceModeOnStartup = false;
+	private String toolBarName;
 
-  /**
-   * Instantiates a new uI form wysiwyg input.
-   * 
-   * @param name the name
-   * @param bindingField the binding field
-   * @param value the value
-   * @param isBasic the is basic
-   */
-  public UIFormWYSIWYGInput(String name, String bindingField, String value, boolean isBasic) {
-    super(name, bindingField, String.class);
-    useBasicToolbar = isBasic;    
-    this.value_ = value;
-  }
+	/**
+	 * Instantiates a new uI form wysiwyg input.
+	 * 
+	 * @param name the name
+	 * @param bindingField the binding field
+	 * @param value the value
+	 * @param isBasic the is basic
+	 */
+	@Deprecated
+	public UIFormWYSIWYGInput(String name, String bindingField, String value, boolean isBasic) {
+		super(name, bindingField, String.class);
+		useBasicToolbar = isBasic;    
+		this.value_ = value;
+	}
 
-  /**
-   * Gets the fCK config.
-   * 
-   * @return the fCK config
-   */
-  public FCKEditorConfig getFCKConfig() { return fckConfig; }
-  
-  /**
-   * Sets the fCK config.
-   * 
-   * @param config the new fCK config
-   */
-  public void setFCKConfig(FCKEditorConfig config) {this.fckConfig = config; }
+	public UIFormWYSIWYGInput(String name, String bindingField, String value) {
+		super(name, bindingField, String.class);	    
+		this.value_ = value;
+	}
 
-  /* (non-Javadoc)
-   * @see org.exoplatform.webui.form.UIFormInputBase#decode(java.lang.Object, org.exoplatform.webui.application.WebuiRequestContext)
-   */
-  public void decode(Object input, WebuiRequestContext context) throws Exception {
-    value_ = (String) input;
-    if(value_ != null && value_.length() == 0) value_ = null ;
-  }
+	/**
+	 * Gets the fCK config.
+	 * 
+	 * @return the fCK config
+	 */
+	public FCKEditorConfig getFCKConfig() { return fckConfig; }
 
-  /* (non-Javadoc)
-   * @see org.exoplatform.webui.core.UIComponent#processRender(org.exoplatform.webui.application.WebuiRequestContext)
-   */
-  public void processRender(WebuiRequestContext context) throws Exception {    
-    HttpServletRequest request = context.getRequest();           
-    FCKEditor editor = new FCKEditor(request,getName());
-    if(fckConfig == null) {
-      FCKEditorConfig editorConfig = new FCKEditorConfig();
-      localize(editorConfig);
-      editor.setConfig(editorConfig);                  
-    }else {
-      localize(fckConfig);
-      editor.setConfig(fckConfig);      
-    }    
-    if(useBasicToolbar) {
-      editor.setToolbarSet(BASIC_TOOLBAR);
-    }else {
-      editor.setToolbarSet(DEFAULT_TOOLBAR);
-    }
-    if (value_ == null) 
-      value_ = "" ;
-    editor.setValue(value_);
-    Writer w =  context.getWriter();            
-    w.write(editor.createHtml());
-    if (this.isMandatory()) w.write(" *");    
-  }
+	/**
+	 * Sets the fCK config.
+	 * 
+	 * @param config the new fCK config
+	 */  
+	public void setFCKConfig(FCKEditorConfig config) {this.fckConfig = config; }
 
-  private void localize(final FCKEditorConfig config){
-    WebuiRequestContext requestContext = WebuiRequestContext.getCurrentInstance();
-    String currentLanguage = requestContext.getLocale().getLanguage();
-    config.put("AutoDetectLanguage","false");
-    config.put("DefaultLanguage",currentLanguage);    
-  }
+	public boolean getSourceModeOnStartup() { return this.sourceModeOnStartup; }
+	public void setSourceModeOnStartup(boolean b) { this.sourceModeOnStartup = b; }
+	
+	public String getToolBarName() { return this.toolBarName; }
+	public void setToolBarName(String name) { this.toolBarName = name; }
+	
+	/* (non-Javadoc)
+	 * @see org.exoplatform.webui.form.UIFormInputBase#decode(java.lang.Object, org.exoplatform.webui.application.WebuiRequestContext)
+	 */
+	public void decode(Object input, WebuiRequestContext context) throws Exception {
+		value_ = (String) input;
+		if(value_ != null && value_.length() == 0) value_ = null ;
+		//In FireFox, FCKEditor add <br /> at these end of content 
+		//when FCKConfig.EnterMode = br so we need clear the tag		
+		if(value_.endsWith("<br />")) {
+			value_ = StringUtils.substringBeforeLast(value_, "<br />");
+		}
+	}
 
+	/* (non-Javadoc)
+	 * @see org.exoplatform.webui.core.UIComponent#processRender(org.exoplatform.webui.application.WebuiRequestContext)
+	 */
+	public void processRender(WebuiRequestContext context) throws Exception {    
+		HttpServletRequest request = context.getRequest();           
+		FCKEditor editor = new FCKEditor(request,getName());
+		if(fckConfig == null) {
+			FCKEditorConfig editorConfig = new FCKEditorConfig();
+			configureFCKConfig(editorConfig);
+			editor.setConfig(editorConfig);                  
+		}else {
+			configureFCKConfig(fckConfig);
+			editor.setConfig(fckConfig);      
+		}    
+		if(toolBarName != null && toolBarName.length()>0) {
+			editor.setToolbarSet(toolBarName);
+		}else {
+			editor.setToolbarSet(BASIC_TOOLBAR);
+		}
+		if (value_ == null) 
+			value_ = "" ;
+		editor.setValue(value_);
+		Writer w =  context.getWriter();            
+		w.write(editor.createHtml());
+		if (this.isMandatory()) w.write(" *");    
+	}
+
+	private void configureFCKConfig(final FCKEditorConfig config){
+		WebuiRequestContext requestContext = WebuiRequestContext.getCurrentInstance();
+		String currentLanguage = requestContext.getLocale().getLanguage();
+		config.put("AutoDetectLanguage","false");
+		config.put("DefaultLanguage",currentLanguage);
+		config.put("SourceModeOnStartup",Boolean.toString(sourceModeOnStartup));
+	}  
 }
