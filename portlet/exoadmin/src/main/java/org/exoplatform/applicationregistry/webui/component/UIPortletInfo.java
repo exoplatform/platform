a0417618bc740d05beb661e34b563deb8c6fe76c
Pham Thanh Tung <tung.pham@exoplatform.com>
2008-09-11 10:35:25 +0000

    PORTAL-1730

---
+++ portlet/exoadmin/src/main/java/org/exoplatform/applicationregistry/webui/component/UIPortletInfo.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2003-2008 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.applicationregistry.webui.component;
+
+import org.exoplatform.services.portletcontainer.pci.PortletData;
+import org.exoplatform.webui.config.annotation.ComponentConfig;
+import org.exoplatform.webui.core.UIComponent;
+
+/**
+ * Created by The eXo Platform SAS
+ * Author : Pham Thanh Tung
+ *          thanhtungty@gmail.com
+ * Sep 11, 2008  
+ */
+@ComponentConfig(
+    template = "app:/groovy/applicationregistry/webui/component/UIPortletInfo.gtmpl"
+)
+public class UIPortletInfo extends UIComponent {
+  
+  private PortletData portlet_;
+  
+  public void setPortlet(PortletData portlet) { portlet_ = portlet; }
+  
+  public PortletData getPortlet() { return portlet_; }
+}
--- portlet/exoadmin/src/main/java/org/exoplatform/applicationregistry/webui/component/UIPortletManagement.java
+++ portlet/exoadmin/src/main/java/org/exoplatform/applicationregistry/webui/component/UIPortletManagement.java
@@ -26,7 +26,10 @@ import org.exoplatform.services.portletcontainer.PortletContainerService;
 import org.exoplatform.services.portletcontainer.pci.PortletData;
 import org.exoplatform.webui.application.WebuiRequestContext;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
+import org.exoplatform.webui.config.annotation.EventConfig;
 import org.exoplatform.webui.core.UIContainer;
+import org.exoplatform.webui.event.Event;
+import org.exoplatform.webui.event.EventListener;
 
 /**
  * Created by The eXo Platform SAS
@@ -36,18 +39,24 @@ import org.exoplatform.webui.core.UIContainer;
  */
 
 @ComponentConfig(
-    template = "app:/groovy/applicationregistry/webui/component/UIPortletManagement.gtmpl"
+    template = "app:/groovy/applicationregistry/webui/component/UIPortletManagement.gtmpl",
+    events = {
+        @EventConfig(listeners = UIPortletManagement.SelectPortletActionListener.class)
+    }
 )
 
 public class UIPortletManagement extends UIContainer {
   
   static final public String LOCAL = "local";
   static final public String REMOTE = "remote";
-  List<PortletData> localPortlets;
-  List<PortletData> remotePortlets;
+  private List<PortletData> localPortlets;
+  private List<PortletData> remotePortlets;
+  private PortletData selectedPorlet ;
   
   public UIPortletManagement() throws Exception {
     init() ;
+    UIPortletInfo uiPortletInfo = addChild(UIPortletInfo.class, null, null) ;
+    uiPortletInfo.setPortlet(localPortlets.get(0)) ;
   }
   
   public void init() throws Exception {
@@ -65,9 +74,39 @@ public class UIPortletManagement extends UIContainer {
   public List<PortletData> getRemotePortlets() {
     return remotePortlets;
   }
+  
+  public PortletData getSelectedPortlet() { return selectedPorlet; }
+  
+  public void setSelectedPortlet(PortletData portlet) { selectedPorlet = portlet; }
+  
+  public void setSelectedPortlet(String name) {
+    for(PortletData ele : localPortlets) {
+      if(ele.getPortletName().equals(name)) {
+        setSelectedPortlet(ele) ;
+        break ;
+      }
+    }
+  }
 
   public void processRender(WebuiRequestContext context) throws Exception {
     super.processRender(context);
+  }
+  
+  static public class SelectPortletActionListener extends EventListener<UIPortletManagement> {
+
+    public void execute(Event<UIPortletManagement> event) throws Exception {
+      UIPortletManagement uiManagement = event.getSource() ;
+      String portletName = event.getRequestContext().getRequestParameter(OBJECTID) ;
+      uiManagement.setSelectedPortlet(portletName) ;
+      UIPortletInfo uiPortletInfo = uiManagement.getChild(UIPortletInfo.class) ;
+      if(uiPortletInfo == null) {
+        uiManagement.getChildren().clear() ;
+        uiPortletInfo = uiManagement.addChild(UIPortletInfo.class, null, null) ;
+      }
+      uiPortletInfo.setPortlet(uiManagement.getSelectedPortlet()) ;
+      event.getRequestContext().addUIComponentToUpdateByAjax(uiManagement) ;
+    }
+    
   }
 
 }
