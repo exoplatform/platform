612802ef5dcc3d9d4bf2d75902f351421253dc94
Thuy Nguyen <thuy.nguyen@exoplatform.com>
2008-10-15 07:45:41 +0000

    add skin for dashboard

--- portlet/dashboard/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
+++ portlet/dashboard/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
@@ -7,6 +7,27 @@ SkinService.addSkin(
 ) ;
 
 SkinService.addSkin(
+    "dashboard/DashboardPortlet",
+    "Mac",
+    "/dashboard/skin/dashboard/webui/component/UIDashboardPortlet/DefaultStylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "dashboard/DashboardPortlet",
+    "Vista",
+    "/dashboard/skin/dashboard/webui/component/UIDashboardPortlet/DefaultStylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "dashboard/GadgetPortlet",
+    "Default",
+    "/dashboard/skin/gadget/webui/component/UIGadgetPortlet/DefaultStylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
     "dashboard/GadgetPortlet",
     "Mac",
     "/dashboard/skin/gadget/webui/component/UIGadgetPortlet/DefaultStylesheet.css",
