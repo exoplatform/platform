7f84dd1829a5b5e0796224e537612bedd3fe3195
Nguyen Ba Uoc <uoc.nguyen@exoplatform.com>
2008-10-30 07:17:44 +0000

    PORTAL-1602

--- web/eXoResources/src/main/webapp/javascript/eXo/core/Cometd.js
+++ web/eXoResources/src/main/webapp/javascript/eXo/core/Cometd.js
@@ -9,10 +9,15 @@ at the handshake, so the transport should not do it, and be set after.
 
 */
 
-
+/**
+ *  @author Uoc Nguyen (uoc.nguyen@exoplatform.com)
+ *  @description Re-written.
+ *
+ */
 function Cometd() {
 	this._connected = false;
 	this._polling = false;
+  this._connecting = false;
 
 	this.currentTransport=null;
 	this.url = '/cometd/cometd';
@@ -20,20 +25,32 @@ function Cometd() {
 	this.exoId = null;
 	this.exoToken = null;
 
-	this.clientId = eXo.core.Browser.getCookie("cometdClientID");
+  var Browser = eXo.core.Browser;
+
+  this.clientId = Browser.getCookie('cometdClientID') || false;
 	this.messageId = 0;
 	this.batch=0;
 
 	this._subscriptions = [];
 	this._messageQ = [];
+  this._connectionReadyCallbacks = [];
 
-	this._maxInterval=30000;
-	this._backoffInterval=1000;		
+	//this._maxInterval=30000;
+	this._maxInterval=5*1000;
+	this._backoffInterval=1000;
+  this._maxTry = 5;
+  this._tryToOpenTunnelCnt = 0;
+  this._retryInterval = 0;
+  this._multiClientsDetectCnt = 0;
 }
 
-
-
-Cometd.prototype.init = function() {
+Cometd.prototype.init = function(forceDisconnect) {
+  this._tryToOpenTunnelCnt = 0;
+  if ((!forceDisconnect &&
+      this._connected) ||
+      this._connecting) {
+    return;
+  }
 	if(!this.currentTransport) {
 		this.currentTransport = new eXo.portal.LongPollTransport();
 		this.currentTransport.init(this);
@@ -43,6 +60,22 @@ Cometd.prototype.init = function() {
 		this.currentTransport.initTunnel();
 	else
 		this.currentTransport.initHandshake();
+};
+
+Cometd.prototype.addOnConnectionReadyCallback = function(handle) {
+  if (handle) {
+    this._connectionReadyCallbacks.push(handle);
+  }
+};
+
+Cometd.prototype.removeOnConnectionReadtCallback = function(handle) {
+  for (var i=0; i<this._connectionReadyCallbacks.length; i++) {
+    if (this._connectionReadyCallbacks[i] == handle) {
+      this._connectionReadyCallbacks[i] = this._connectionReadyCallbacks[this._connectionReadyCallbacks.length - 1];
+      this._connectionReadyCallbacks.pop();
+      break;
+    }
+  }
 };
 
 // public API functions called by cometd or by the transport classes
@@ -56,41 +89,54 @@ Cometd.prototype.isConnected = function(){
 }
 
 Cometd.prototype._deliver = function(message){
+  //console.warn('Polling: ' + this._polling + ' - connected: ' + this._connected);
 	// dipatch events along the specified path
 
-	if(!message["channel"]){
-		if(message["success"] !== true){
-			//console.debug("cometd error: no channel for message!", message);
+	if(!message['channel']){
+		if(message['success'] !== true){
+			//console.debug('cometd error: no channel for message!', message);
 			return;
 		}
 	}
 	this.lastMessage = message;
 
 	if(message.advice){
-		this.advice = message.advice; // TODO maybe merge?
+    this.adviceBackup = this.advice;
+		this.advice = message.advice;
+    this.multiClients = message.advice['multiple-clients'];
+    if (this.multiClients) {
+      this._multiClientsDetectCnt ++;
+      //console.warn('Multiple clients detected and notify from server');
+      if (this._multiClientsDetectCnt == 1) {
+        window.alert('You has multiple tab/window using Cometd!\nPlease keep only once.');
+      }
+    } else {
+      this._multiClientsDetectCnt = 0;
+      this.resetRetryInterval();
+    }
 	}
 
 	// check to see if we got a /meta channel message that we care about
-	if(	(message["channel"]) &&
+	if(	(message['channel']) &&
 		(message.channel.length > 5)&&
-		(message.channel.substr(0, 5) == "/meta")){
+		(message.channel.substr(0, 5) == '/meta')){
 		// check for various meta topic actions that we need to respond to
 		switch(message.channel){
-			case "/meta/connect":
+			case '/meta/connect':
 				if(message.successful && !this._connected){
 					this._connected = true;
 					this.endBatch();
 				}                                     
 				break;
-			case "/meta/subscribe":
+			case '/meta/subscribe':
 				if(!message.successful){
-					alert("todo manage error subscription");
+					alert('todo manage error subscription');
 					return;
 				}
 				break;
-			case "/meta/unsubscribe":
+			case '/meta/unsubscribe':
 				if(!message.successful){
-					alert("todo manage error unsubscription");
+					alert('todo manage error unsubscription');
 					return;
 				}
 				break;
@@ -125,7 +171,7 @@ Cometd.prototype.subscribe = function(	/*String */	channel,
 		if(!subs || subs.length==0){
 			subs=[];
 			var message = {
-				channel: "/meta/subscribe",
+				channel: '/meta/subscribe',
 				subscription: channel,
 				exoId: this.exoId,
 				exoToken: this.exoToken
@@ -137,7 +183,6 @@ Cometd.prototype.subscribe = function(	/*String */	channel,
 	}
 }
 
-
 Cometd.prototype.unsubscribe = function(/*string*/ channel){
 
 	var tname = channel;
@@ -146,7 +191,7 @@ Cometd.prototype.unsubscribe = function(/*string*/ channel){
 	}
 
 	this._sendMessage({
-		channel: "/meta/unsubscribe",
+		channel: '/meta/unsubscribe',
 		subscription: channel,
 		exoId: this.exoId,
 		exoToken: this.exoToken
@@ -158,22 +203,35 @@ Cometd.prototype.startBatch = function(){
 }
 
 Cometd.prototype.increaseRetryInterval = function() {
-	if (!this.advice)
-		this.advice = {};
-	if(!this.advice.interval)
-		this.advice.interval = 0;
-	if (this.advice.interval < 115000) {
-		this.advice.interval += 5000;
+  this.advice = this.advice || {};
+	if(!this.advice.interval ||
+     (this.advice.interval &&
+      this.advice.interval > this._maxInterval)) {
+    this.resetRetryInterval();
+  } else {
+		this._retryInterval += this._backoffInterval;
+    this.advice.interval = this._retryInterval;
 	}
+  //console.warn('Increased retry interval to: ' + this._retryInterval);
 }
 
 Cometd.prototype.resetRetryInterval = function() {
+  //console.warn('Reset retry interval');
 	if(this.advice) 
 		this.advice.interval = 0;
-
+  this._retryInterval = 0;
 }
 
 Cometd.prototype.endBatch = function(){
+  this._tryToOpenTunnelCnt = 0;
+  this._connecting = false;
+  // Callback to on connection ready handlers
+  for (var i=0; i<this._connectionReadyCallbacks.length; i++) {
+    var handler = this._connectionReadyCallbacks[i];
+    if (handler) {
+      handler();
+    }
+  }
 	if(--this.batch <= 0 && this.currentTransport && this._connected){
 		this.batch=0;
 
@@ -185,8 +243,8 @@ Cometd.prototype.endBatch = function(){
 	}
 }
 
-
 Cometd.prototype.disconnect = function(){
+  this._tryToOpenTunnelCnt = 0;
 	this._subscriptions.each(this.unsubscribe, this);
 	this._messageQ = [];
 	if(this.currentTransport){
@@ -198,12 +256,10 @@ Cometd.prototype.disconnect = function(){
 
 Cometd.prototype._backoff = function(){
 	if(!this.advice || !this.advice.interval){
-		this.advice={reconnect:"retry",interval:0};
+		this.advice={reconnect:'retry',interval:0};
 	}
-	if(this.advice.interval<this._maxInterval){
-		this.advice.interval+=this._backoffInterval;
-	}
-	/*if(this.advice.reconnect == "handshake") {
+  this.increaseRetryInterval();
+	/*if(this.advice.reconnect == 'handshake') {
 		
 	}*/
 }
@@ -213,7 +269,7 @@ function LongPollTransport() {
 
 
 	instance.init = function(cometd) {
-		this._connectionType="long-polling";
+		this._connectionType='long-polling';
 		this._cometd=cometd;
 	}
 
@@ -225,13 +281,13 @@ function LongPollTransport() {
 
 	instance.initHandshake = function() {
 		var message = {
-			channel:	"/meta/handshake",
+			channel:	'/meta/handshake',
 			id:	this._cometd.messageId++,
 			exoId: this._cometd.exoId,
 			exoToken: this._cometd.exoToken
 		};
 	
-		var query = "message=" + eXo.core.JSON.stringify(message);
+		var query = 'message=' + eXo.core.JSON.stringify(message);
 
 		var request = new eXo.portal.AjaxRequest('POST', this._cometd.url, query);
 		request.onSuccess = function(request){
@@ -250,18 +306,18 @@ function LongPollTransport() {
 		this._cometd.handshakeReturn = data;
 	
 		// pick a transport
-		if(data["advice"]){
+		if(data['advice']){
 			this._cometd.advice = data.advice;
 		}
    
 	   	if(!data.successful){
-			//console.debug("cometd init failed");
-			if(this._cometd.advice && this._cometd.advice["reconnect"]=="none"){
+			//console.debug('cometd init failed');
+			if(this._cometd.advice && this._cometd.advice['reconnect']=='none'){
 				return;
 			}
 
-			if( this._cometd.advice && this._cometd.advice["interval"] && this._cometd.advice.interval>0 ){
-				setTimeout(function(){ eXo.core.Cometd.init(); }, this._cometd.advice.interval);
+			if( this._cometd.advice && this._cometd.advice['interval'] && this._cometd.advice.interval>0 ){
+				setTimeout(function(){ eXo.core.Cometd.init(); }, this._cometd._retryInterval);
 			}else{
 				this._cometd.init(this.url,this._props);
 			}
@@ -269,12 +325,12 @@ function LongPollTransport() {
 			return;
 		}
 		if(data.version < this.minimumVersion){
-			//console.debug("cometd protocol version mismatch. We wanted", this.minimumVersion, "but got", data.version);
+			//console.debug('cometd protocol version mismatch. We wanted', this.minimumVersion, 'but got', data.version);
 			return;
 		}
 
 		this._cometd.clientId = data.clientId;
-		eXo.core.Browser.setCookie("cometdClientID", this._cometd.clientId, 1);
+		eXo.core.Browser.setCookie('cometdClientID', this._cometd.clientId, 1);
 
 		this.initTunnel();
 	
@@ -282,7 +338,7 @@ function LongPollTransport() {
 
 	instance.initTunnel = function() {
 		var message = {
-			channel:	"/meta/connect",
+			channel:	'/meta/connect',
 			clientId:	this._cometd.clientId,
 			connectionType: this._connectionType,
 			id:	this._cometd.messageId++
@@ -293,7 +349,7 @@ function LongPollTransport() {
 	instance.openTunnelWith = function(content, url){
 		this._cometd._polling = true;
 		// just a hack, need to be changed, we should serialize the full object
-		var query = "message=" + content.message;
+		var query = 'message=' + content.message;
 	
 		var request = new eXo.portal.AjaxRequest('POST', (url||this._cometd.url), query);
 		//timeout set to 3 min because of longpoll
@@ -302,7 +358,7 @@ function LongPollTransport() {
 								this._cometd._polling = false;
 								if (request.status >=200 && request.status < 300) {
 									this._cometd.deliver(request.evalResponse());
-									this._cometd.resetRetryInterval();
+									//this._cometd.resetRetryInterval();
 								}
 								else
 									this._cometd._backoff();
@@ -312,7 +368,8 @@ function LongPollTransport() {
 		request.onError = 	function(err) {
 								this.tunnelReq = null;
 								this._cometd._polling = false;
-								//console.debug("tunnel opening failed:", err);
+								//console.debug('tunnel opening failed:', err);
+                this._cometd._tryToOpenTunnelCnt++;
 								this.tunnelCollapse();
 								throw (new Error('tunnel opening failed')) ;
 							}.bind(this);
@@ -321,22 +378,25 @@ function LongPollTransport() {
 	}
 
 	instance.tunnelCollapse = function(){
+    if (this._cometd._tryToOpenTunnelCnt > this._cometd._maxTry) {
+      return;
+    }
 		if(!this._cometd._polling){
 			// try to restart the tunnel
 			this._cometd._polling = false;
 
 			// TODO handle transport specific advice
 
-			if(this._cometd["advice"]){
-				if(this._cometd.advice["reconnect"]=="none"){
+			if(this._cometd['advice']){
+				if(this._cometd.advice['reconnect']=='none'){
 					return;
 				}
 
-				if(	(this._cometd.advice["interval"])&&
+				if(	(this._cometd.advice['interval'])&&
 					(this._cometd.advice.interval>0) ){
 					var transport = this;
 					setTimeout(function(){ transport._connect(); },
-						this._cometd.advice.interval);
+						this._cometd._retryInterval);
 						this._cometd.increaseRetryInterval();
 				}else{
 					this._connect();
@@ -350,8 +410,8 @@ function LongPollTransport() {
 	}
 
 	instance._connect = function(){
-		if(	(this._cometd["advice"])&&
-			(this._cometd.advice["reconnect"]=="handshake")
+		if(	(this._cometd['advice'])&&
+			(this._cometd.advice['reconnect']=='handshake')
 		){
 			this._cometd.clientId = null;
 			this._cometd.init(this._cometd.url,this._cometd._props);
@@ -359,11 +419,11 @@ function LongPollTransport() {
 			this.openTunnelWith({
 				message: eXo.core.JSON.stringify([
 					{
-						channel:	"/meta/connect",
+						channel:	'/meta/connect',
 						connectionType: this._connectionType,
 						clientId:	this._cometd.clientId,
 						timestamp:	this.lastTimestamp,
-						id:		""+this._cometd.messageId++
+						id:		''+this._cometd.messageId++
 					}
 				])
 			});
@@ -373,10 +433,10 @@ function LongPollTransport() {
 	instance.sendMessages = function(messages){
 			for(var i=0; i<messages.length; i++){
 				messages[i].clientId = this._cometd.clientId;
-				messages[i].id = ""+this._cometd.messageId++;
+				messages[i].id = ''+this._cometd.messageId++;
 			}
 
-			var query = "message=" + eXo.core.JSON.stringify(messages);
+			var query = 'message=' + eXo.core.JSON.stringify(messages);
 
 			var request = new eXo.portal.AjaxRequest('POST', this._cometd.url, query);
 			request.onSuccess = function(request){
@@ -390,11 +450,11 @@ function LongPollTransport() {
 	}
 
 	instance.disconnect = function(){
-		var query = "message=" + eXo.core.JSON.stringify([
+		var query = 'message=' + eXo.core.JSON.stringify([
 			{
-				channel:	"/meta/disconnect",
+				channel:	'/meta/disconnect',
 				clientId:	this._cometd.clientId,
-				id:		""+this._cometd.messageId++
+				id:		''+this._cometd.messageId++
 			}
 		]);
 		var request = new eXo.portal.AjaxRequest('POST', this._cometd.url, query);
