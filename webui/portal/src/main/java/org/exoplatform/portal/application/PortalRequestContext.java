e338de6b92c74de983b88c4c3b9ab4a22f551cf0
Pham Xuan Hoa <hoa.pham@exoplatform.com>
2008-11-04 06:55:24 +0000

    PORTAL-2205 allow retrive page title/page meta

--- webui/portal/src/main/java/org/exoplatform/portal/application/PortalRequestContext.java
+++ webui/portal/src/main/java/org/exoplatform/portal/application/PortalRequestContext.java
@@ -30,6 +30,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.exoplatform.Constants;
+import org.exoplatform.portal.webui.portal.UIPortal;
+import org.exoplatform.portal.webui.util.Util;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.web.application.JavascriptManager;
 import org.exoplatform.web.application.URLBuilder;
@@ -43,35 +45,36 @@ import org.exoplatform.webui.core.lifecycle.HtmlValidator;
  * It mainly implements the abstract methods and overide some.
  */
 public class PortalRequestContext extends WebuiRequestContext {
-  
+
   protected static Log log = ExoLogger.getLogger("portal:PortalRequestContext");  
-  
+
   final  static public int PUBLIC_ACCESS  =   0 ;
   final  static public int PRIVATE_ACCESS =   1 ;
-  
+
   final static public String UI_COMPONENT_ACTION = "portal:action" ;
   final static public String UI_COMPONENT_ID = "portal:componentId" ;
-  final static public String CACHE_LEVEL = "portal:cacheLevel" ;
-  
-  
+  final static public String CACHE_LEVEL = "portal:cacheLevel" ;  
+  final static public String REQUEST_TITLE = "portal:requestTitle".intern();
+  final static public String REQUEST_METADATA = "portal:requestMetadata".intern();  
+
   private String portalOwner_ ;
   private String nodePath_ ;
   private String requestURI_ ;
   private String portalURI ;
-  
+
   private int accessPath = -1 ;  
-  
+
   private HttpServletRequest request_ ;
   private HttpServletResponse response_ ;
-  
+
   private String cacheLevel_ = "cacheLevelPortlet";
   private boolean  ajaxRequest_ = true ;
   private boolean  forceFullUpdate = false;
   private Writer writer_ ;
   protected JavascriptManager jsmanager_ = new  JavascriptManager() ;
-  
+
   public  JavascriptManager getJavascriptManager() { return jsmanager_ ; }
-  
+
   public PortalRequestContext(WebuiApplication app, HttpServletRequest req, HttpServletResponse res) throws Exception {
     super(app);
     request_ = req ;
@@ -80,7 +83,7 @@ public class PortalRequestContext extends WebuiRequestContext {
     ajaxRequest_ = "true".equals(req.getParameter("ajaxRequest")) ;
     String cache = req.getParameter(CACHE_LEVEL);
     if(cache != null) cacheLevel_ = cache;
-    
+
     requestURI_ = URLDecoder.decode(req.getRequestURI(), "UTF-8");
     String pathInfo = req.getPathInfo() ;
     if(pathInfo == null) pathInfo = "/" ;
@@ -88,12 +91,12 @@ public class PortalRequestContext extends WebuiRequestContext {
     if(colonIndex < 0) colonIndex = pathInfo.length();
     portalOwner_ =  pathInfo.substring(1, colonIndex) ;
     nodePath_ = pathInfo.substring(colonIndex , pathInfo.length()) ;
-    
+
     portalURI = requestURI_.substring(0, requestURI_.lastIndexOf(nodePath_)) + "/";
-    
+
     if(requestURI_.indexOf("/public/") >= 0) accessPath =  PUBLIC_ACCESS ;
     else if(requestURI_.indexOf("/private/") >= 0) accessPath =  PRIVATE_ACCESS ;
-    
+
     //TODO use the encoding from the locale-config.xml file
     response_.setContentType("text/html; charset=UTF-8");
     try {
@@ -101,24 +104,35 @@ public class PortalRequestContext extends WebuiRequestContext {
     }catch (UnsupportedEncodingException e) {
       log.error("Encoding not supported", e);
     }
-    
+
     urlBuilder = new PortalURLBuilder(requestURI_);
   }
-  
+
   public void refreshResourceBundle() throws Exception {
     appRes_ = getApplication().getResourceBundle(uiApplication_.getLocale()) ;  
   }
+
+  public String getTitle() {
+    String title = (String)request_.getAttribute(REQUEST_TITLE);
+    if(title != null) return title;
+    UIPortal uiportal = Util.getUIPortal();
+    return uiportal.getSelectedNode().getResolvedLabel();    
+  }
   
+  @SuppressWarnings("unchecked")
+  public Map<String,String> getMetaInformation() {
+    return (Map<String,String>)request_.getAttribute(REQUEST_METADATA);    
+  }
   public Locale getLocale() {  return uiApplication_.getLocale() ;} 
-  
+
   public String getCacheLevel() { return cacheLevel_ ; }
 
   public String getRequestParameter(String name) { return request_.getParameter(name) ; }
-  
+
   public String[] getRequestParameterValues(String name)  {
     return request_.getParameterValues(name) ;
   }  
-  
+
   public Map<String, String[]> getPortletParameters() {
     Map<String, String[]> unsortedParams = getRequest().getParameterMap();
     Map<String, String[]> sortedParams = new HashMap<String, String[]>(); 
@@ -131,48 +145,48 @@ public class PortalRequestContext extends WebuiRequestContext {
     }    
     return sortedParams;
   }
-  
+
   final public String getRequestContextPath() { return  request_.getContextPath(); }
-  
+
   public  String getActionParameterName() {  return PortalRequestContext.UI_COMPONENT_ACTION ; }
-  
+
   public  String getUIComponentIdParameterName() { return PortalRequestContext.UI_COMPONENT_ID; }
-  
+
   public String getPortalOwner() { return portalOwner_ ; }
-  
+
   public String getNodePath() { return nodePath_  ; }
-  
+
   public String getRequestURI()  { return requestURI_ ; }
-  
+
   public String getPortalURI() { return portalURI ; }
-  
+
   public URLBuilder getURLBuilder() { return urlBuilder; }
-  
+
   public int  getAccessPath() { return accessPath ;}
-  
+
   final public String getRemoteUser() { return request_.getRemoteUser() ; }
   final public boolean isUserInRole(String roleUser){ return request_.isUserInRole(roleUser); }
-  
+
   final public Writer getWriter() throws Exception { 
     if(writer_ == null) {
       writer_ = new HtmlValidator(response_.getWriter()) ;
     }
     return writer_ ; 
   }
-  
+
   final public  boolean useAjax() {  return ajaxRequest_; }
   
   @SuppressWarnings("unchecked")
   final public HttpServletRequest getRequest(){ return request_; }
-  
+
   @SuppressWarnings("unchecked")
   final public HttpServletResponse getResponse(){ return response_; }
-  
+
   final public boolean getFullRender() { return forceFullUpdate; }
 
   final public void setFullRender(boolean forceFullUpdate) { this.forceFullUpdate = forceFullUpdate; }
 
-  
+
   public void setHeaders(Map<String, String> headers) {
     Set<String> keys = headers.keySet();
     for (Iterator<String> iter = keys.iterator(); iter.hasNext();) {
@@ -180,6 +194,6 @@ public class PortalRequestContext extends WebuiRequestContext {
       response_.setHeader(key, headers.get(key));
     }
   }
-  
-  
+
+
 }
