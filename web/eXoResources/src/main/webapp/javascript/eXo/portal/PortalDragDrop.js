9d9b0e5e0eefd7e708cec31e97e790466bddf80a
Tan Pham Dinh <tan.pham@exoplatform.com>
2008-12-02 13:48:26 +0000

    PORTAL-2378, correct config of applicaton registry

--- web/eXoResources/src/main/webapp/javascript/eXo/portal/PortalDragDrop.js
+++ web/eXoResources/src/main/webapp/javascript/eXo/portal/PortalDragDrop.js
@@ -13,6 +13,9 @@ function PortalDragDrop() {
  */
 
 PortalDragDrop.prototype.init = function(e) {
+	if (!e) e = window.event;
+	if(((e.which) && (e.which == 2 || e.which == 3)) || ((e.button) && (e.button == 2)))	return;
+	
 	var DOMUtil = eXo.core.DOMUtil ;
 	var Browser = eXo.core.Browser ;
   var DragDrop = eXo.core.DragDrop ;
