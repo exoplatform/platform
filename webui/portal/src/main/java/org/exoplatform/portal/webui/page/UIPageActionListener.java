aa05500deef356637cfb2862968f306710dec856
Nguyen Dinh Tam <tam.nguyen@exoplatform.com>
2009-01-15 07:26:04 +0000

    WEBOS-176 Error when login program in special case

--- webui/portal/src/main/java/org/exoplatform/portal/webui/page/UIPageActionListener.java
+++ webui/portal/src/main/java/org/exoplatform/portal/webui/page/UIPageActionListener.java
@@ -138,17 +138,18 @@ public class UIPageActionListener {
           	break;
           }
         }
-//      TODO tam.nguyen: filter navigation, select navigation up to user
-        filter:
-        for(PageNavigation nav: navigations){
-          for(PageNode child: nav.getNodes()){
-            if(PageNavigationUtils.filter(child, pcontext.getRemoteUser()) != null) {
-              selecttedNode = child;
-              break filter;
+        // TODO tam.nguyen: filter navigation, select navigation up to user
+        if (selecttedNode == null) {
+          filter:
+          for(PageNavigation nav: navigations){
+            for(PageNode child: nav.getNodes()){
+              if(PageNavigationUtils.filter(child, pcontext.getRemoteUser()) != null) {
+                selecttedNode = child;
+                break filter;
+              }
             }
           }
         }
-
         uiPortal.setSelectedNode(selecttedNode);
 				if(selecttedNode == null) selectedPaths_.add(uiPortal.getSelectedNode()) ;
 				uiPortal.setSelectedPaths(selectedPaths_);
