ccfb146be623564fb054d989ec8d5ee685878f16
Patrice Lamarque <patrice.lamarque@exoplatform.com>
2010-06-17 02:50:13 +0000

    Profile filter now leverages PortalContainer.isScopeValid()

--- component/common/src/main/java/org/exoplatform/commons/platform/ProfileFilter.java
+++ component/common/src/main/java/org/exoplatform/commons/platform/ProfileFilter.java
@@ -1,80 +1,192 @@
 package org.exoplatform.commons.platform;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collection;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.Set;
 
-import javax.portlet.ActionRequest;
-import javax.portlet.ActionResponse;
 import javax.portlet.PortletContext;
 import javax.portlet.PortletException;
 import javax.portlet.PortletRequestDispatcher;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
-import javax.portlet.filter.ActionFilter;
 import javax.portlet.filter.FilterChain;
 import javax.portlet.filter.FilterConfig;
 import javax.portlet.filter.RenderFilter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
-import org.exoplatform.container.RootContainer;
+import org.exoplatform.container.PortalContainer;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
 
-public class ProfileFilter implements RenderFilter, ActionFilter {
+public class ProfileFilter implements RenderFilter {
 
   private static final String DISABLED_JSP = "/jsp/portlet-disabled.jsp";
 
-  private static Log LOG = ExoLogger.getExoLogger(ProfileFilter.class);
+  private static Log          LOG          = ExoLogger.getExoLogger(ProfileFilter.class);
 
-  String             profiles;
 
-  String             name;
-
-  PortletContext     context;
+  PortletContext              context;
 
   public void destroy() {
   }
 
   public void init(FilterConfig filterConfig) throws PortletException {
-    profiles = filterConfig.getInitParameter("profiles");
-    name = filterConfig.getPortletContext().getPortletContextName();
     context = filterConfig.getPortletContext();
   }
 
   public void doFilter(RenderRequest request, RenderResponse response, FilterChain chain) throws IOException,
                                                                                          PortletException {
-    if (profilesDisabled()) {
 
-      LOG.info("portlet" + name + " is currently disabled. You need to enable it with -Dexo.profile="
-          + profiles);
+    boolean isPortletActive = PortalContainer.isScopeValid(PortalContainer.getInstance(),
+                                                           new FakeServletContext());
+
+    if (isPortletActive) {
+      chain.doFilter(request, response);
+
+    } else {
+
+      LOG.info("portlet" + context.getPortletContextName()
+          + " is currently disabled. You need to enable it with -Dexo.profile=");
       PortletRequestDispatcher dispatcher = context.getRequestDispatcher(DISABLED_JSP);
       dispatcher.include(request, response);
-    } else {
-      chain.doFilter(request, response);
+
     }
   }
 
-  public void doFilter(ActionRequest request, ActionResponse response, FilterChain chain) throws IOException,
-                                                                                         PortletException {
+  /**
+   * a fake servelt context that gives this portlet servlet context name in
+   * return to {@link #getServletContextName()}
+   * 
+   * @author patricelamarque
+   */
+  class FakeServletContext implements ServletContext {
 
-    if (profilesDisabled()) {
-
-      LOG.info("portlet" + name + " is currently disabled. You need to enable it with -Dexo.profile="
-          + profiles);
-    
-      PortletRequestDispatcher dispatcher = context.getRequestDispatcher(DISABLED_JSP);
-      dispatcher.include(request, response);
-    } else {
-      chain.doFilter(request, response);
+    public Object getAttribute(String name) {
+      // TODO Auto-generated method stub
+      return null;
     }
-  }
 
-  private boolean profilesDisabled() {
-    if (profiles != null) {
-      Collection<String> col = Arrays.asList(profiles.split(","));
-      return !RootContainer.getProfiles().containsAll(col);
+    public Enumeration getAttributeNames() {
+      // TODO Auto-generated method stub
+      return null;
     }
-    return true;
+
+    public ServletContext getContext(String uripath) {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    public String getInitParameter(String name) {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    public Enumeration getInitParameterNames() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    public int getMajorVersion() {
+      // TODO Auto-generated method stub
+      return 0;
+    }
+
+    public String getMimeType(String file) {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    public int getMinorVersion() {
+      // TODO Auto-generated method stub
+      return 0;
+    }
+
+    public RequestDispatcher getNamedDispatcher(String name) {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    public String getRealPath(String path) {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    public RequestDispatcher getRequestDispatcher(String path) {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    public URL getResource(String path) throws MalformedURLException {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    public InputStream getResourceAsStream(String path) {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    public Set getResourcePaths(String path) {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    public String getServerInfo() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    public Servlet getServlet(String name) throws ServletException {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    // HACK : we
+    public String getServletContextName() {
+      return context.getPortletContextName();
+    }
+
+    public Enumeration getServletNames() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    public Enumeration getServlets() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    public void log(String msg) {
+      // TODO Auto-generated method stub
+
+    }
+
+    public void log(Exception exception, String msg) {
+      // TODO Auto-generated method stub
+
+    }
+
+    public void log(String message, Throwable throwable) {
+      // TODO Auto-generated method stub
+
+    }
+
+    public void removeAttribute(String name) {
+      // TODO Auto-generated method stub
+
+    }
+
+    public void setAttribute(String name, Object object) {
+      // TODO Auto-generated method stub
+
+    }
+
   }
 
 }
