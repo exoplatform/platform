6b0a48eddeedd2f83821d6d77c557e8c67513a0e
Ahmed Zaoui <zaoui.ahmed@exoplatform.com>
2010-07-01 16:35:52 +0000

    PLF-173:create office portal

---
+++ office/portlets/construction/src/main/java/org/exoplatform/sebastian2/component/UIConstructionPortlet.java
@@ -0,0 +1,19 @@
+package org.exoplatform.sebastian2.component;
+
+import org.exoplatform.webui.config.annotation.ComponentConfig;
+import org.exoplatform.webui.core.UIPortletApplication;
+import org.exoplatform.webui.core.lifecycle.UIApplicationLifecycle;
+
+
+@ComponentConfig(
+	lifecycle = UIApplicationLifecycle.class,
+	template = "app:/groovy/construction/portlet/UIConstructionPortlet.gtmpl"
+)
+
+public class UIConstructionPortlet extends UIPortletApplication {
+
+	public UIConstructionPortlet() throws Exception {
+
+	}
+
+}
