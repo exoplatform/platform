3b8dc0d0d9ec8eff7c5be9ca52f4d7377c1d36d6
Philippe  Aristote <philippe.aristote@exoplatform.com>
2010-07-21 07:26:10 +0000

    PLF-148 reflect commons changes in packaging descriptor

--- packaging/module/src/main/javascript/platform.packaging.module.js
+++ packaging/module/src/main/javascript/platform.packaging.module.js
@@ -34,11 +34,11 @@ function getModule(params)
    module.fck.deployName = "fck";
    
    // cometd required by KS & CS
-   module.cometd = new Project("org.exoplatform.platform", "exo.platform.commons.comet.webapp", "war", cometVersion).
+   module.cometd = new Project("org.exoplatform.commons", "exo.platform.commons.comet.webapp", "war", cometVersion).
    addDependency(new Project("org.mortbay.jetty", "cometd-bayeux", "jar", "${org.mortbay.jetty.cometd-bayeux.version}")).
    addDependency(new Project("org.mortbay.jetty", "jetty-util", "jar", "${org.mortbay.jetty.jetty-util.version}")).
    addDependency(new Project("org.mortbay.jetty", "cometd-api", "jar", "${org.mortbay.jetty.cometd-api.version}")).
-   addDependency(new Project("org.exoplatform.platform", "exo.platform.commons.comet.service", "jar", cometVersion));  	
+   addDependency(new Project("org.exoplatform.commons", "exo.platform.commons.comet.service", "jar", cometVersion));  	
    module.cometd.deployName = "cometd";
 	
    // main portal container config	
