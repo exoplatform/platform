a75b2b427723ac633121b1623e1e188b9b70509f
Boubaker  Khanfir <boubaker.khanfir@exoplatform.com>
2011-03-25 16:50:36 +0000

    PLF-884 : Sample Template Portals

---
+++ extension/webapp/src/main/webapp/WEB-INF/conf/uiconf/portal/webui/portal/PortalTemplateConfigOption.groovy
@@ -0,0 +1,45 @@
+/**
+ * Copyright (C) 2009 eXo Platform SAS.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+import java.util.List;
+import java.util.ArrayList;
+import org.exoplatform.portal.webui.portal.PortalTemplateConfigOption ;
+import org.exoplatform.webui.core.model.SelectItemCategory;
+
+List options = new ArrayList();
+
+  SelectItemCategory acme = new SelectItemCategory("ACMEPortal");
+  acme.addSelectItemOption(
+      new PortalTemplateConfigOption("ACMEPortal", "acme", "ACME Portal", "ACMEPortal").addGroup("/platform/guests")
+  );
+  options.add(acme);  
+  
+  SelectItemCategory intranet = new SelectItemCategory("IntranetPortal");
+  intranet.addSelectItemOption(
+      new PortalTemplateConfigOption("IntranetPortal", "intranet", "Intranet Portal", "IntranetPortal").addGroup("/platform/guests")
+  );
+  options.add(intranet);
+  
+  SelectItemCategory basic = new SelectItemCategory("BasicPortal");
+  basic.addSelectItemOption(
+      new PortalTemplateConfigOption("BasicPortal", "basic", "Basic Portal", "BasicPortal").addGroup("/platform/guests")
+  );
+  options.add(basic);
+  
+return options ;
