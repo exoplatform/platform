c5c1ba897d58aac3dc69c99c53a3c7f4a4ab3321
Anouar Chattouna <anouar.chattouna@exoplatform.com>
2011-05-05 13:31:44 +0000

    [PLF-1316] : revert last commit from plf 3.0.x

--- server/tomcat/patch/src/main/resources/stop_eXo.sh
+++ server/tomcat/patch/src/main/resources/stop_eXo.sh
@@ -50,7 +50,7 @@ cd ./bin
 
 # shutdown.sh -force -> it makes a Kill -9 on the process
 if [ -r ./shutdown.sh ]; then
-	exec ./shutdown.sh $@
+	exec ./shutdown.sh $1
 else
 	echo shutdown.sh is missing.
 fi
