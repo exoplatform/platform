0f3f40d677373f46bf2902840a6abbbbd26b1a26
Boubaker  Khanfir <boubaker.khanfir@exoplatform.com>
2011-07-26 11:56:03 +0000

    PLF-1824: Inline-editing doesn't work on PLF

--- extension/portlets/platformNavigation/src/main/java/org/exoplatform/platform/component/UIAdminToolbarPortlet.java
+++ extension/portlets/platformNavigation/src/main/java/org/exoplatform/platform/component/UIAdminToolbarPortlet.java
@@ -19,15 +19,41 @@
 
 package org.exoplatform.platform.component;
 
+import javax.portlet.MimeResponse;
+import javax.portlet.RenderResponse;
+
+import org.exoplatform.webui.application.WebuiApplication;
+import org.exoplatform.webui.application.WebuiRequestContext;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.core.UIPortletApplication;
 import org.exoplatform.webui.core.lifecycle.UIApplicationLifecycle;
+import org.w3c.dom.Element;
 
 @ComponentConfig(lifecycle = UIApplicationLifecycle.class)
-public class UIAdminToolbarPortlet extends UIPortletApplication
-{
-   public UIAdminToolbarPortlet() throws Exception
-   {
-      addChild(UIAdminToolbarContainer.class, null, null);
-   }
+public class UIAdminToolbarPortlet extends UIPortletApplication {
+  public UIAdminToolbarPortlet() throws Exception {
+    addChild(UIAdminToolbarContainer.class, null, null);
+  }
+
+  /*
+   * (non-Javadoc)
+   * @see
+   * org.exoplatform.webui.core.UIPortletApplication#processRender(org.
+   * exoplatform .webui.application.WebuiApplication,
+   * org.exoplatform.webui.application.WebuiRequestContext)
+   */
+  public void processRender(WebuiApplication app, WebuiRequestContext context) throws Exception {
+    RenderResponse response = context.getResponse();
+    Element elementS = response.createElement("script");
+    elementS.setAttribute("type", "text/javascript");
+    elementS.setAttribute("src", "/eXoWCMResources/javascript/eXo/wcm/frontoffice/private/QuickEdit.js");
+    response.addProperty(MimeResponse.MARKUP_HEAD_ELEMENT, elementS);
+
+    elementS = response.createElement("script");
+    elementS.setAttribute("type", "text/javascript");
+    elementS.setAttribute("src", "/eXoWCMResources/javascript/eXo/wcm/frontoffice/private/InlineEditing.js");
+    response.addProperty(MimeResponse.MARKUP_HEAD_ELEMENT, elementS);
+
+    super.processRender(app, context);
+  }
 }
