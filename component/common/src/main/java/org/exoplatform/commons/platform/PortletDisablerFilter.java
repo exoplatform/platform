7fcdcac8e8ce5d429e49087b9f88de2e4f1cd9ab
Clément  Balaÿ <cbalay@exoplatform.com>
2011-08-03 10:30:47 +0000

    PLF-98 - Gracefully deactivate portlets when their profile is inactive

--- component/common/src/main/java/org/exoplatform/commons/platform/PortletDisablerFilter.java
+++ component/common/src/main/java/org/exoplatform/commons/platform/PortletDisablerFilter.java
@@ -1,7 +1,12 @@
 package org.exoplatform.commons.platform;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Enumeration;
@@ -20,7 +25,10 @@ import javax.servlet.Servlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
+import org.exoplatform.container.ExoContainer;
 import org.exoplatform.container.PortalContainer;
+import org.exoplatform.container.configuration.ConfigurationManager;
+import org.exoplatform.container.configuration.ConfigurationManagerImpl;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
 
@@ -30,7 +38,7 @@ import org.exoplatform.services.log.Log;
  */
 public class PortletDisablerFilter implements RenderFilter {
 
-  private static final String DISABLED_JSP = "/jsp/portlet-disabled.jsp";
+  private static final String DISABLED_HTML = "war:/html/portlet-disabled.html";
 
   private static Log          LOG          = ExoLogger.getExoLogger(PortletDisablerFilter.class);
 
@@ -45,26 +53,79 @@ public class PortletDisablerFilter implements RenderFilter {
 
   /**
    * Serves {@link #DISABLED_JSP} if the portlet is not a valid dependency of the current portal container.
-   */
+   */ 
   public void doFilter(RenderRequest request, RenderResponse response, FilterChain chain) throws IOException,
                                                                                          PortletException {
 
-    boolean isPortletActive = PortalContainer.isScopeValid(PortalContainer.getInstance(),
-                                                           new FakeServletContext());
+    boolean isPortletActive = PortalContainer.isScopeValid(PortalContainer.getInstance(), new FakeServletContext());
 
     if (isPortletActive) {
       chain.doFilter(request, response);
 
     } else {
       LOG.info("portlet" + context.getPortletContextName() + " is currently disabled.");
-      PortletRequestDispatcher dispatcher = context.getRequestDispatcher(DISABLED_JSP);
-      dispatcher.include(request, response);
+      
+      PortalContainer pContainer = PortalContainer.getInstance();
+      ConfigurationManager confManager = new ConfigurationManagerImpl(pContainer.getPortalContext(), null);
+      
+      String html = "";
+      try {
+        InputStream inputStream = confManager.getInputStream(DISABLED_HTML);
+        html = convertStreamToString(inputStream);
+        html = mergeDisabledContent(html);
+      } catch (Exception e) {
+        LOG.error(e.getMessage());
+      }
+      
+      response.getWriter().write(html);
 
     }
   }
 
   /**
-   * a fake servelt context that gives this portlet servlet context name in
+   * Replace some variables into HTML content
+   * @param content
+   * @return
+   */
+  public String mergeDisabledContent(String content) {
+    String result = content;
+
+    result = result.replaceAll("\\$\\[portletName\\]", context.getPortletContextName());
+    result = result.replaceAll("\\$\\[requiredProfileName\\]", "");
+    result = result.replaceAll("\\$\\[listUserProfiles\\]", ExoContainer.getProfiles().toString());
+    
+    return result;
+  }
+  
+  public String convertStreamToString(InputStream is) throws IOException {
+    /*
+    * To convert the InputStream to String we use the
+    * Reader.read(char[] buffer) method. We iterate until the
+    * Reader return -1 which means there's no more data to
+    * read. We use the StringWriter class to produce the string.
+    */
+    if (is != null) {
+      Writer writer = new StringWriter();
+    
+      char[] buffer = new char[1024];
+      try {
+          Reader reader = new BufferedReader(
+                  new InputStreamReader(is, "UTF-8"));
+          int n;
+          while ((n = reader.read(buffer)) != -1) {
+              writer.write(buffer, 0, n);
+          }
+      } finally {
+          is.close();
+      }
+      return writer.toString();
+    } else {        
+      return "";
+    }
+  }
+
+  /**
+   * a fake servlet context that gives this portlet servlet context name in
    * return to {@link #getServletContextName()}
    */
   @SuppressWarnings("unchecked")
