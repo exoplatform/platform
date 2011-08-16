c96e89af763a4f57faed04b31cb158b09a134010
Boubaker  Khanfir <boubaker.khanfir@exoplatform.com>
2011-08-16 11:46:45 +0000

    PLF-1902: Gadget resources are not redeployed properly when using Platform Upgrade Framework

--- component/upgrade/plugins/src/main/java/org/exoplatform/platform/upgrade/plugins/LocalGadgetImporter.java
+++ component/upgrade/plugins/src/main/java/org/exoplatform/platform/upgrade/plugins/LocalGadgetImporter.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import org.exoplatform.application.gadget.LocalImporter;
 import org.exoplatform.application.gadget.impl.GadgetRegistry;
+import org.exoplatform.application.gadget.impl.LocalGadgetData;
 import org.exoplatform.container.PortalContainer;
 import org.exoplatform.container.configuration.ConfigurationManager;
 import org.gatein.common.io.IOTools;
@@ -16,16 +17,26 @@ import org.gatein.common.io.IOTools;
 public class LocalGadgetImporter extends LocalImporter {
   private ConfigurationManager configurationManager;
   private PortalContainer container;
+  private String gadgetPath;
+  private String gadgetRootAbsolutePath;
 
   public LocalGadgetImporter(String name, GadgetRegistry registry, String gadgetPath, ConfigurationManager configurationManager,
       PortalContainer container) {
     super(name, registry, gadgetPath, true);
     this.configurationManager = configurationManager;
     this.container = container;
+    this.gadgetPath = gadgetPath;
+    if (gadgetPath.startsWith("war:")) {
+      gadgetPath = gadgetPath.replace("war:", "");
+      this.gadgetRootAbsolutePath = container.getPortalContext().getRealPath(gadgetPath);
+      this.gadgetRootAbsolutePath = this.gadgetRootAbsolutePath.replace("\\", "/");
+      int gadgetRootPathIndex = this.gadgetRootAbsolutePath.lastIndexOf(gadgetPath);
+      this.gadgetRootAbsolutePath = this.gadgetRootAbsolutePath.substring(0, gadgetRootPathIndex);
+    }
   }
 
   @Override
-  public String getName(String resourcePath) throws IOException {
+  public String getName(String resourcePath) {
     // It's a directory, remove the trailing '/'
     resourcePath = resourcePath.replace("\\", "/");
     if (resourcePath.endsWith("/")) {
@@ -41,22 +52,38 @@ public class LocalGadgetImporter extends LocalImporter {
 
   @Override
   public String getParent(String resourcePath) throws IOException {
-    try {
-      if (!resourcePath.startsWith("war:") && !resourcePath.startsWith("classpath:") && !resourcePath.startsWith("jar:")) {
-        File file = new File(resourcePath);
-        return file.getParent();
-      } else if (resourcePath.startsWith("war:")) {
-        resourcePath = resourcePath.replace("war:", "");
-        File file = new File(container.getPortalContext().getRealPath(resourcePath)).getParentFile();
-        return file.getAbsolutePath();
-      } else {
-        File file = new File(configurationManager.getURL(resourcePath).getPath());
-        return file.getParent();
-      }
-    } catch (Exception exception) {
-      exception.printStackTrace();
+    String separator = getUsedSeparator(resourcePath);
+    if (resourcePath.indexOf("\\") >= 0) {// local path with an OS that
+                                          // uses \\ as separator
+      separator = "\\";
+    } else {
+      separator = "/";
     }
-    return null;
+    // It's a directory, remove the trailing separator
+    if (resourcePath.endsWith(separator)) {
+      resourcePath = resourcePath.substring(0, resourcePath.length() - 1);
+    }
+    // Get index of last separator
+    int index = resourcePath.lastIndexOf(separator);
+    // Return the parent that ends with a separator
+    return resourcePath.substring(0, index + 1);
+  }
+
+  public String getRealPath(String resourcePath) throws Exception {
+    if (!resourcePath.startsWith("war:") && !resourcePath.startsWith("classpath:") && !resourcePath.startsWith("jar:")) {
+      File file = new File(resourcePath);
+      return file.getAbsolutePath();
+    } else if (resourcePath.startsWith("war:")) {
+      if (!resourcePath.equals(gadgetPath)) {
+        resourcePath = resourcePath.replace("war:", "");
+        return gadgetRootAbsolutePath + resourcePath;
+      }
+      File file = new File(container.getPortalContext().getRealPath(resourcePath));
+      return file.getAbsolutePath();
+    } else {
+      File file = new File(configurationManager.getURL(resourcePath).getPath());
+      return file.getAbsolutePath();
+    }
   }
 
   @Override
@@ -87,14 +114,29 @@ public class LocalGadgetImporter extends LocalImporter {
     }
   }
 
+  private String getUsedSeparator(String filePath) {
+    String separator = null;
+    if (filePath.indexOf("\\") >= 0) {// local path with an OS that
+                                      // uses \\ as separator
+      separator = "\\";
+    } else {
+      separator = "/";
+    }
+    return separator;
+  }
+
   @Override
   public Iterable<String> getChildren(String folderPath) throws IOException {
     try {
-      File file = new File(folderPath);
+      String absoluteFolderPath = getRealPath(folderPath);
+      File file = new File(absoluteFolderPath);
       File[] children = file.listFiles();
       List<String> childList = new ArrayList<String>();
+      if (!folderPath.endsWith("/") && !folderPath.endsWith("\\")) {
+        folderPath += getUsedSeparator(folderPath);
+      }
       for (File fileChild : children) {
-        childList.add(fileChild.getAbsolutePath());
+        childList.add(folderPath + fileChild.getName());
       }
       return childList;
     } catch (Exception exception) {
@@ -104,12 +146,30 @@ public class LocalGadgetImporter extends LocalImporter {
   }
 
   @Override
-  public boolean isFile(String resourcePath) throws IOException {
-    return !resourcePath.endsWith("/") && !resourcePath.endsWith("\\");
+  public boolean isFile(String resourcePath) {
+    Boolean isFile = null;
+    try {
+      File file = new File(getRealPath(resourcePath));
+      if (file.exists()) {
+        isFile = file.isFile();
+      }
+    } catch (Exception exception) {
+      // Nothing to do, the file doesn't exist or may be the path has
+      // another format
+    }
+    if (isFile == null) {
+      return !resourcePath.endsWith("/") && !resourcePath.endsWith("\\");
+    } else {
+      return isFile;
+    }
   }
 
   @Override
   public String getMimeType(String fileName) {
-    return URLConnection.guessContentTypeFromName(fileName);
+    String mimeType = URLConnection.guessContentTypeFromName(fileName);
+    if (mimeType == null) {
+      mimeType = container.getPortalContext().getMimeType(fileName);
+    }
+    return mimeType;
   }
 }
