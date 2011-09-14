9cbc97b7897eaddb59d5e06f840b7e475c224a1a
Tung Minh Vu <tung.vuminh@exoplatform.com>
2011-09-14 03:13:24 +0000

    INTRA-278: Gadgets Sanitization

--- exo-gadget-pack/gadget-pack-services/src/test/java/org/exoplatform/platform/gadget/services/FriendSuggestion/FriendsuggestionWsTest.java
+++
@@ -1,9 +0,0 @@
-package org.exoplatform.platform.gadget.services.FriendSuggestion;
-
-import org.exoplatform.platform.gadget.services.test.GadgetWsTestcase;
-
-public class FriendsuggestionWsTest extends GadgetWsTestcase {
-	//TODO: setup social environment to get the services up
-	public void testGetSuggestedFriends() {
-	}
-}
