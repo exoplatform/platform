8916135ae8f5c6994e507ed5c065452aaa934b5f
Philippe  Aristote <philippe.aristote@exoplatform.com>
2011-10-20 04:38:41 +0000

    PLF-2271 remove Mail bookmark; add My Documents bookmark; fix typo in AddressBook bookmark

--- samples/gadgets-sample/service/src/main/java/org/exoplatform/platform/gadget/services/Bookmark/BookmarkRestService.java
+++ samples/gadgets-sample/service/src/main/java/org/exoplatform/platform/gadget/services/Bookmark/BookmarkRestService.java
@@ -75,7 +75,7 @@ public class BookmarkRestService implements ResourceContainer {
 			if(!userPrivateNode.hasNode("Bookmarks")){
 				Node bookmarksNode = userPrivateNode.addNode("Bookmarks");
 				userPrivateNode.save();
-				String default_bookmarks="[{\"name\":\"Mail\", \"link\":\"/portal/intranet/mail\"},{\"name\":\"Calendar\", \"link\":\"/portal/intranet/calendar\"},{\"name\":\"AdressBook\", \"link\":\"/portal/intranet/contact\"}]";
+				String default_bookmarks="[{\"name\":\"My Documents\", \"link\":\"/portal/intranet/documents\"},{\"name\":\"Calendar\", \"link\":\"/portal/intranet/calendar\"},{\"name\":\"AddressBook\", \"link\":\"/portal/intranet/contact\"}]";
 				bookmarksNode.setProperty("exo:bookmarkService_bookmarks", default_bookmarks);
 				bookmarksNode.save();
 			}
