30a4d45a59bca80e92df98a31b4bbdd67359f31e
Quang Hong Nguyen <quang.nguyenhong@exoplatform.com>
2011-11-04 03:52:38 +0000

    PLF-2384:IllegalStateException when creating a new space

--- component/common/src/main/java/org/exoplatform/platform/common/space/listeners/CustomizeSpaceHomePageListener.java
+++ component/common/src/main/java/org/exoplatform/platform/common/space/listeners/CustomizeSpaceHomePageListener.java
@@ -1,5 +1,7 @@
 package org.exoplatform.platform.common.space.listeners;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.exoplatform.commons.utils.ExoProperties;
 import org.exoplatform.container.xml.InitParams;
 import org.exoplatform.platform.common.space.SpaceCustomizationService;
@@ -15,6 +17,7 @@ public class CustomizeSpaceHomePageListener extends SpaceListenerPlugin {
 
   private ExoProperties welcomeSCVCustomPreferences = null;
   private SpaceCustomizationService spaceCustomizationService = null;
+  private Map<String, Boolean> spaceIds = new HashMap<String, Boolean>();
 
   public CustomizeSpaceHomePageListener(SpaceCustomizationService spaceCustomizationService_, InitParams params) {
     this.spaceCustomizationService = spaceCustomizationService_;
@@ -24,17 +27,33 @@ public class CustomizeSpaceHomePageListener extends SpaceListenerPlugin {
   @Override
   public void spaceCreated(SpaceLifeCycleEvent spaceLifeCycleEvent) {
 
-    String spacePrettyName = spaceLifeCycleEvent.getSpace().getPrettyName();
-    String spaceGroupId = spaceLifeCycleEvent.getSpace().getGroupId();
-
-    spaceCustomizationService.createSpaceHomePage(spacePrettyName, spaceGroupId, welcomeSCVCustomPreferences);
+    spaceIds.put(spaceLifeCycleEvent.getSpace().getGroupId(), true);
   }
 
   @Override
   public void applicationActivated(SpaceLifeCycleEvent event) {}
 
   @Override
-  public void applicationAdded(SpaceLifeCycleEvent spaceLifeCycleEvent) {}
+  public void applicationAdded(SpaceLifeCycleEvent spaceLifeCycleEvent) {
+	    // Workaround : Unfortunately the 'spaceCreated' listener is called
+	    // before the creation of space's pages&navigations, so the Home page
+	    // isn't accessible and couldn't be modified there.
+	    // To be sure that pages are created when calling
+	    // 'spaceCustomizationService.createSpaceHomePage', we have added this
+	    // instruction in 'applicationAdded'.
+	    // To be sure that this will be called once, when space is created, we
+	    // used 'spaceIds' map
+	
+	    String spaceGroupId = spaceLifeCycleEvent.getSpace().getGroupId();
+	    Boolean spaceCreated = spaceIds.get(spaceGroupId);
+	    if (spaceCreated == null || !spaceCreated) {
+	      return;
+	    }
+	
+	    spaceIds.put(spaceGroupId, false);
+	    String spacePrettyName = spaceLifeCycleEvent.getSpace().getPrettyName();
+	    spaceCustomizationService.createSpaceHomePage(spacePrettyName, spaceGroupId, welcomeSCVCustomPreferences);
+	  }
 
   @Override
   public void applicationDeactivated(SpaceLifeCycleEvent event) {}
