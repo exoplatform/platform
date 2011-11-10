a13d102f691a21a9d2b33d6be068f2c5f12d6af8
May Thi Dang <maydt@exoplatform.com>
2011-11-10 10:30:26 +0000

    PLF-2272:Favorites Documents : add tab for My Documents

--- samples/gadgets-sample/service/src/main/java/org/exoplatform/platform/gadget/services/favorite/FavoriteRESTService.java
+++ samples/gadgets-sample/service/src/main/java/org/exoplatform/platform/gadget/services/favorite/FavoriteRESTService.java
@@ -1,6 +1,5 @@
 package org.exoplatform.platform.gadget.services.favorite;
 
-
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -11,6 +10,7 @@ import java.util.List;
 
 import javax.jcr.ItemNotFoundException;
 import javax.jcr.Node;
+import javax.jcr.NodeIterator;
 import javax.jcr.RepositoryException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -21,16 +21,13 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.exoplatform.ecm.utils.comparator.PropertyValueComparator;
-import org.exoplatform.services.cms.documents.FavoriteService;
-import org.exoplatform.services.cms.drives.DriveData;
-import org.exoplatform.services.cms.drives.ManageDriveService;
-import org.exoplatform.services.jcr.RepositoryService;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
 import org.exoplatform.services.rest.resource.ResourceContainer;
-import org.exoplatform.container.ExoContainer;
 import org.exoplatform.container.ExoContainerContext;
-import org.exoplatform.ecm.webui.utils.Utils;
+import org.exoplatform.services.jcr.ext.hierarchy.NodeHierarchyCreator;
+import org.exoplatform.services.jcr.ext.app.SessionProviderService;
+import org.exoplatform.services.cms.link.LinkManager;
 
 /**
  * @author lamphan AUG 01, 2010
@@ -38,9 +35,8 @@ import org.exoplatform.ecm.webui.utils.Utils;
 
 @Path("/plf/favorite/")
 public class FavoriteRESTService implements ResourceContainer {
-  private final FavoriteService favoriteService;
 
-  private ManageDriveService   manageDriveService;
+  //private ManageDriveService   manageDriveService;
 
   private static final String DATE_MODIFIED   = "exo:dateModified";
 
@@ -54,26 +50,32 @@ public class FavoriteRESTService implements ResourceContainer {
   /** The Constant IF_MODIFIED_SINCE_DATE_FORMAT. */
   private static final String IF_MODIFIED_SINCE_DATE_FORMAT = "EEE, dd MMM yyyy HH:mm:ss z";
   
-  private static final Log LOG = ExoLogger.getLogger(FavoriteRESTService.class
-      .getName());
+  private static final Log LOG = ExoLogger.getLogger(FavoriteRESTService.class.getName());
+  
+  final static public String[] SPECIFIC_FOLDERS = { "exo:musicFolder", "exo:videoFolder", "exo:pictureFolder", "exo:documentFolder", "exo:searchFolder" };
 
-  public FavoriteRESTService(FavoriteService favoriteService, ManageDriveService manageDriveService) {
+ /* public FavoriteRESTService(FavoriteService favoriteService, ManageDriveService manageDriveService) {
     this.favoriteService = favoriteService;
     this.manageDriveService = manageDriveService;
-  }
-
+  }*/
+  
   @GET
-  @Path("/all/{repoName}/{workspaceName}/{userName}")
-  public Response getFavoriteByUser(@PathParam("repoName") String repoName,
-      @PathParam("workspaceName") String wsName,
-      @PathParam("userName") String userName, @QueryParam("showItems") String showItems) throws Exception {
+  @Path("/get-documents/{userName}")
+  public Response getFavoriteByUser(@PathParam("userName") String userName, @QueryParam("showItems") String showItems) throws Exception {
     List<FavoriteNode> listFavorites = new ArrayList<FavoriteNode>();
-    List<DriveData> listDrive = manageDriveService.getAllDrives();
+    List<Node> listNodes = new ArrayList<Node>();
     
     if (showItems == null || showItems.trim().length() == 0) showItems = String.valueOf(NO_PER_PAGE);
     try {
-      List<Node> listNodes = favoriteService.getAllFavoriteNodesByUser(wsName,
-          repoName, userName);
+      NodeHierarchyCreator nodeHierarchyCreator = (NodeHierarchyCreator)ExoContainerContext.getCurrentContainer().getComponentInstanceOfType(NodeHierarchyCreator.class);
+      SessionProviderService sessionProviderService = (SessionProviderService)ExoContainerContext.getCurrentContainer().getComponentInstanceOfType(SessionProviderService.class);
+        
+      Node userNode = nodeHierarchyCreator.getUserNode(sessionProviderService.getSystemSessionProvider(null), userName);
+      Node privateDrive = userNode.getNode("Private");
+      Node publicDrive = userNode.getNode("Public");
+      getListDocument(privateDrive, listNodes);
+      getListDocument(publicDrive, listNodes);
+      
       Collections.sort(listNodes, new PropertyValueComparator(DATE_MODIFIED, PropertyValueComparator.DESCENDING_ORDER));
       FavoriteNode favoriteNode;
       
@@ -82,10 +84,9 @@ public class FavoriteRESTService implements ResourceContainer {
         favoriteNode.setName(favorite.getName());
         favoriteNode.setTitle(getTitle(favorite));
         favoriteNode.setDateAddFavorite(getDateFormat(favorite.getProperty(DATE_MODIFIED).getDate()));
-        favoriteNode.setDriveName(getDriveName(listDrive, favorite));
-        favoriteNode.setPath(favorite.getPath());
-        favoriteNode.setLinkImage("Icon16x16 default16x16Icon" + Utils.getNodeTypeIcon(favorite, "16x16Icon"));
-        favoriteNode.setFullLink(createFullLink(favorite));
+        favoriteNode.setNodePath(createFullLink(userNode, favorite));
+        String linkImage = "Icon16x16 default16x16Icon" + getNodeTypeIcon(favorite, "16x16Icon");
+        favoriteNode.setLinkImage(linkImage);
 
         if (favoriteNode != null) {
           if (listFavorites.size() < Integer.valueOf(showItems))
@@ -105,24 +106,133 @@ public class FavoriteRESTService implements ResourceContainer {
     listResultNode.setListFavorite(listFavorites);
 
     DateFormat dateFormat = new SimpleDateFormat(IF_MODIFIED_SINCE_DATE_FORMAT);
-    return Response.ok(listResultNode, new MediaType("application", "json"))
-                   .header(LAST_MODIFIED_PROPERTY, dateFormat.format(new Date()))
-                   .build();
+    return Response.ok(listResultNode, new MediaType("application", "json")).header(LAST_MODIFIED_PROPERTY, dateFormat.format(new Date())).build();
+  }
+
+  @GET
+  @Path("/all/{repoName}/{workspaceName}/{userName}")
+  public Response getFavoriteByUser(@PathParam("repoName") String repoName,
+      @PathParam("workspaceName") String wsName,
+      @PathParam("userName") String userName, @QueryParam("showItems") String showItems) throws Exception {
+    List<FavoriteNode> listFavorites = new ArrayList<FavoriteNode>();
+    
+    if (showItems == null || showItems.trim().length() == 0) showItems = String.valueOf(NO_PER_PAGE);
+    try {
+      NodeHierarchyCreator nodeHierarchyCreator = (NodeHierarchyCreator)ExoContainerContext.getCurrentContainer().getComponentInstanceOfType(NodeHierarchyCreator.class);
+      SessionProviderService sessionProviderService = (SessionProviderService)ExoContainerContext.getCurrentContainer().getComponentInstanceOfType(SessionProviderService.class);
+        
+      Node userNode = nodeHierarchyCreator.getUserNode(sessionProviderService.getSystemSessionProvider(null), userName);
+      
+      List<Node> listNodes = getAllFavoritesByUser(wsName, repoName, userNode);
+      Collections.sort(listNodes, new PropertyValueComparator(DATE_MODIFIED, PropertyValueComparator.DESCENDING_ORDER));
+      FavoriteNode favoriteNode;
+      
+      for (Node favorite : listNodes) {
+        favoriteNode = new FavoriteNode();
+        favoriteNode.setName(favorite.getName());
+        favoriteNode.setTitle(getTitle(favorite));
+        favoriteNode.setDateAddFavorite(getDateFormat(favorite.getProperty(DATE_MODIFIED).getDate()));
+        favoriteNode.setNodePath(createFullLink(userNode, favorite));
+        String linkImage = "Icon16x16 default16x16Icon" + getNodeTypeIcon(favorite, "16x16Icon");
+        favoriteNode.setLinkImage(linkImage);
+        
+        if (favoriteNode != null) {
+          if (listFavorites.size() < Integer.valueOf(showItems))
+            listFavorites.add(favoriteNode);
+        }
+      }
+      
+    } catch (ItemNotFoundException e) {
+      LOG.error(e);
+    } catch (RepositoryException e) {
+      LOG.error(e);
+    } catch (Exception e) {
+      LOG.error(e);
+      return Response.serverError().build();
+    }
+    ListResultNode listResultNode = new ListResultNode();
+    listResultNode.setListFavorite(listFavorites);
+
+    DateFormat dateFormat = new SimpleDateFormat(IF_MODIFIED_SINCE_DATE_FORMAT);
+    return Response.ok(listResultNode, new MediaType("application", "json")).header(LAST_MODIFIED_PROPERTY, dateFormat.format(new Date())).build();
   }
   
+  public static String getNodeTypeIcon(Node node, String appended) throws RepositoryException {
+	    StringBuilder str = new StringBuilder();
+	    
+	    if (node == null) return "";
+	    
+	    String nodeType = node.getPrimaryNodeType().getName();
+	    
+	    if (node.isNodeType("exo:symlink")) {
+		    LinkManager linkManager = (LinkManager)ExoContainerContext.getCurrentContainer().getComponentInstanceOfType(LinkManager.class);
+	      try {
+	        nodeType = node.getProperty("exo:primaryType").getString();
+	        node = linkManager.getTarget(node);
+	        if (node == null)
+	          return "";
+	      } catch (Exception e) {
+	        return "";
+	      }
+	    }
+	    
+	    nodeType = nodeType.replace(':', '_') + appended;
+	    str.append(nodeType);
+	    str.append(" ");
+	    str.append("default16x16Icon");
+	    if (node.isNodeType("nt:file")) {
+	      if (node.hasNode("jcr:content")) {
+	        Node jcrContentNode = node.getNode("jcr:content");
+	        str.append(' ').append(
+	            jcrContentNode.getProperty("jcr:mimeType").getString().replaceAll(
+	                "/|\\.", "_")).append(appended);
+	      }
+	    }
+	    return str.toString();
+	  }
   
-  public String createFullLink(Node node) throws Exception{
+  public void getListDocument(Node node, List<Node> listNodes) throws RepositoryException {
+	    LinkManager linkManager = (LinkManager)ExoContainerContext.getCurrentContainer().getComponentInstanceOfType(LinkManager.class);
+	    String primaryType = node.getProperty("jcr:primaryType").getString();
+	    if (primaryType.contains("nt:file") || linkManager.isLink(node)) {
+	    		listNodes.add(node);
+	        return;
+	        }
+	    else
+	    {
+	    	if (node.hasNodes()){
+	    	        NodeIterator childNodes = node.getNodes();
+	                while (childNodes.hasNext()) {
+	                        Node childNode = childNodes.nextNode();
+	                        getListDocument(childNode, listNodes);            
+	                }
+	    	}
+	    }
+	}
+  
+  public List<Node> getAllFavoritesByUser(String workspace, String repository, Node userNode) throws Exception {
+	    List<Node> ret = new ArrayList<Node>();
+	    LinkManager linkManager = (LinkManager)ExoContainerContext.getCurrentContainer().getComponentInstanceOfType(LinkManager.class);
+	    NodeHierarchyCreator nodeHierarchyCreator = (NodeHierarchyCreator)ExoContainerContext.getCurrentContainer().getComponentInstanceOfType(NodeHierarchyCreator.class);
+	    String favoritePath = nodeHierarchyCreator.getJcrPath("userPrivateFavorites");
+	    Node favoriteFolder= userNode.getNode(favoritePath);
+	      
+	    NodeIterator nodeIter = favoriteFolder.getNodes();
+	    while (nodeIter.hasNext()) {
+	      Node childNode = nodeIter.nextNode();
+	      String primaryType = childNode.getProperty("jcr:primaryType").getString();
+	      if (linkManager.isLink(childNode) || primaryType.contains("nt:file")) {
+	        ret.add(childNode);
+	      }
+	    }
+	    return ret;
+	  }
+  
+  public String createFullLink(Node userNode, Node document) throws Exception{
 	  
-	    String drivename = node.getSession().getWorkspace().getName();
-	    ExoContainer container = ExoContainerContext.getCurrentContainer();
-	    RepositoryService repoService = (RepositoryService)container.getComponentInstanceOfType(RepositoryService.class);
-	    String repository = repoService.getCurrentRepository().getConfiguration().getName();
-	    String nodePath = node.getPath();
-	    String NodeUrl = new StringBuilder().append(repository)
-	    					.append("/")
-	                                        .append(drivename)
-	                                        .append(nodePath)
-	                                        .toString();
+    	String userPath = userNode.getPath();
+      	String favoritePath = document.getPath();
+      	String NodeUrl = favoritePath.substring(userPath.length()+1);
 	    return NodeUrl;
   }
 
@@ -137,7 +247,7 @@ public class FavoriteRESTService implements ResourceContainer {
     return String.valueOf(date.getTimeInMillis());
   }
 
-
+/*
   private String getDriveName(List<DriveData> listDrive, Node node) throws RepositoryException{
     String driveName = "";
     for (DriveData drive : listDrive) {
@@ -148,7 +258,7 @@ public class FavoriteRESTService implements ResourceContainer {
       }
     }
     return driveName;
-  }
+  }*/
 
   public class ListResultNode {
 
@@ -168,11 +278,8 @@ public class FavoriteRESTService implements ResourceContainer {
     private String name;
     private String nodePath;
     private String dateAddFavorite;
-    private String driveName;
     private String title;
     private String linkImage;
-    private String fullLink;
-    
     
     public String getName() {
       return name;
@@ -182,11 +289,11 @@ public class FavoriteRESTService implements ResourceContainer {
       this.name = name;
     }
 
-    public void setPath(String path) {
-      this.nodePath = path;
+    public void setNodePath(String nodePath) {
+      this.nodePath = nodePath;
     }
 
-    public String getPath() {
+    public String getNodePath() {
       return nodePath;
     }
 
@@ -197,14 +304,14 @@ public class FavoriteRESTService implements ResourceContainer {
     public String getDateAddFavorite() {
       return dateAddFavorite;
     }
-
+/*
     public void setDriveName(String driveName) {
       this.driveName = driveName;
     }
 
     public String getDriveName() {
       return driveName;
-    }
+    }*/
 
     public void setTitle(String title) {
       this.title = title;
@@ -212,14 +319,6 @@ public class FavoriteRESTService implements ResourceContainer {
 
     public String getTitle() {
       return title;
-    }
-
-    public void setFullLink(String fullLink) {
-      this.fullLink = fullLink;
-    }
-
-    public String getFullLink() {
-      return fullLink;
     }
     
     public void setLinkImage(String linkImage) {
@@ -231,4 +330,3 @@ public class FavoriteRESTService implements ResourceContainer {
     }
   }
   }
-
