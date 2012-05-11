f476f6e76aa508854e9bb96b204a1fd6dd6bbbb5
Clement Balay <cbalay@exoplatform.com>
2012-05-11 15:30:50 +0100

    PLF-3227 - new tests

--- component/common/src/main/java/org/exoplatform/platform/common/admin/TermsAndConditionsViewServlet.java
+++ component/common/src/main/java/org/exoplatform/platform/common/admin/TermsAndConditionsViewServlet.java
@@ -21,8 +21,10 @@ public class TermsAndConditionsViewServlet extends HttpServlet {
   private static Log logger = ExoLogger.getLogger(TermsAndConditionsFilter.class);
   
   protected final static String INITIAL_URI_PARAM = "tacURI";
-  private final static String TC_JSP_RESOURCE = "/WEB-INF/jsp/admin/termsandconditions.jsp";
+  private final static String TC_JSP_RESOURCE = "/jsp/termsandconditions.jsp";
   private final static String INITIAL_URI_ATTRIBUTE = "org.gatein.portal.login.initial_uri";
+  
+  private final static String TC_JSP_RESOURCE_TEST0 = "/WEB-INF/jsp/admin/termsandconditions.jsp";
 
   private final static String TC_JSP_RESOURCE_TEST1 = "/WEB-INF/jsp/admin/test1-termsandconditions.jsp";
   private final static String TC_JSP_RESOURCE_TEST2 = "/WEB-INF/jsp/admin/test2-termsandconditions.jsp";
@@ -30,6 +32,15 @@ public class TermsAndConditionsViewServlet extends HttpServlet {
   private final static String TC_JSP_RESOURCE_TEST4 = "/WEB-INF/jsp/admin/test4-termsandconditions.jsp";
   private final static String TC_JSP_RESOURCE_TEST5 = "/WEB-INF/jsp/admin/test5-termsandconditions.jsp";
   private final static String TC_JSP_RESOURCE_TEST6 = "/WEB-INF/jsp/admin/test6-termsandconditions.jsp";
+
+  private final static String TC_JSP_RESOURCE_TEST7 = "/jsp/admin/termsandconditions.jsp";
+  
+  private final static String TC_JSP_RESOURCE_TEST11 = "/jsp/admin/test1-termsandconditions.jsp";
+  private final static String TC_JSP_RESOURCE_TEST12 = "/jsp/admin/test2-termsandconditions.jsp";
+  private final static String TC_JSP_RESOURCE_TEST13 = "/jsp/admin/test3-termsandconditions.jsp";
+  private final static String TC_JSP_RESOURCE_TEST14 = "/jsp/admin/test4-termsandconditions.jsp";
+  private final static String TC_JSP_RESOURCE_TEST15 = "/jsp/admin/test5-termsandconditions.jsp";
+  private final static String TC_JSP_RESOURCE_TEST16 = "/jsp/admin/test6-termsandconditions.jsp";
 
   @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -40,7 +51,7 @@ public class TermsAndConditionsViewServlet extends HttpServlet {
     int testNumber = -1;
     try {
       testNumber = Integer.valueOf(sTestNumber);
-      if(testNumber < 1 || testNumber > 6) {
+      if(testNumber < 0 || testNumber > 6) {
         testNumber = -1;
       }
     }
@@ -55,12 +66,20 @@ public class TermsAndConditionsViewServlet extends HttpServlet {
       String jspResource = TC_JSP_RESOURCE;
       if(testNumber != -1) {
         switch(testNumber) {
+          case 0: jspResource = TC_JSP_RESOURCE_TEST0;break;
           case 1: jspResource = TC_JSP_RESOURCE_TEST1;break;
           case 2: jspResource = TC_JSP_RESOURCE_TEST2;break;
           case 3: jspResource = TC_JSP_RESOURCE_TEST3;break;
           case 4: jspResource = TC_JSP_RESOURCE_TEST4;break;
           case 5: jspResource = TC_JSP_RESOURCE_TEST5;break;
           case 6: jspResource = TC_JSP_RESOURCE_TEST6;break;
+          case 7: jspResource = TC_JSP_RESOURCE_TEST7;break;
+          case 11: jspResource = TC_JSP_RESOURCE_TEST11;break;
+          case 12: jspResource = TC_JSP_RESOURCE_TEST12;break;
+          case 13: jspResource = TC_JSP_RESOURCE_TEST13;break;
+          case 14: jspResource = TC_JSP_RESOURCE_TEST14;break;
+          case 15: jspResource = TC_JSP_RESOURCE_TEST15;break;
+          case 16: jspResource = TC_JSP_RESOURCE_TEST16;break;
           default: break;
         }
       }
