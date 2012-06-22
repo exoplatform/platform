f3bbb30777e3d4d8f26282b7d04a117f6da1f62d
Omar BOURAS <obouras@exoplatform.com>
2012-06-22 12:11:55 +0100

    PLF-3310: Introduce CATALINA_PID in setenv.sh

--- server/tomcat/patch/src/main/resources/bin/setenv.sh
+++ server/tomcat/patch/src/main/resources/bin/setenv.sh
@@ -19,6 +19,8 @@
 #
 
 
+# Catalina PID
+export CATALINA_PID=${CATALINA_HOME}/temp/catalina.pid
 # Sets some variables
 LOG_OPTS="-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog"
 SECURITY_OPTS="-Djava.security.auth.login.config=$CATALINA_HOME/conf/jaas.conf"
