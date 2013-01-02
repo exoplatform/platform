e8cd6721b6894fcf6c6a842a77e5994359002e6a
kmenzli <kmenzli@exoplatform.com>
2013-01-02 13:02:23 +0100

    PLF-3837 : Upgrade to use GateIn 3.5.0.Final [ Fix SpaceSearch script on SpaceNavigationPortlet ]

--- extension/portlets/platformNavigation/src/main/webapp/javascript/eXo/platform/navigation/UISpaceNavigationPortlet/UISpaceNavigation.js
+++ extension/portlets/platformNavigation/src/main/webapp/javascript/eXo/platform/navigation/UISpaceNavigationPortlet/UISpaceNavigation.js
@@ -1,106 +1,107 @@
 (function($) {
-var UISpaceNavigation = {
-    init: function(uicomponentId, mySpaceRestUrl, defaultValueForTextSearch, noSpace, selectSpaceAction) {
-        var me = UISpaceNavigation;
-        me.mySpaceRestUrl = mySpaceRestUrl;
-        me.lastSearchKeyword = "";
-        me.defaultValueForTextSearch = defaultValueForTextSearch;
-        me.selectSpaceAction = selectSpaceAction;
-        me.noSpace=noSpace;
-        var me = eXo.navigation.UISpaceNavigation;
-        var navigationSpaceSearch = document.getElementById(uicomponentId);
-        var textField = $(navigationSpaceSearch).find("input.searchText")[0];
-        textField.value = defaultValueForTextSearch;
+    var UISpaceNavigation = {
+        init: function(uicomponentId, mySpaceRestUrl, defaultValueForTextSearch,noSpace, selectSpaceAction) {
+            var me = UISpaceNavigation;
+            me.mySpaceRestUrl = mySpaceRestUrl;
+            me.lastSearchKeyword = "";
+            me.defaultValueForTextSearch = defaultValueForTextSearch;
+            me.selectSpaceAction = selectSpaceAction;
+            me.noSpace=noSpace;
+            var me = UISpaceNavigation;
+            var navigationSpaceSearch = document.getElementById(uicomponentId);
+            var textField = $(navigationSpaceSearch).find("input.searchText")[0];
+            textField.value = defaultValueForTextSearch;
+            textField.onkeydown = function() {
+                me.onTextSearchChange(uicomponentId);
+            };
 
-        textField.onkeydown = function() {
-            me.onTextSearchChange(uicomponentId);
-        };
+            textField.onkeypress = function() {
+                me.onTextSearchChange(uicomponentId);
+            };
 
-        textField.onkeypress = function() {
-            me.onTextSearchChange(uicomponentId);
-        };
+            textField.onkeyup = function() {
+                me.onTextSearchChange(uicomponentId);
+            };
 
-        textField.onkeyup = function() {
-            me.onTextSearchChange(uicomponentId);
-        };
+            textField.onfocus = function() {
+                if (textField.value == me.defaultValueForTextSearch) {
+                    textField.value = "";
+                }
+                textField.className="searchText Focus"
+            };
 
-        textField.onfocus = function() {
-            if (textField.value == me.defaultValueForTextSearch) {
-                textField.value = "";
+            textField.onclick = function(event) {
+
+                if (event.stopPropagation){
+                    event.stopPropagation();
+                } else if(window.event){
+                    window.event.cancelBubble=true;
+                }
+            };
+
+            // When textField lost focus
+            textField.onblur = function() {
+                if (textField.value == "") {
+                    textField.value = me.defaultValueForTextSearch;
+                    textField.className="searchText LostFocus";
+                }
+            };
+
+
+        },
+        requestData: function(keyword, uicomponentId) {
+            var me = UISpaceNavigation;
+
+            $.ajax({
+                async : false,
+                url : me.mySpaceRestUrl + "?keyword=" + keyword,
+                type : 'GET',
+                data : '',
+                success : function(data) {
+                    me.render(data, uicomponentId);
+                }
+            });
+        },
+        render: function(dataList, uicomponentId) {
+            var me = UISpaceNavigation;
+            me.dataList = dataList;
+
+            var navigationSpaceSearch = document.getElementById(uicomponentId);
+            var spacesListREsult = $(navigationSpaceSearch).find('ul.spaceNavigation')[0];
+            //var spaces = dataList.jsonList;
+            var spaces = dataList;
+            var groupSpaces = '';
+            for (i = 0; i < spaces.length; i++) {
+                var spaceId = spaces[i].id;
+                var spaceUrl = window.location.protocol + "//" + window.location.host + "/" + spaces[i].url;
+                var name = spaces[i].displayName;
+                var imageUrl=spaces[i].avatarUrl;
+                if (imageUrl == null) {
+                    imageUrl = "/social-resources/skin/ShareImages/SpaceImages/SpaceLogoDefault_61x61.gif";
+                }
+                var spaceDiv = "<li class='spaceItem'>"+"<a class='spaceIcon'"
+                    + "' href='" + spaceUrl + "'><img src='"+imageUrl+"'/>"
+                    + name + "</a></li><br/>";
+                groupSpaces += spaceDiv;
             }
-            textField.className="searchText Focus"
-        };
-
-        textField.onclick = function(event) {
-
-            if (event.stopPropagation){
-                event.stopPropagation();
-            } else if(window.event){
-                window.event.cancelBubble=true;
+            if(groupSpaces!=''){
+                spacesListREsult.innerHTML = groupSpaces;
+            }else{
+                spacesListREsult.innerHTML= me.noSpace  ;
             }
-        };
+        },
+        onTextSearchChange: function(uicomponentId) {
+            var me = UISpaceNavigation;
+            var navigationSpaceSearch = document.getElementById(uicomponentId);
+            var textSearch = $(navigationSpaceSearch).find("input.searchText")[0].value;
 
-        // When textField lost focus
-        textField.onblur = function() {
-            if (textField.value == "") {
-                textField.value = me.defaultValueForTextSearch;
-                textField.className="searchText LostFocus";
+            if (textSearch != me.lastSearchKeyword) {
+                me.lastSearchKeyword = textSearch;
+                me.requestData(textSearch, uicomponentId);
+
             }
-        };
-    },
-    requestData:  function(keyword, uicomponentId) {
-        var me = UISpaceNavigation;
-
-        $.ajax({
-        async : false,
-        url : me.mySpaceRestUrl + "?keyword=" + keyword,
-        type : 'GET',
-        data : '',
-        success : function(data) {
-            me.render(data, uicomponentId);
         }
-        })
-    },
-    render: function(dataList, uicomponentId) {
-        var me = UISpaceNavigation;
-        me.dataList = dataList;
+    };
 
-        var navigationSpaceSearch = document.getElementById(uicomponentId);
-        var spacesListREsult = $(navigationSpaceSearch).find('ul.spaceNavigation')[0];
-        //var spaces = dataList.jsonList;
-        var spaces = dataList;
-        var groupSpaces = '';
-        for (i = 0; i < spaces.length; i++) {
-            var spaceId = spaces[i].id;
-            var spaceUrl = window.location.protocol + "//" + window.location.host + "/" + spaces[i].url;
-            var name = spaces[i].displayName;
-            var imageUrl=spaces[i].avatarUrl;
-            if (imageUrl == null) {
-                imageUrl = "/social-resources/skin/ShareImages/SpaceImages/SpaceLogoDefault_61x61.gif";
-            }
-            var spaceDiv = "<li class='spaceItem'>"+"<a class='spaceIcon'"
-                + "' href='" + spaceUrl + "'><img src='"+imageUrl+"'/>"
-                + name + "</a></li><br/>";
-            groupSpaces += spaceDiv;
-        }
-        if(groupSpaces!=''){
-            spacesListREsult.innerHTML = groupSpaces;
-        }else{
-            spacesListREsult.innerHTML= me.noSpace  ;
-        }
-    },
-    onTextSearchChange: function(uicomponentId) {
-        var me = UISpaceNavigation;
-        var navigationSpaceSearch = document.getElementById(uicomponentId);
-        var textSearch = $(navigationSpaceSearch).find("input.searchText")[0].value;
-
-        if (textSearch != me.lastSearchKeyword) {
-            me.lastSearchKeyword = textSearch;
-            me.requestData(textSearch, uicomponentId);
-
-        }
-    }
-};
-
-return UISpaceNavigation;
+    return UISpaceNavigation;
 })($);
