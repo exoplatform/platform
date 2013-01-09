b0ce7adb6193c521d2674701781699459b7a1627
rtouzi <rtouzi@exoplatform.com>
2013-01-09 14:49:01 +0100

    PLF-4039 :[Intranet-Homepage][Who's on line Portlet] integrate I18N

--- extension/portlets/homepagePortlets/src/main/java/org/exoplatform/platform/portlet/juzu/whoisonline/WhoIsOnLineController.java
+++ extension/portlets/homepagePortlets/src/main/java/org/exoplatform/platform/portlet/juzu/whoisonline/WhoIsOnLineController.java
@@ -34,17 +34,23 @@ public class WhoIsOnLineController {
   public Response.Render index() {
       Locale locale = RequestContext.getCurrentInstance().getLocale();
       ResourceBundle rs = ResourceBundle.getBundle("whoisonline/whoisonline", locale);
+      String connectLabel = rs.getString("connect.label");
+      String messageLabel = rs.getString("message.label");
       String headerLabel = rs.getString("header.label");
       String userId= RequestContext.getCurrentInstance().getRemoteUser();
       List<User> friends = whoIsOnline.getFriends(userId);
-    return index.with().set("users", friends).set("headerLabel",headerLabel).render();
+    return index.with().set("users", friends).set("headerLabel",headerLabel).set("messageLabel",messageLabel).set("connectLabel",connectLabel).render();
   }
 
   @Ajax
   @Resource
   public Response.Render users() {
       String userId= RequestContext.getCurrentInstance().getRemoteUser();
+      Locale locale = RequestContext.getCurrentInstance().getLocale();
+      ResourceBundle rs = ResourceBundle.getBundle("whoisonline/whoisonline", locale);
+      String connectLabel = rs.getString("connect.label");
+      String messageLabel = rs.getString("message.label");
       List<User> friends = whoIsOnline.getFriends(userId);
-    return users.with().set("users", friends).render();
+    return users.with().set("users", friends).set("messageLabel",messageLabel).set("connectLabel",connectLabel).render();
   }
 }
