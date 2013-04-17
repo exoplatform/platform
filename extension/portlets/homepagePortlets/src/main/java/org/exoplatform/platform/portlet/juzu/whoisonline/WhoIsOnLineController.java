2887b4de88358c4d483870841bd9b886d26b6aee
rtouzi <rtouzi@exoplatform.com>
2013-04-17 18:17:12 +0100

    PLF-4927 :"Who is online" portlet does not display.

--- extension/portlets/homepagePortlets/src/main/java/org/exoplatform/platform/portlet/juzu/whoisonline/WhoIsOnLineController.java
+++ extension/portlets/homepagePortlets/src/main/java/org/exoplatform/platform/portlet/juzu/whoisonline/WhoIsOnLineController.java
@@ -4,15 +4,15 @@ import juzu.Path;
 import juzu.Resource;
 import juzu.Response;
 import juzu.View;
-import org.exoplatform.commons.juzu.ajax.Ajax;
 import juzu.template.Template;
+import org.exoplatform.commons.juzu.ajax.Ajax;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
 import org.exoplatform.web.application.RequestContext;
 
 import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
-import java.util.ResourceBundle;
 
 /**
  * @author <a href="rtouzi@exoplatform.com">rtouzi</a>
@@ -20,36 +20,53 @@ import java.util.ResourceBundle;
  */
 public class WhoIsOnLineController {
 
-    private static  Locale locale = null;
-    private static ResourceBundle rs=null;
-    private  List<User> friends = new ArrayList<User>();
-  @Inject
-  WhoIsOnline whoIsOnline;
+    private static Log log = ExoLogger.getLogger(WhoIsOnLineController.class);
 
-  @Inject
-  @Path("index.gtmpl")
-  Template index;
+    @Inject
+    WhoIsOnline whoIsOnline;
 
-  @Inject
-  @Path("users.gtmpl")
-  Template users;
+    @Inject
+    @Path("index.gtmpl")
+    Template index;
 
-  @View
-  public Response.Render index() {
-      locale=RequestContext.getCurrentInstance().getLocale();
-      rs = ResourceBundle.getBundle("locale/portlet/whoisonline/whoisonline", locale);
-      String userId= RequestContext.getCurrentInstance().getRemoteUser();
-       friends = whoIsOnline.getFriends(userId);
-      return index.with().set("users", friends).set("headerLabel",rs.getString("header.label")).set("messageLabel",rs.getString("message.label")).set("connectLabel",rs.getString("connect.label")).ok();
-  }
+    @Inject
+    @Path("users.gtmpl")
+    Template users;
 
-  @Ajax
-  @Resource
-  public Response.Render users() {
-      locale=RequestContext.getCurrentInstance().getLocale();
-      rs = ResourceBundle.getBundle("locale/portlet/whoisonline/whoisonline", locale);
-      String userId= RequestContext.getCurrentInstance().getRemoteUser();
-       friends = whoIsOnline.getFriends(userId);
-      return users.with().set("users", friends).set("messageLabel",rs.getString("message.label")).set("connectLabel",rs.getString("connect.label")).ok();
-  }
+    @View
+    public Response.Render index() {
+        try {
+
+            String userId = RequestContext.getCurrentInstance().getRemoteUser();
+            List<User> friends = whoIsOnline.getFriends(userId);
+            if (friends == null) {
+                friends = new ArrayList<User>();
+                log.info("No  logged user | WhoIsOnLin Portlet will not be displayed");
+            }
+            return index.with().set("users", friends).ok();
+
+        } catch (Exception e) {
+            log.error("Error while rendering WhoIsOnLine Portlet :" + e.getMessage(), e);
+            return index.with().set("users", new ArrayList<User>()).ok();
+        }
+
+    }
+
+    @Ajax
+    @Resource
+    public Response.Render users() {
+        try {
+            String userId = RequestContext.getCurrentInstance().getRemoteUser();
+            List<User> friends = whoIsOnline.getFriends(userId);
+            if (friends == null) {
+                friends = new ArrayList<User>();
+                log.info("No  logged user | WhoIsOnLin Portlet will not be displayed");
+            }
+            return users.with().set("users", friends).ok();
+
+        } catch (Exception e) {
+            log.error("Error while rendering WhoIsOnLine Portlet :" + e.getMessage(), e);
+            return users.with().set("users", new ArrayList<User>()).ok();
+        }
+    }
 }
