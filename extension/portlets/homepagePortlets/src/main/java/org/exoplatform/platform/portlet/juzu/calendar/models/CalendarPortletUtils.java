6f04df1b7cfb4a46bf59e8df53fa4d062cdcf0bc
rtouzi <rtouzi@exoplatform.com>
2013-04-19 18:14:14 +0100

    PLF-4612 :Time of event is shown wrong at Calendar gadget on Home page gadget

--- extension/portlets/homepagePortlets/src/main/java/org/exoplatform/platform/portlet/juzu/calendar/models/CalendarPortletUtils.java
+++ extension/portlets/homepagePortlets/src/main/java/org/exoplatform/platform/portlet/juzu/calendar/models/CalendarPortletUtils.java
@@ -1,5 +1,16 @@
 package org.exoplatform.platform.portlet.juzu.calendar.models;
 
+import org.exoplatform.calendar.service.CalendarService;
+import org.exoplatform.calendar.service.CalendarSetting;
+import org.exoplatform.container.PortalContainer;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+import org.exoplatform.web.application.RequestContext;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * @author <a href="fbradai@exoplatform.com">Fbradai</a>
  * @date 1/10/13
@@ -7,6 +18,8 @@ package org.exoplatform.platform.portlet.juzu.calendar.models;
 public class CalendarPortletUtils {
     public final static String HOME_PAGE_CALENDAR_SETTINGS = "IntranetHomePageCalendarSettings";
     public final static int JOUR_MS = 86399999;
+    private static Log log = ExoLogger.getLogger(CalendarPortletUtils.class);
+    private static ConcurrentHashMap<String, CalendarSetting> calendarSettingsByUserName = new ConcurrentHashMap<String, CalendarSetting>();
 
     public static boolean contains(String[] s, String str) {
         int i = 0;
@@ -20,4 +33,51 @@ public class CalendarPortletUtils {
         }
         return false;
     }
+
+    public static Calendar getInstanceOfCurrentCalendar() {
+        try {
+            CalendarSetting setting = getCurrentUserCalendarSetting();
+            return getCalendarInstanceBySetting(setting);
+        } catch (Exception e) {
+            if (log.isWarnEnabled()) log.warn("Could not get calendar setting!", e);
+            Calendar calendar = Calendar.getInstance();
+            calendar.setLenient(false);
+            return calendar;
+        }
+    }
+
+    public static CalendarSetting getCurrentUserCalendarSetting() {
+
+        try {
+            String user = getCurrentUser();
+            CalendarSetting setting = calendarSettingsByUserName.get(user);
+            if (setting == null) {
+                setting = getCalendarService().getCalendarSetting(user);
+                calendarSettingsByUserName.put(user, setting);
+            }
+            return setting;
+        } catch (Exception e) {
+            log.warn("could not get calendar setting of user", e);
+            return null;
+        }
+
+    }
+
+    static public CalendarService getCalendarService() throws Exception {
+        return (CalendarService) PortalContainer.getInstance().getComponentInstance(CalendarService.class);
+    }
+
+    static public String getCurrentUser() throws Exception {
+        return RequestContext.getCurrentInstance().getRemoteUser();
+    }
+
+    public static Calendar getCalendarInstanceBySetting(final CalendarSetting calendarSetting) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setLenient(false);
+        calendar.setTimeZone(TimeZone.getTimeZone(calendarSetting.getTimeZone()));
+        calendar.setFirstDayOfWeek(Integer.parseInt(calendarSetting.getWeekStartOn()));
+        calendar.setMinimalDaysInFirstWeek(4);
+        return calendar;
+    }
+
 }
