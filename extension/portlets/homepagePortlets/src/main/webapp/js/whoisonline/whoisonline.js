55f902c743440a4d45ba91ceeb6d6e7466ba78fa
Vi Quoc Hanh <hanh.vi@exoplatform.com>
2013-07-08 17:27:43 +0000

    PLF-5236 | Who Is Online portlet took a lot of time when user login.Fix Rule 08.

--- extension/portlets/homepagePortlets/src/main/webapp/js/whoisonline/whoisonline.js
+++ extension/portlets/homepagePortlets/src/main/webapp/js/whoisonline/whoisonline.js
@@ -1,10 +1,13 @@
 (function($) {
+	
+	var glables = null;
 
     return {
         ajaxWhoIsOnLine: function(labels) {
             //
             var labels = labels;
             
+            glables = labels;
             
             // User Profile Popup initialize
               var portal = eXo.social.portal;
@@ -28,4 +31,34 @@
         }
 
     };
+    
+    var refresh = function() {
+    	//
+    	if (glables != null) {
+    		// User Profile Popup initialize
+    		var portal = eXo.social.portal;
+    		var restUrl = 'http://' + window.location.host + portal.context + '/' + portal.rest + '/social/people' + '/getPeopleInfo/{0}.json';
+
+    		var userLinks = $('#onlineList').find('a:[href*="/profile/"]');
+    		$.each(userLinks, function (idx, el) {
+    			var userUrl = $(el).attr('href');
+    			var userId = userUrl.substring(userUrl.lastIndexOf('/') + 1);
+
+    			$(el).userPopup({
+    				restURL: restUrl,
+    				labels: glables,
+    				content: false,
+    				defaultPosition: "left",
+    				keepAlive: true,
+    				maxWidth: "240px"
+    			});
+    		});
+    	} 
+    }
+    
+    // Wait 1/2 second (not realistic of course)
+    // And we should use setInterval with 60 seconds
+    setTimeout(refresh, 500);
+    setInterval(refresh,60000);
+    
 })($);
