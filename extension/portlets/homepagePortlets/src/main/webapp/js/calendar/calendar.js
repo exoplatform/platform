f6d4f63b7a93da57ff5f74faf6c9ae74e0f24bcb
rtouzi <rtouzi@exoplatform.com>
2013-11-25 10:44:50 +0100

    PLF-4903  : [PLF 4] Calendar Gadget - Configure Mode

--- extension/portlets/homepagePortlets/src/main/webapp/js/calendar/calendar.js
+++ extension/portlets/homepagePortlets/src/main/webapp/js/calendar/calendar.js
@@ -7,14 +7,22 @@
                 var calId = $(this).parent().children("input").val();
                 $('div.CalendarPortletContainer').jzLoad(
                         "CalendarPortletController.deleteCalendar()",
-                        {"calendarId":calId});
+                        {"calendarId":calId}, function() {
+
+                        $('*[rel="tooltip"]').tooltip();
+
+                    });
             });
 
             $('.jz').on("click", '.uiIconAdd', function () {
                 var calId = $(this).parent().children("input").val();
                 $('div.CalendarPortletContainer').jzLoad(
                         "CalendarPortletController.addCalendar()",
-                        {"calendarId":calId});
+                        {"calendarId":calId}, function() {
+
+                        $('*[rel="tooltip"]').tooltip();
+
+                    });
             });
 
             $('.jz').on('mouseover', '.calendarItem', function() {
@@ -36,35 +44,55 @@
                 }
                 $('ul.NonDisplayedCalendar').jzLoad(
                         "CalendarPortletController.getSearchResult()",
-                        {"key":seakey});
+                        {"key":seakey}, function() {
+
+                        $('*[rel="tooltip"]').tooltip();
+
+                    });
             });
 
             $('.jz').on('show', 'input.PLFcalendarSearchKey', function () {
                 var seakey = $(this).val();
                 $('ul.NonDisplayedCalendar').jzLoad(
                         "CalendarPortletController.getSearchResult()",
-                        {"key":seakey});
+                        {"key":seakey}, function() {
+
+                        $('*[rel="tooltip"]').tooltip();
+
+                    });
             });
 
             $('.jz').on("keydown", 'input.PLFcalendarSearchKey', function () {
                 var seakey = $(this).val();
                 $('ul.NonDisplayedCalendar').jzLoad(
                         "CalendarPortletController.getSearchResult()",
-                        {"key":seakey});
+                        {"key":seakey}, function() {
+
+                        $('*[rel="tooltip"]').tooltip();
+
+                    });
             });
 
             $('.jz').on("keypress", 'input.PLFcalendarSearchKey', function () {
                 var seakey = $(this).val();
                 $('ul.NonDisplayedCalendar').jzLoad(
                         "CalendarPortletController.getSearchResult()",
-                        {"key":seakey});
+                        {"key":seakey}, function() {
+
+                        $('*[rel="tooltip"]').tooltip();
+
+                    });
             });
 
             $('.jz').on("keyup", 'input.PLFcalendarSearchKey', function () {
                 var seakey = $(this).val();
                 $('ul.NonDisplayedCalendar').jzLoad(
                         "CalendarPortletController.getSearchResult()",
-                        {"key":seakey});
+                        {"key":seakey}, function() {
+
+                        $('*[rel="tooltip"]').tooltip();
+
+                    });
             });
 
             $('.jz').on("focus", 'input.PLFcalendarSearchKey', function () {
@@ -83,37 +111,59 @@
 
             $('.jz').on('click', '.btn', function(){
                 $('div.CalendarPortletContainer').jzLoad(
-                        "CalendarPortletController.calendarHome()");
+                        "CalendarPortletController.calendarHome()", function() {
+
+                        $('*[rel="tooltip"]').tooltip();
+
+                    });
             });
 
             $('.jz').on("click", '.prevDate', function() {
                 $('div.CalendarPortletContainer').jzLoad(
                         "CalendarPortletController.decDate()",
-                        {"nbClick":"1"});
+                        {"nbClick":"1"}, function() {
+
+                        $('*[rel="tooltip"]').tooltip();
+
+                    });
             });
             $('.jz').on("click", '.nextDate', function() {
                 $('div.CalendarPortletContainer').jzLoad(
                         "CalendarPortletController.incDate()",
-                        {"nbClick":"1"});
+                        {"nbClick":"1"}, function() {
+
+                        $('*[rel="tooltip"]').tooltip();
+
+                    });
             });
             $('.jz').on("click", '.settingsLink', function(){
                 $('.CalendarPortletContainer').jzLoad(
-                        "CalendarPortletController.setting()");
+                        "CalendarPortletController.setting()", function() {
+
+                        $('*[rel="tooltip"]').tooltip();
+
+                    });
             });
 
             $('.jz').on('mouseover', '.calendarPortlet', function () {
                 $('.settingsContainerPage').children("a").css("display", "block");
+                $('*[rel="tooltip"]').tooltip();
 
             });
             $('.jz').on('mouseout', '.calendarPortlet', function () {
                 $('.settingsContainerPage').children("a").css("display", "none");
+                $('*[rel="tooltip"]').tooltip();
             });
 
             setTimeout(function(){
-                $('div.CalendarPortletContainer').jzLoad("CalendarPortletController.calendarHome()");
+                $('div.CalendarPortletContainer').jzLoad("CalendarPortletController.calendarHome()", function() {
+
+                                 $('*[rel="tooltip"]').tooltip();
+
+                                   });
             },100);
         }
     };
     //call to init should be in the template
-    //init();    
+    //init();
 })($);
