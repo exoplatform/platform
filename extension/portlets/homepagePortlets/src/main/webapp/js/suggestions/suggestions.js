04ea2264c77eb2853031ce5af55731918a8dcf15
TRAN Trung Thanh <thanhtt@exoplatform.com>
2014-04-24 14:36:53 +0700

    PLF-5883: Lose iframe when clicking links in Getting Started and Suggestions applications
    Problem analysis
    - The window target is currently wrong.
    
    Fix description:
    - Set the target to current window.

--- extension/portlets/homepagePortlets/src/main/webapp/js/suggestions/suggestions.js
+++ extension/portlets/homepagePortlets/src/main/webapp/js/suggestions/suggestions.js
@@ -91,7 +91,7 @@
 
                     link += "<div class='peoplePicture pull-left'><div class='avatarXSmall'><img src='"+item.avatar+"'></div></div>";
                     link += "<div class='peopleInfo'>";
-                    link += "<div class='peopleName'><a href='"+item.profile+"' target='_parent'>"+item.suggestionName+"</a></div>";
+                    link += "<div class='peopleName'><a href='"+item.profile+"' target='_self'>"+item.suggestionName+"</a></div>";
                     link += "<div style='display:none;' class='peopleAction' ><a class='connect btn-primary btn btn-mini' href='#' onclick='return false'>"+connect+"</a><a class='ignore' href='#' onclick='return false'><i class='uiIconClose'></i></a></div>";
                     link +="<div class='peoplePosition'>"+item.title+"</div><div class='peopleConnection'>"+item.number+"&nbsp;"+connection+"</div>";
                     link += "</div></li>";
