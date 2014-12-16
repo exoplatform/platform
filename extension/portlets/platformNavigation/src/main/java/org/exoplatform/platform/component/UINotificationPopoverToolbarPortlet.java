a3173601a44e4aa1dcd7265ab5abb22546630415
tuvd08 <duytucntt@gmail.com>
2014-12-17 02:11:11 +0700

    SOC-4524 | [Notification-Web] Implements intranet message storage

--- extension/portlets/platformNavigation/src/main/java/org/exoplatform/platform/component/UINotificationPopoverToolbarPortlet.java
+++ extension/portlets/platformNavigation/src/main/java/org/exoplatform/platform/component/UINotificationPopoverToolbarPortlet.java
@@ -1,21 +1,12 @@
 package org.exoplatform.platform.component;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.exoplatform.commons.api.notification.NotificationContext;
-import org.exoplatform.commons.api.notification.channel.AbstractChannel;
-import org.exoplatform.commons.api.notification.channel.ChannelManager;
-import org.exoplatform.commons.api.notification.channel.template.AbstractTemplateBuilder;
-import org.exoplatform.commons.api.notification.model.ChannelKey;
-import org.exoplatform.commons.api.notification.model.MessageInfo;
-import org.exoplatform.commons.api.notification.model.NotificationInfo;
+import org.exoplatform.commons.api.notification.model.WebFilter;
+import org.exoplatform.commons.api.notification.service.WebNotificationService;
 import org.exoplatform.commons.api.notification.service.setting.UserSettingService;
-import org.exoplatform.commons.api.notification.service.storage.IntranetNotificationDataStorage;
 import org.exoplatform.commons.notification.channel.WebChannel;
-import org.exoplatform.commons.notification.impl.NotificationContextImpl;
-import org.exoplatform.commons.utils.CommonsUtils;
 import org.exoplatform.portal.application.PortalRequestContext;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
@@ -40,11 +31,11 @@ import org.exoplatform.webui.event.EventListener;
 public class UINotificationPopoverToolbarPortlet extends UIPortletApplication {
   private static final Log LOG = ExoLogger.getLogger(UINotificationPopoverToolbarPortlet.class);
   private String currentUser = "";
-  private IntranetNotificationDataStorage dataStorage;
+  private WebNotificationService webNftService;
   private UserSettingService userSettingService;
 
   public UINotificationPopoverToolbarPortlet() throws Exception {
-    dataStorage = getApplicationComponent(IntranetNotificationDataStorage.class);
+    webNftService = getApplicationComponent(WebNotificationService.class);
     userSettingService = getApplicationComponent(UserSettingService.class);
   }
   
@@ -55,18 +46,8 @@ public class UINotificationPopoverToolbarPortlet extends UIPortletApplication {
   }
 
   protected List<String> getNotifications() throws Exception {
-    List<String> messages = new ArrayList<String>();
-    List<NotificationInfo> infos = dataStorage.get(currentUser, 0, 8);
-    ChannelManager channelManager = CommonsUtils.getService(ChannelManager.class);
-    AbstractChannel webChannel = channelManager.getChannel(new ChannelKey(WebChannel.ID));
-    for (NotificationInfo info : infos) {
-      AbstractTemplateBuilder builder = webChannel.getTemplateBuilder(info.getKey());
-      NotificationContext nCtx = NotificationContextImpl.cloneInstance();
-      nCtx.setNotificationInfo(info);
-      MessageInfo msg = builder.buildMessage(nCtx);
-      messages.add(msg.getBody());
-    }
-    return messages;
+    WebFilter filter = new WebFilter(currentUser, true, 8);
+    return webNftService.getNotificationContents(filter);
   }
 
   protected List<String> getActions() {
@@ -89,7 +70,7 @@ public class UINotificationPopoverToolbarPortlet extends UIPortletApplication {
       String notificationId = event.getRequestContext().getRequestParameter(OBJECTID);
       UINotificationPopoverToolbarPortlet portlet = event.getSource();
       LOG.info("Run action MarkReadActionListener");
-      portlet.dataStorage.saveRead(portlet.currentUser, notificationId);
+      portlet.webNftService.markRead(notificationId);
       // Ignore reload portlet
       ((PortalRequestContext) event.getRequestContext().getParentAppRequestContext()).ignoreAJAXUpdateOnPortlets(true);
     }
@@ -100,7 +81,7 @@ public class UINotificationPopoverToolbarPortlet extends UIPortletApplication {
       String notificationId = event.getRequestContext().getRequestParameter(OBJECTID);
       UINotificationPopoverToolbarPortlet portlet = event.getSource();
       LOG.info("Run action RemoveActionListener: " + notificationId);
-      portlet.dataStorage.remove(portlet.currentUser, notificationId);
+      portlet.webNftService.remove(portlet.currentUser, notificationId);
       // Ignore reload portlet
       ((PortalRequestContext) event.getRequestContext().getParentAppRequestContext()).ignoreAJAXUpdateOnPortlets(true);
     }
@@ -110,7 +91,7 @@ public class UINotificationPopoverToolbarPortlet extends UIPortletApplication {
     public void execute(Event<UINotificationPopoverToolbarPortlet> event) throws Exception {
       UINotificationPopoverToolbarPortlet portlet = event.getSource();
       LOG.info("Run action MarkAllReadActionListener: " + portlet.currentUser);
-      portlet.dataStorage.saveReadAll(portlet.currentUser);
+      portlet.webNftService.markReadAll(portlet.currentUser);
       // Ignore reload portlet
       ((PortalRequestContext) event.getRequestContext().getParentAppRequestContext()).ignoreAJAXUpdateOnPortlets(true);
     }
