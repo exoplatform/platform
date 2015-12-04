639964e08e040da32c96fdd90fbe5b8f1585f007
Nguyen The Tuyen <tuyennt@exoplatform.com>
2015-12-04 13:59:19 +0700

    EXOGTN-2136 [Oauth Authentication] The avatar is not extracted

--- component/oauth-auth/src/main/java/org/exoplatform/oauth/service/impl/OAuthRegistrationServicesImpl.java
+++ component/oauth-auth/src/main/java/org/exoplatform/oauth/service/impl/OAuthRegistrationServicesImpl.java
@@ -18,7 +18,11 @@
  */
 package org.exoplatform.oauth.service.impl;
 
+import org.apache.commons.io.FilenameUtils;
 import org.exoplatform.commons.utils.ListAccess;
+import org.exoplatform.commons.utils.MimeTypeResolver;
+import org.exoplatform.container.component.ComponentRequestLifecycle;
+import org.exoplatform.container.component.RequestLifeCycle;
 import org.exoplatform.container.xml.InitParams;
 import org.exoplatform.container.xml.ValueParam;
 import org.exoplatform.oauth.filter.OAuthAbstractFilter;
@@ -30,6 +34,12 @@ import org.exoplatform.services.organization.UserHandler;
 import org.exoplatform.services.organization.UserProfile;
 import org.exoplatform.services.organization.UserProfileHandler;
 import org.exoplatform.services.organization.UserStatus;
+import org.exoplatform.social.core.identity.model.Identity;
+import org.exoplatform.social.core.identity.model.Profile;
+import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
+import org.exoplatform.social.core.image.ImageUtils;
+import org.exoplatform.social.core.manager.IdentityManager;
+import org.exoplatform.social.core.model.AvatarAttachment;
 import org.gatein.common.logging.Logger;
 import org.gatein.common.logging.LoggerFactory;
 import org.gatein.security.oauth.spi.AccessTokenContext;
@@ -38,9 +48,14 @@ import org.gatein.security.oauth.spi.OAuthProviderType;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class OAuthRegistrationServicesImpl implements OAuthRegistrationServices {
@@ -49,8 +64,9 @@ public class OAuthRegistrationServicesImpl implements OAuthRegistrationServices 
     private final List<String> registerOnFly;
 
     private final OrganizationService orgService;
+    private final IdentityManager identityManager;
 
-    public OAuthRegistrationServicesImpl(InitParams initParams, OrganizationService orgService) {
+    public OAuthRegistrationServicesImpl(InitParams initParams, OrganizationService orgService, IdentityManager identityManager) {
         ValueParam onFly = initParams.getValueParam("registerOnFly");
         String onFlyValue = onFly == null ? "" : onFly.getValue();
         if(onFlyValue != null && !onFlyValue.isEmpty()) {
@@ -60,6 +76,7 @@ public class OAuthRegistrationServicesImpl implements OAuthRegistrationServices 
         }
 
         this.orgService = orgService;
+        this.identityManager = identityManager;
     }
 
     @Override
@@ -135,6 +152,9 @@ public class OAuthRegistrationServicesImpl implements OAuthRegistrationServices 
       User user = providerType.getOauthPrincipalProcessor().convertToGateInUser(principal);
       user.setPassword(randomPassword(16));
 
+      if (orgService instanceof ComponentRequestLifecycle) {
+        RequestLifeCycle.begin((ComponentRequestLifecycle)orgService);
+      }
       try {
         orgService.getUserHandler().createUser(user, true);
 
@@ -149,11 +169,59 @@ public class OAuthRegistrationServicesImpl implements OAuthRegistrationServices 
         newUserProfile.setAttribute(providerType.getUserNameAttrName(), principal.getUserName());
         profileHandler.saveUserProfile(newUserProfile, true);
 
+        //
+        String avatar = principal.getAvatar();
+        if (avatar != null && !avatar.trim().isEmpty()) {
+          int WIDTH = 200;
+          String fileName = FilenameUtils.getBaseName(avatar);
+          MimeTypeResolver mimeTypeResolver = new MimeTypeResolver();
+          String mimeType = mimeTypeResolver.getMimeType(avatar);
+          if (!mimeType.toLowerCase().contains("image")) {
+            mimeType = "image/png";
+          }
+
+          try {
+            URL url = new URL(avatar);
+            InputStream is = url.openStream();
+            if (is != null) {
+              AvatarAttachment avatarAttachment = ImageUtils.createResizedAvatarAttachment(is, WIDTH, 0, null, fileName, mimeType, null);
+              if (avatarAttachment == null) {
+                avatarAttachment = new AvatarAttachment(null, fileName, mimeType, is, null, System.currentTimeMillis());
+              }
+              is.close();
+
+              Identity identity = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, user.getUserName(), true);
+              Profile p = identity.getProfile();
+
+              p.setProperty(Profile.AVATAR, avatarAttachment);
+              Map<String, Object> props = p.getProperties();
+              for (String key : props.keySet()) {
+                if (key.startsWith(Profile.AVATAR + ImageUtils.KEY_SEPARATOR)) {
+                  p.removeProperty(key);
+                }
+              }
+
+              identityManager.updateProfile(p);
+            }
+          } catch (MalformedURLException ex) {
+            log.warn("Can not fetch Avatar of oauth user because the URL is invalid: " + avatar, ex);
+          } catch (IOException ex) {
+            log.warn("Can not fetch Avatar at URL: " + avatar, ex);
+          } catch (Exception ex) {
+            // This exception may be thrown by the AvatarAttachment constructor
+            log.warn("Can not fetch Avatar at URL: " + avatar, ex);
+          }
+        }
+
       } catch (Exception ex) {
         if (log.isErrorEnabled()) {
           log.error("Exception when trying to create user: " + user.getUserName() + " on-fly", ex);
         }
         user = null;
+      } finally {
+        if (orgService instanceof ComponentRequestLifecycle) {
+          RequestLifeCycle.end();
+        }
       }
 
       return user;
