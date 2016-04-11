a7dbcf2979fcf72554c74eb67fdaa6a315d3d808
nttuyen <nttuyen_it@yahoo.com>
2016-04-11 18:44:01 +0700

    PLF-6583 Be able to create a task from the top bar navigation

--- extension/portlets/platformNavigation/src/main/webapp/javascript/eXo/platform/navigation/ToolBarNavigation/UIToolBarNavigation.js
+++ extension/portlets/platformNavigation/src/main/webapp/javascript/eXo/platform/navigation/ToolBarNavigation/UIToolBarNavigation.js
@@ -1,17 +1,27 @@
 (function ($) {
     var portalNavigation = {
 
-        cancelNextClick:function (ComponentId, baseId, message) {
+        cancelNextClick:function (ComponentId, baseId, message, link) {
             var component = $("#" + ComponentId);
             var parent = $("#" + baseId);
+            var $span = parent.find('span');
 
             var portalNav = portalNavigation;
 
             component.hide();
-            parent.find("span").html(message).css('display', 'inline').fadeOut(2000, function () {
-                component.removeAttr("style");
 
-            });
+            var html = message;
+            if (link) {
+                html = '<a href="'+link+'">' + message + '</a>';
+            }
+
+            $span.html(html).css('display', 'inline');
+            setTimeout(function() {
+                $span.fadeOut(1000, function() {
+                    component.parent().removeClass('open');
+                    component.removeAttr("style");
+                });
+            }, 5000);
         },
         ClickActionButton:function ( baseId) {
 
