108090ca5afca120ee460ab4e80df7ad7e0cc7a8
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2016-07-13 13:54:36 +0200

    PLF-6708 : Continue implementing web component for document preview

--- extension/webapp/src/main/webapp/javascript/document-preview.js
+++ extension/webapp/src/main/webapp/javascript/document-preview.js
@@ -60,11 +60,27 @@
     },
 
     render: function () {
+      resizeEventHandler();
+      $(window).on('resize', resizeEventHandler);
+      $(document).on('keyup', closeEventHandler);
+
+      // Bind close event. Return body scroll, turn off keyup
+      $(".exitWindow > .uiIconClose", $('#uiDocumentPreview')).click(function() {
+        $('body').removeClass('modal-open');
+        setTimeout(function() {
+          $('body').css('overflow', 'visible');
+          $(document).off('keyup', closeEventHandler);
+          $(window).off('resize', resizeEventHandler);
+        }, 500);
+      });
+
       var docContentContainer = $('#documentPreviewContent');
       var docContent = '';
       if(this.options.isWebContent) {
         docContent += '<div class="uiPreviewWebContent">';
       }
+
+      docContentContainer.load('/rest/private/contentviewer/repository/collaboration/' + this.docId);
 
       if(this.options.isWebContent) {
         docContent += '</div>';
@@ -80,6 +96,91 @@
     }
 
   };
+
+  // Bind Esc key
+  var closeEventHandler = function(e) {
+    $('#presentationMode').blur();
+    if (e.keyCode == 27 && ("presentationMode" != e.target.id || $.browser.mozilla)) {
+      $(".exitWindow > .uiIconClose", $uiDocumentPreview).trigger("click");
+    }
+  }
+
+  // Resize Event
+  var resizeEventHandler = function() {
+    // Calculate margin
+    var pdfDisplayAreaHeight = window.innerHeight - 92;
+    var $uiDocumentPreview = $('#uiDocumentPreview');
+    $('#outerContainer', $uiDocumentPreview).height(pdfDisplayAreaHeight); // pdf viewer
+    var $commentArea = $('.commentArea', $uiDocumentPreview);
+    var $commentAreaTitle = $('.title', $commentArea);
+    var $commentInputBox = $('.commentInputBox', $commentArea);
+    var $commentList = $('.commentList', $commentArea);
+    var $highlightBox = $('.highlightBox', $commentArea);
+    var $actionBarCommentArea = $('.actionBar', $commentArea);
+    var commentAreaHeight = window.innerHeight - 30;
+    $commentArea.height(commentAreaHeight);
+    $commentList.css('max-height', commentAreaHeight - $commentAreaTitle.innerHeight() - $commentInputBox.innerHeight() - $highlightBox.innerHeight() - $actionBarCommentArea.innerHeight() - 16); //16 is padding of commentList
+    $commentList.scrollTop(20000);
+
+    // Media viewer, no preview file
+    var $navigationContainer = $(".navigationContainer", $uiDocumentPreview);
+    var $uiContentBox = $('.uiContentBox', $navigationContainer);
+    var $video = $('.videoContent', $uiContentBox);
+    var $flowplayerContentDetail = $('.ContentDetail', $uiContentBox);
+    var $flowplayerPlayerContent = $('.PlayerContent', $flowplayerContentDetail);
+    var $flowplayer = $('object', $flowplayerPlayerContent);
+    var $flashViewer = $('.FlashViewer', $uiContentBox);
+    var $embedFlashViewer = $('embed', $flashViewer);
+    var $windowmediaplayer = $('#MediaPlayer1', $uiContentBox);
+    var $embedWMP = $('embed', $windowmediaplayer);
+
+    $navigationContainer.height(pdfDisplayAreaHeight);
+    $uiContentBox.height(pdfDisplayAreaHeight);
+    $flowplayerContentDetail.height(pdfDisplayAreaHeight);
+    $flowplayerPlayerContent.height(pdfDisplayAreaHeight - 5);
+    $flashViewer.height(pdfDisplayAreaHeight - 5);
+
+    $flowplayer.css('max-width', $uiContentBox.width() - 2);
+    $flowplayer.css('max-height', $uiContentBox.height() - 3);
+    $flowplayer.css('width', '100%');
+    $flowplayer.css('height', '100%');
+
+    $video.css('max-width', $uiContentBox.width() - 2);
+    $video.css('max-height', $uiContentBox.height() - 3);
+    $video.css('width', '100%');
+    $video.css('height', '100%');
+
+    $windowmediaplayer.css('max-width', $uiContentBox.width() - 2);
+    $windowmediaplayer.css('max-height', $uiContentBox.height() - 7);
+    $windowmediaplayer.css('width', '100%');
+    $windowmediaplayer.css('height', '100%');
+    $embedWMP.css('width', '100%');
+    $embedWMP.css('height', '100%')
+
+    $embedFlashViewer.css('max-width', $uiContentBox.width() - 2);
+    $embedFlashViewer.css('max-height', $uiContentBox.height() - 3);
+    $embedFlashViewer.css('width', '100%');
+    $embedFlashViewer.css('height', '100%');
+
+    var $img = $('a > img', $uiContentBox);
+
+    if ($img.length > 0) {
+      $img.css('max-width', $uiContentBox.width() + 1);
+      $img.css('max-height', $uiContentBox.height() + 1);
+      $img.css('width', 'auto');
+      $img.css('height', 'auto');
+      $navigationContainer.css('overflow', 'hidden');
+    }
+
+    $('.uiPreviewWebContent', $uiDocumentPreview).height(pdfDisplayAreaHeight - 30) // webcontent
+    var $EmbedHtml =  $('.EmbedHtml', $uiDocumentPreview);
+    $EmbedHtml.height(pdfDisplayAreaHeight) // External embedded
+
+    // Resize image flick
+    $img = $('.uiDocumentPreviewMainWindow > .EmbedHtml > a > img');
+    $img.css('max-width', $EmbedHtml.width());
+    $img.css('max-height', $EmbedHtml.height());
+  }
 
   return documentPreview;
 })($);
