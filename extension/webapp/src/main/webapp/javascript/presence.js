7b1a485e0895c2cf00be00e53759512d70fa9bdf
Boubaker Khanfir <bkhanfir@exoplatform.com>
2018-05-16 12:59:06 +0100

    CHAT-835 Fix Javascript ping method to make it periodic

--- extension/webapp/src/main/webapp/javascript/presence.js
+++ extension/webapp/src/main/webapp/javascript/presence.js
@@ -1,24 +1,24 @@
 (function($) {
     var obj = {};
-
     var url = eXo.env.server.context + "/" + eXo.env.portal.rest + "/state/ping";
     obj.sendPing = function(frequency) {
+        obj.sendSinglePing();
+        setInterval(function() {
+            if(!obj.error) {
+                obj.sendSinglePing();
+            }}, frequency);
+    }
+    obj.sendSinglePing = function() {
         $.ajax({
             url: url,
-            dataType: "json",
             context: this,
-            success: function(data){
-                setTimeout(obj.sendPing, frequency, frequency);
-            },
             error: function(xhr){
-                if (xhr.status >= 500) {
-                    setTimeout(obj.sendPing, frequency * 2, frequency);
-                } else if (xhr.status != 200) {
+                if (xhr && xhr.status != 200) {
                     console.log("Last ping returns a status code " + xhr.status + ", stopping");
+                    this.error = true;
                 }
             }
         });
-    }
-
+    };
     return obj;
 })($);
