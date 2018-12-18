abe29f6469c528039d32e9f961a1a1cdc40eaf9c
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2018-12-18 09:11:20 +0100

    PLF-8190 : remove useless named queries and public qualifier in interface

--- exo-gadget-pack/gadget-pack-services/src/main/java/org/exoplatform/platform/gadget/services/LoginHistory/jpa/dao/LoginHistoryDAO.java
+++ exo-gadget-pack/gadget-pack-services/src/main/java/org/exoplatform/platform/gadget/services/LoginHistory/jpa/dao/LoginHistoryDAO.java
@@ -15,14 +15,17 @@ public class LoginHistoryDAO extends GenericDAOJPAImpl<LoginHistoryEntity, Long>
     public List<String> getActiveUsersId(Long fromTime) {
         // returns a list of active users from a given time
         Timestamp from = new Timestamp(fromTime);
-        return getEntityManager().createNamedQuery("loginHistory.getActiveUsersId").setParameter("from",from).getResultList();
+        return getEntityManager()
+                .createNamedQuery("loginHistory.getActiveUsersId")
+                .setParameter("from",from)
+                .getResultList();
     }
 
     public Long getLoginCountPerDay(String userId, Long fromDay, Long toDay) {
         // returns the number of a user's login for a given period of time
         Timestamp from = new Timestamp(fromDay);
         Timestamp to = new Timestamp(toDay);
-        return (Long) getEntityManager().createNamedQuery("loginHistory.getLoginCountPerDay")
+        return (Long) getEntityManager().createNamedQuery("loginHistory.getLoginsCountOfUserInDateRange")
                                         .setParameter("userId",userId)
                                         .setParameter("from", from)
                                         .setParameter("to",to).getSingleResult();
@@ -32,7 +35,7 @@ public class LoginHistoryDAO extends GenericDAOJPAImpl<LoginHistoryEntity, Long>
         try {
             Long lastLogin;
             LoginHistoryEntity loginHistoryEntity = getEntityManager()
-                    .createNamedQuery("loginHistory.getLastLoginHistory", LoginHistoryEntity.class)
+                    .createNamedQuery("loginHistory.getLastLoginsOfUser", LoginHistoryEntity.class)
                     .setParameter("userId", userId)
                     .setMaxResults(1)
                     .getSingleResult();
@@ -44,7 +47,8 @@ public class LoginHistoryDAO extends GenericDAOJPAImpl<LoginHistoryEntity, Long>
     }
 
     public List<LoginHistoryEntity> getLastLoginsOfUser(int numLogins, String userId) {
-        List<LoginHistoryEntity> loginHistoryEntityList = getEntityManager().createNamedQuery("loginHistory.getLastLoginsOfUser", LoginHistoryEntity.class)
+        List<LoginHistoryEntity> loginHistoryEntityList = getEntityManager()
+                .createNamedQuery("loginHistory.getLastLoginsOfUser", LoginHistoryEntity.class)
                 .setParameter("userId", userId)
                 .setMaxResults(numLogins)
                 .getResultList();
@@ -52,26 +56,31 @@ public class LoginHistoryDAO extends GenericDAOJPAImpl<LoginHistoryEntity, Long>
     }
 
     public List<LoginHistoryEntity> getLastLogins(int numLogins) {
-        List<LoginHistoryEntity> loginHistoryEntityList = getEntityManager().createNamedQuery("loginHistory.getLastLogins", LoginHistoryEntity.class)
-                    .setMaxResults(numLogins)
-                    .getResultList();
+        List<LoginHistoryEntity> loginHistoryEntityList = getEntityManager()
+                .createNamedQuery("loginHistory.getLastLogins", LoginHistoryEntity.class)
+                .setMaxResults(numLogins)
+                .getResultList();
         return loginHistoryEntityList;
     }
 
     public List<LoginHistoryEntity> getLoginHistory(String userId, long fromTime, long toTime) {
         Timestamp from = new Timestamp(fromTime);
         Timestamp to = new Timestamp(toTime);
-        List<LoginHistoryEntity> loginHistoryEntityList = getEntityManager().createNamedQuery("loginHistory.getUserLoginHistory",LoginHistoryEntity.class)
+        List<LoginHistoryEntity> loginHistoryEntityList = getEntityManager()
+                .createNamedQuery("loginHistory.getLastLoginsOfUserInDateRange",LoginHistoryEntity.class)
                 .setParameter("userId",userId)
                 .setParameter("from",from)
                 .setParameter("to",to).getResultList();
         return loginHistoryEntityList;
     }
 
-    public Set<String> getLastUsersLogin(long fromTime) throws Exception {
+    public Set<String> getLastLoginsAfterDate(long fromTime) throws Exception {
         Timestamp from = new Timestamp(fromTime);
         try {
-            List<String> userIds = getEntityManager().createNamedQuery("loginHistory.getLastUsersLogin").setParameter("from",from).getResultList();
+            List<String> userIds = getEntityManager()
+                    .createNamedQuery("loginHistory.getLastLoginsAfterDate")
+                    .setParameter("from",from)
+                    .getResultList();
             Set<String> users = new LinkedHashSet<String>(userIds);
             return users;
         } catch (Exception e) {
@@ -81,8 +90,17 @@ public class LoginHistoryDAO extends GenericDAOJPAImpl<LoginHistoryEntity, Long>
     }
 
     public long getBeforeLastLogin(String userId) {
-        Long lastLoginID = (Long) getEntityManager().createNamedQuery("loginHistory.getUserLastLoginID").setParameter("userId",userId).getSingleResult();
-        Long beforeLastLoginID = (Long) getEntityManager().createNamedQuery("loginHistory.getBeforeLastLoginID").setParameter("userId",userId).setParameter("id",lastLoginID).getSingleResult();
+        LoginHistoryEntity lastLogin = getEntityManager()
+                .createNamedQuery("loginHistory.getLastLoginsOfUser", LoginHistoryEntity.class)
+                .setParameter("userId", userId)
+                .setMaxResults(1)
+                .getSingleResult();
+        Long lastLoginID = lastLogin.getID();
+        Long beforeLastLoginID = (Long) getEntityManager()
+                .createNamedQuery("loginHistory.getBeforeLastLoginID")
+                .setParameter("userId",userId)
+                .setParameter("id",lastLoginID)
+                .getSingleResult();
         LoginHistoryEntity loginHistoryEntity = find(beforeLastLoginID);
         return loginHistoryEntity.getLoginDate().getTime();
     }
--- exo-gadget-pack/gadget-pack-services/src/main/java/org/exoplatform/platform/gadget/services/LoginHistory/jpa/entity/LoginHistoryEntity.java
+++ exo-gadget-pack/gadget-pack-services/src/main/java/org/exoplatform/platform/gadget/services/LoginHistory/jpa/entity/LoginHistoryEntity.java
@@ -9,14 +9,12 @@ import java.sql.Timestamp;
 @ExoEntity
 @Table(name = "LOGIN_HISTORY")
 @NamedQueries({
-        @NamedQuery(name = "loginHistory.getLastLoginHistory",query = "SELECT l FROM LoginHistoryEntity l WHERE l.userId = :userId ORDER BY l.id DESC"),
-        @NamedQuery(name = "loginHistory.getUserLastLoginID",query = "SELECT MAX(l.id) FROM LoginHistoryEntity l WHERE l.userId = :userId"),
-        @NamedQuery(name = "loginHistory.getBeforeLastLoginID",query = "SELECT MAX(l.id) FROM LoginHistoryEntity l WHERE l.userId = :userId AND l.id < :id"),
-        @NamedQuery(name = "loginHistory.getUserLoginHistory",query = "SELECT l FROM LoginHistoryEntity l WHERE l.userId = :userId AND l.loginDate BETWEEN :from AND :to"),
         @NamedQuery(name = "loginHistory.getLastLogins",query = "SELECT l FROM LoginHistoryEntity l ORDER BY l.id DESC"),
-        @NamedQuery(name = "loginHistory.getLastLoginsOfUser",query = "SELECT l FROM LoginHistoryEntity l WHERE l.userId = :userId"),
-        @NamedQuery(name = "loginHistory.getLoginCountPerDay",query = "SELECT COUNT (l) FROM LoginHistoryEntity l WHERE l.userId = :userId AND l.loginDate BETWEEN :from AND :to"),
-        @NamedQuery(name = "loginHistory.getLastUsersLogin",query = "SELECT l.userId FROM LoginHistoryEntity l WHERE l.loginDate >= :from ORDER BY l.id DESC"),
+        @NamedQuery(name = "loginHistory.getLastLoginsOfUser",query = "SELECT l FROM LoginHistoryEntity l WHERE l.userId = :userId ORDER BY l.id DESC"),
+        @NamedQuery(name = "loginHistory.getBeforeLastLoginID",query = "SELECT MAX(l.id) FROM LoginHistoryEntity l WHERE l.userId = :userId AND l.id < :id"),
+        @NamedQuery(name = "loginHistory.getLastLoginsOfUserInDateRange",query = "SELECT l FROM LoginHistoryEntity l WHERE l.userId = :userId AND l.loginDate BETWEEN :from AND :to"),
+        @NamedQuery(name = "loginHistory.getLoginsCountOfUserInDateRange",query = "SELECT COUNT (l) FROM LoginHistoryEntity l WHERE l.userId = :userId AND l.loginDate BETWEEN :from AND :to"),
+        @NamedQuery(name = "loginHistory.getLastLoginsAfterDate",query = "SELECT l.userId FROM LoginHistoryEntity l WHERE l.loginDate >= :from ORDER BY l.id DESC"),
         @NamedQuery(name = "loginHistory.getActiveUsersId",query = "SELECT DISTINCT l.userId FROM LoginHistoryEntity l where l.loginDate < :from")
 })
 public class LoginHistoryEntity {
