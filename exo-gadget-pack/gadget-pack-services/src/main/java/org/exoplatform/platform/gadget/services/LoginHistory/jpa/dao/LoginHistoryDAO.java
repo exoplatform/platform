da09fdabf5d7f5be2138343292d1e0e81c922c7b
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2018-12-24 09:31:55 +0100

    PLF-8190 : return null when no login for the given user

--- exo-gadget-pack/gadget-pack-services/src/main/java/org/exoplatform/platform/gadget/services/LoginHistory/jpa/dao/LoginHistoryDAO.java
+++ exo-gadget-pack/gadget-pack-services/src/main/java/org/exoplatform/platform/gadget/services/LoginHistory/jpa/dao/LoginHistoryDAO.java
@@ -5,199 +5,281 @@ import org.exoplatform.platform.gadget.services.LoginHistory.jpa.entity.LoginHis
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
 
+import javax.persistence.NoResultException;
 import java.sql.Timestamp;
 import java.util.*;
 
 public class LoginHistoryDAO extends GenericDAOJPAImpl<LoginHistoryEntity, Long> {
-    private static final Log LOG = ExoLogger.getLogger(LoginHistoryDAO.class);
+  private static final Log LOG = ExoLogger.getLogger(LoginHistoryDAO.class);
 
-    /**
-     * returns a list of users IDs that logged in from a given date.
-     * @param fromTime
-     * @return
-     */
-    public List<String> getActiveUsersId(Long fromTime) {
-        Timestamp from = new Timestamp(fromTime);
-        return getEntityManager()
-                .createNamedQuery("loginHistory.getActiveUsersId")
-                .setParameter("from",from)
-                .getResultList();
+  /**
+   * returns a list of users IDs that logged in from a given date.
+   * 
+   * @param fromTime
+   * @return
+   */
+  public List<String> getActiveUsersId(Long fromTime) {
+    Timestamp from = new Timestamp(fromTime);
+    List<String> activeUsersId;
+    try {
+      activeUsersId = getEntityManager().createNamedQuery("loginHistory.getActiveUsersId")
+              .setParameter("from", from)
+              .getResultList();
+    } catch (NoResultException e) {
+      LOG.error("No active users found.");
+      activeUsersId = null;
     }
+    return activeUsersId;
+  }
 
-    /**
-     * if the userId's set to "AllUsers" or is null,
-     * returns the count per day of all loggins,
-     * else it returns the the count per day for a given user.
-     * @param userId
-     * @param fromDay
-     * @param toDay
-     * @return
-     */
-    public Long getLoginCountPerDay(String userId, Long fromDay, Long toDay) {
-        Timestamp from = new Timestamp(fromDay);
-        Timestamp to = new Timestamp(toDay);
-        if (userId.equals("AllUsers") || userId == null){
-            return (Long) getEntityManager().createNamedQuery("loginHistory.getLoginsCountOfAllUserInDateRange").setParameter("from",from).setParameter("to",to).getSingleResult();
-        } else {
-            return (Long) getEntityManager().createNamedQuery("loginHistory.getLoginsCountOfUserInDateRange")
-                    .setParameter("userId",userId)
-                    .setParameter("from", from)
-                    .setParameter("to",to).getSingleResult();
-        }
+  /**
+   * returns the count of logins per day for a given user.
+   * 
+   * @param userId
+   * @param fromDay
+   * @param toDay
+   * @return
+   */
+  public Long getLoginCountPerDay(String userId, Long fromDay, Long toDay) {
+    Timestamp from = new Timestamp(fromDay);
+    Timestamp to = new Timestamp(toDay);
+    Long count;
+    try {
+      count = (Long) getEntityManager().createNamedQuery("loginHistory.getLoginsCountOfUserInDateRange")
+              .setParameter("userId", userId)
+              .setParameter("from", from)
+              .setParameter("to", to)
+              .getSingleResult();
+    } catch (NoResultException e) {
+      LOG.error("No login found for "+userId+".");
+      count = null;
     }
+    return count;
+  }
 
-    /**
-     * returns the last login entry date of a given user.
-     * @param userId
-     * @return
-     */
-    public Long getLastLogin(String userId) {
-        Long lastLogin;
-        LoginHistoryEntity loginHistoryEntity = getEntityManager()
-                    .createNamedQuery("loginHistory.getLastLoginsOfUser", LoginHistoryEntity.class)
-                    .setParameter("userId", userId)
-                    .setMaxResults(1)
-                    .getSingleResult();
-        lastLogin = loginHistoryEntity.getLoginDate().getTime();
-        return loginHistoryEntity == null ? 0 : lastLogin;
+  /**
+   * returns the count per day of all users logins.
+   * @param fromDay
+   * @param toDay
+   * @return
+   */
+  public Long getLoginsCountInDateRange(Long fromDay, Long toDay) {
+    Timestamp from = new Timestamp(fromDay);
+    Timestamp to = new Timestamp(toDay);
+    Long count;
+    try {
+      count = (Long) getEntityManager().createNamedQuery("loginHistory.getLoginsCountInDateRange")
+              .setParameter("from", from)
+              .setParameter("to", to)
+              .getSingleResult();
+    } catch (NoResultException e) {
+      LOG.error("No Login found.");
+      count = null;
     }
+    return count;
+  }
 
-    /**
-     * returns the last n history logins of a user
-     * set by a given limit number numLogins.
-     * @param numLogins
-     * @param userId
-     * @return
-     */
-    public List<LoginHistoryEntity> getLastLoginsOfUser(int numLogins, String userId) {
-        List<LoginHistoryEntity> loginHistoryEntityList = getEntityManager()
-                .createNamedQuery("loginHistory.getLastLoginsOfUser", LoginHistoryEntity.class)
+  /**
+   * returns the last login entry date of a given user.
+   * 
+   * @param userId
+   * @return
+   */
+  public Long getLastLogin(String userId) {
+    Long lastLogin;
+    try {
+      LoginHistoryEntity loginHistoryEntity = getEntityManager().createNamedQuery("loginHistory.getLastLoginsOfUser", LoginHistoryEntity.class)
+                                                                .setParameter("userId", userId)
+                                                                .setMaxResults(1)
+                                                                .getSingleResult();
+      lastLogin = loginHistoryEntity.getLoginDate().getTime();
+    } catch (NoResultException e) {
+      LOG.error("No last login found for "+userId+".");
+      lastLogin = null;
+    }
+    return lastLogin;
+  }
+
+  /**
+   * returns the last n history logins of a user set by a given limit number
+   * numLogins.
+   * 
+   * @param numLogins
+   * @param userId
+   * @return
+   */
+  public List<LoginHistoryEntity> getLastLoginsOfUser(int numLogins, String userId) {
+    List<LoginHistoryEntity> loginHistoryEntityList;
+    try {
+      loginHistoryEntityList = getEntityManager().createNamedQuery("loginHistory.getLastLoginsOfUser", LoginHistoryEntity.class)
+              .setParameter("userId", userId)
+              .setMaxResults(numLogins)
+              .getResultList();
+    } catch (NoResultException e) {
+      LOG.error("No login found for "+userId+".");
+      loginHistoryEntityList = null;
+    }
+    return loginHistoryEntityList;
+  }
+
+  /**
+   * returns the last n user IDs set by a given limit number numLogins.
+   * 
+   * @param numLogins
+   * @return
+   */
+  public List<String> getLastLoggedUsers(int numLogins) {
+    List<String> lastLoggedUsers;
+    try {
+      lastLoggedUsers = getEntityManager().createNamedQuery("loginHistory.getLastLoggedUsers")
+              .setMaxResults(numLogins)
+              .getResultList();
+    } catch (NoResultException e) {
+      LOG.error("No logged Users found");
+      lastLoggedUsers = null;
+    }
+    return lastLoggedUsers;
+  }
+
+  /**
+   * returns the last history login entry of a given user.
+   * 
+   * @param userId
+   * @return
+   */
+  public LoginHistoryEntity getLastLoginOfUser(String userId) {
+    LoginHistoryEntity lastLogin;
+    try {
+      lastLogin = getEntityManager().createNamedQuery("loginHistory.getLastLoginsOfUser", LoginHistoryEntity.class)
+              .setParameter("userId", userId)
+              .setMaxResults(1)
+              .getSingleResult();
+    } catch (NoResultException e) {
+      LOG.error("No last login found for "+userId+".");
+      lastLogin = null;
+    }
+    return lastLogin;
+  }
+
+  /**
+   * returns a list of the last n login history entries set by a given limit
+   * number numLogins.
+   * 
+   * @param numLogins
+   * @return
+   */
+  public List<LoginHistoryEntity> getLastLogins(int numLogins) {
+    List<LoginHistoryEntity> loginHistoryEntityList;
+    try {
+      loginHistoryEntityList = getEntityManager().createNamedQuery("loginHistory.getLastLogins", LoginHistoryEntity.class)
+              .setMaxResults(numLogins)
+              .getResultList();
+    } catch (NoResultException e) {
+      LOG.error("No last logins found.");
+      loginHistoryEntityList = null;
+    }
+    return loginHistoryEntityList;
+  }
+
+  /**
+   * returns a list of login history entries of a given user between a given two
+   * dates, fromTime and toTime.
+   * 
+   * @param userId
+   * @param fromTime
+   * @param toTime
+   * @return
+   */
+  public List<LoginHistoryEntity> getLoginHistory(String userId, long fromTime, long toTime) {
+    Timestamp from = new Timestamp(fromTime);
+    Timestamp to = new Timestamp(toTime);
+    List<LoginHistoryEntity> loginHistoryEntityList;
+    try {
+      loginHistoryEntityList = getEntityManager().createNamedQuery("loginHistory.getLastLoginsOfUserInDateRange", LoginHistoryEntity.class)
+              .setParameter("userId", userId)
+              .setParameter("from", from)
+              .setParameter("to", to)
+              .getResultList();
+    } catch (NoResultException e) {
+      LOG.error("No login found for "+userId+".");
+      loginHistoryEntityList = null;
+    }
+    return loginHistoryEntityList;
+  }
+
+  /**
+   * returns a list of login history entries a given two dates, fromTime and
+   * toTime.
+   * 
+   * @param fromTime
+   * @param toTime
+   * @return
+   */
+  public List<LoginHistoryEntity> getAllLoginHistory(long fromTime, long toTime) {
+    Timestamp from = new Timestamp(fromTime);
+    Timestamp to = new Timestamp(toTime);
+    List<LoginHistoryEntity> loginHistoryEntityList;
+    try {
+      loginHistoryEntityList = getEntityManager().createNamedQuery("loginHistory.getLastLoginsInDateRange", LoginHistoryEntity.class)
+              .setParameter("from", from)
+              .setParameter("to", to)
+              .getResultList();
+    } catch (NoResultException e) {
+      LOG.error("No logins found.");
+      loginHistoryEntityList = null;
+    }
+    return loginHistoryEntityList;
+  }
+
+  /**
+   * returns a list of user IDs that did log in from a given date till now.
+   * 
+   * @param fromTime
+   * @return
+   * @throws Exception
+   */
+  public Set<String> getLastLoginsAfterDate(long fromTime) throws Exception {
+    Timestamp from = new Timestamp(fromTime);
+    Set<String> users;
+    try {
+      List<String> userIds = getEntityManager().createNamedQuery("loginHistory.getLastLoginsAfterDate")
+                                               .setParameter("from", from)
+                                               .getResultList();
+      users = new LinkedHashSet<>(userIds);
+    } catch (NoResultException e) {
+      LOG.error("No login found since "+from+".");
+      users = null;
+    }
+    return users;
+  }
+
+  /**
+   * returns the just before last login entry date of a given user.
+   * 
+   * @param userId
+   * @return
+   * @throws Exception
+   */
+  public long getBeforeLastLogin(String userId) throws Exception {
+    long beforeLastLogin;
+    try {
+      LoginHistoryEntity lastLogin = getLastLoginOfUser(userId);
+      if (lastLogin == null) {
+        beforeLastLogin = 0;
+      } else {
+        Long lastLoginID = lastLogin.getID();
+        Long beforeLastLoginID = (Long) getEntityManager().createNamedQuery("loginHistory.getBeforeLastLoginID")
                 .setParameter("userId", userId)
-                .setMaxResults(numLogins)
-                .getResultList();
-        return loginHistoryEntityList;
-    }
-
-    /**
-     * returns the last n user IDs set by a given
-     * limit number numLogins.
-     * @param numLogins
-     * @return
-     */
-    public List<String> getLastLoggedUsers(int numLogins) {
-        List<String> lastLoggedUsers = getEntityManager().createNamedQuery("loginHistory.getLastLoggedUsers")
-                .setMaxResults(numLogins)
-                .getResultList();
-        return lastLoggedUsers;
-    }
-
-    /**
-     * returns the last history login entry of a given user.
-     * @param userId
-     * @return
-     */
-    public LoginHistoryEntity getLastLoginOfUser(String userId) {
-        LoginHistoryEntity lastLogin = getEntityManager()
-                .createNamedQuery("loginHistory.getLastLoginsOfUser", LoginHistoryEntity.class)
-                .setParameter("userId", userId)
-                .setMaxResults(1)
+                .setParameter("id", lastLoginID)
                 .getSingleResult();
-        return lastLogin;
+        LoginHistoryEntity loginHistoryEntity = find(beforeLastLoginID);
+        beforeLastLogin = loginHistoryEntity.getLoginDate().getTime();
+      }
+    } catch (NoResultException e) {
+      LOG.error("Error while retrieving " + userId + "'s before last login: " + e.getMessage(), e);
+      beforeLastLogin = 0;
     }
-
-    /**
-     * returns a list of the last n login history entries
-     * set by a given limit number numLogins.
-     * @param numLogins
-     * @return
-     */
-    public List<LoginHistoryEntity> getLastLogins(int numLogins) {
-        List<LoginHistoryEntity> loginHistoryEntityList = getEntityManager()
-                .createNamedQuery("loginHistory.getLastLogins", LoginHistoryEntity.class)
-                .setMaxResults(numLogins)
-                .getResultList();
-        return loginHistoryEntityList;
-    }
-
-    /**
-     * returns a list of login history entries of a given user
-     * between a given two dates, fromTime and toTime.
-     * @param userId
-     * @param fromTime
-     * @param toTime
-     * @return
-     */
-    public List<LoginHistoryEntity> getLoginHistory(String userId, long fromTime, long toTime) {
-        Timestamp from = new Timestamp(fromTime);
-        Timestamp to = new Timestamp(toTime);
-        List<LoginHistoryEntity> loginHistoryEntityList = getEntityManager()
-                .createNamedQuery("loginHistory.getLastLoginsOfUserInDateRange",LoginHistoryEntity.class)
-                .setParameter("userId",userId)
-                .setParameter("from",from)
-                .setParameter("to",to).getResultList();
-        return loginHistoryEntityList;
-    }
-
-    /**
-     * returns a list of login history entries a given two dates,
-     * fromTime and toTime.
-     * @param fromTime
-     * @param toTime
-     * @return
-     */
-    public List<LoginHistoryEntity> getAllLoginHistory(long fromTime, long toTime) {
-        Timestamp from = new Timestamp(fromTime);
-        Timestamp to = new Timestamp(toTime);
-        List<LoginHistoryEntity> loginHistoryEntityList = getEntityManager()
-                .createNamedQuery("loginHistory.getLastLoginsInDateRange",LoginHistoryEntity.class)
-                .setParameter("from",from)
-                .setParameter("to",to).getResultList();
-        return loginHistoryEntityList;
-    }
-
-    /**
-     * returns a list of user IDs that did log in
-     * from a given date till now.
-     * @param fromTime
-     * @return
-     * @throws Exception
-     */
-    public Set<String> getLastLoginsAfterDate(long fromTime) throws Exception {
-        Timestamp from = new Timestamp(fromTime);
-        try {
-            List<String> userIds = getEntityManager()
-                    .createNamedQuery("loginHistory.getLastLoginsAfterDate")
-                    .setParameter("from",from)
-                    .getResultList();
-            Set<String> users = new LinkedHashSet<>(userIds);
-            return users;
-        } catch (Exception e) {
-            LOG.error("Error while getting login history of users " + e.getMessage(), e);
-        }
-        return null;
-    }
-
-    /**
-     *returns the just before last login entry date of a given user.
-     * @param userId
-     * @return
-     * @throws Exception
-     */
-    public long getBeforeLastLogin(String userId) throws Exception {
-        try {
-            LoginHistoryEntity lastLogin = getLastLoginOfUser(userId);
-            Long lastLoginID = lastLogin.getID();
-            Long beforeLastLoginID = (Long) getEntityManager()
-                    .createNamedQuery("loginHistory.getBeforeLastLoginID")
-                    .setParameter("userId",userId)
-                    .setParameter("id",lastLoginID)
-                    .getSingleResult();
-            LoginHistoryEntity loginHistoryEntity = find(beforeLastLoginID);
-            return loginHistoryEntity.getLoginDate().getTime();
-        } catch (Exception e) {
-            LOG.debug("Error while retrieving " + userId + "'s before last login: " + e.getMessage(), e);
-            throw e;
-        }
-    }
+    return beforeLastLogin;
+  }
 
 }
--- exo-gadget-pack/gadget-pack-services/src/main/java/org/exoplatform/platform/gadget/services/LoginHistory/jpa/entity/LoginHistoryEntity.java
+++ exo-gadget-pack/gadget-pack-services/src/main/java/org/exoplatform/platform/gadget/services/LoginHistory/jpa/entity/LoginHistoryEntity.java
@@ -9,60 +9,58 @@ import java.util.Date;
 @Entity
 @ExoEntity
 @Table(name = "LOGIN_HISTORY")
-@NamedQueries({
-        @NamedQuery(name = "loginHistory.getLastLogins",query = "SELECT l FROM LoginHistoryEntity l ORDER BY l.id DESC"),
-        @NamedQuery(name = "loginHistory.getLastLoggedUsers",query = "SELECT DISTINCT l.userId FROM LoginHistoryEntity l"),
-        @NamedQuery(name = "loginHistory.getLastLoginsOfUser",query = "SELECT l FROM LoginHistoryEntity l WHERE l.userId = :userId ORDER BY l.id DESC"),
-        @NamedQuery(name = "loginHistory.getBeforeLastLoginID",query = "SELECT MAX(l.id) FROM LoginHistoryEntity l WHERE l.userId = :userId AND l.id < :id"),
-        @NamedQuery(name = "loginHistory.getLastLoginsInDateRange",query = "SELECT l FROM LoginHistoryEntity l WHERE l.loginDate BETWEEN :from AND :to"),
-        @NamedQuery(name = "loginHistory.getLastLoginsOfUserInDateRange",query = "SELECT l FROM LoginHistoryEntity l WHERE l.userId = :userId AND l.loginDate BETWEEN :from AND :to"),
-        @NamedQuery(name = "loginHistory.getLoginsCountOfAllUserInDateRange",query = "SELECT COUNT (l) FROM LoginHistoryEntity l WHERE l.loginDate BETWEEN :from AND :to"),
-        @NamedQuery(name = "loginHistory.getLoginsCountOfUserInDateRange",query = "SELECT COUNT (l) FROM LoginHistoryEntity l WHERE l.userId = :userId AND l.loginDate BETWEEN :from AND :to"),
-        @NamedQuery(name = "loginHistory.getLastLoginsAfterDate",query = "SELECT l.userId FROM LoginHistoryEntity l WHERE l.loginDate >= :from ORDER BY l.id DESC"),
-        @NamedQuery(name = "loginHistory.getActiveUsersId",query = "SELECT DISTINCT l.userId FROM LoginHistoryEntity l where l.loginDate >= :from")
-})
+@NamedQueries({ @NamedQuery(name = "loginHistory.getLastLogins", query = "SELECT l FROM LoginHistoryEntity l ORDER BY l.id DESC"),
+    @NamedQuery(name = "loginHistory.getLastLoggedUsers", query = "SELECT DISTINCT l.userId FROM LoginHistoryEntity l"),
+    @NamedQuery(name = "loginHistory.getLastLoginsOfUser", query = "SELECT l FROM LoginHistoryEntity l WHERE l.userId = :userId ORDER BY l.id DESC"),
+    @NamedQuery(name = "loginHistory.getBeforeLastLoginID", query = "SELECT MAX(l.id) FROM LoginHistoryEntity l WHERE l.userId = :userId AND l.id < :id"),
+    @NamedQuery(name = "loginHistory.getLastLoginsInDateRange", query = "SELECT l FROM LoginHistoryEntity l WHERE l.loginDate BETWEEN :from AND :to"),
+    @NamedQuery(name = "loginHistory.getLastLoginsOfUserInDateRange", query = "SELECT l FROM LoginHistoryEntity l WHERE l.userId = :userId AND l.loginDate BETWEEN :from AND :to"),
+    @NamedQuery(name = "loginHistory.getLoginsCountInDateRange", query = "SELECT COUNT (l) FROM LoginHistoryEntity l WHERE l.loginDate BETWEEN :from AND :to"),
+    @NamedQuery(name = "loginHistory.getLoginsCountOfUserInDateRange", query = "SELECT COUNT (l) FROM LoginHistoryEntity l WHERE l.userId = :userId AND l.loginDate BETWEEN :from AND :to"),
+    @NamedQuery(name = "loginHistory.getLastLoginsAfterDate", query = "SELECT l.userId FROM LoginHistoryEntity l WHERE l.loginDate >= :from ORDER BY l.id DESC"),
+    @NamedQuery(name = "loginHistory.getActiveUsersId", query = "SELECT DISTINCT l.userId FROM LoginHistoryEntity l WHERE l.loginDate >= :from") })
 public class LoginHistoryEntity {
-    @Id
-    @GeneratedValue
-    @Column(name = "ID")
-    private long id;
+  @Id
+  @GeneratedValue
+  @Column(name = "ID")
+  private long   id;
 
-    @Column(name = "USER_ID")
-    private String userId;
+  @Column(name = "USER_ID")
+  private String userId;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "LOGIN_DATE")
-    private Date loginDate;
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(name = "LOGIN_DATE")
+  private Date   loginDate;
 
-    public LoginHistoryEntity() {
-    }
+  public LoginHistoryEntity() {
+  }
 
-    public LoginHistoryEntity(String userID) {
-        this.userId = userID;
-    }
+  public LoginHistoryEntity(String userID) {
+    this.userId = userID;
+  }
 
-    public LoginHistoryEntity(String userId, Date loginDate) {
-        this.userId = userId;
-        this.loginDate = loginDate;
-    }
+  public LoginHistoryEntity(String userId, Date loginDate) {
+    this.userId = userId;
+    this.loginDate = loginDate;
+  }
 
-    public long getID() {
-        return id;
-    }
+  public long getID() {
+    return id;
+  }
 
-    public String getUserID() {
-        return userId;
-    }
+  public String getUserID() {
+    return userId;
+  }
 
-    public void setUserID(String userID) {
-        this.userId = userID;
-    }
+  public void setUserID(String userID) {
+    this.userId = userID;
+  }
 
-    public Date getLoginDate() {
-        return loginDate;
-    }
+  public Date getLoginDate() {
+    return loginDate;
+  }
 
-    public void setLoginDate(Timestamp loginDate) {
-        this.loginDate = loginDate;
-    }
+  public void setLoginDate(Timestamp loginDate) {
+    this.loginDate = loginDate;
+  }
 }
