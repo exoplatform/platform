0e48f64e465f613ca08810939b5fcfc5517d6089
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2019-06-24 10:02:52 +0200

    PLF-8373 : use BrandingService service in upgrade plugin

--- component/common/src/main/java/org/exoplatform/platform/common/branding/BrandingUpgradePlugin.java
+++ component/common/src/main/java/org/exoplatform/platform/common/branding/BrandingUpgradePlugin.java
@@ -1,22 +1,18 @@
 package org.exoplatform.platform.common.branding;
 
-import org.exoplatform.commons.file.model.FileItem;
-import org.exoplatform.commons.file.services.FileService;
-import org.exoplatform.commons.persistence.impl.EntityManagerService;
+import javax.jcr.Node;
+import javax.jcr.Property;
+import javax.jcr.Session;
+
+import org.apache.commons.io.IOUtils;
+
 import org.exoplatform.commons.upgrade.UpgradeProductPlugin;
-import org.exoplatform.container.ExoContainerContext;
 import org.exoplatform.container.xml.InitParams;
 import org.exoplatform.services.jcr.RepositoryService;
 import org.exoplatform.services.jcr.core.ManageableRepository;
 import org.exoplatform.services.jcr.ext.common.SessionProvider;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
-
-import javax.jcr.Node;
-import javax.jcr.Property;
-import javax.jcr.Session;
-import java.io.InputStream;
-import java.util.Date;
 
 public class BrandingUpgradePlugin extends UpgradeProductPlugin {
   private static final Log    LOG                     = ExoLogger.getLogger(BrandingUpgradePlugin.class);
@@ -25,27 +21,20 @@ public class BrandingUpgradePlugin extends UpgradeProductPlugin {
 
   private static final String logo_name               = "logo.png";
 
-  private final String        WORKSPACE_COLLABORATION = "collaboration";
-
-  private static final String FILE_API_NAME_SPACE     = "CompanyBranding";
+  private static final String WORKSPACE_COLLABORATION = "collaboration";
 
   private RepositoryService   repositoryService;
 
-  private FileService         fileService;
-
-  EntityManagerService        entityManagerService;
+  private BrandingService brandingService;
 
   public BrandingUpgradePlugin(InitParams initParams,
                                RepositoryService repositoryService,
-                               EntityManagerService entityManagerService,
-
-                               FileService fileService)
+                               BrandingService brandingService)
 
   {
     super(initParams);
     this.repositoryService = repositoryService;
-    this.fileService = fileService;
-    this.entityManagerService = entityManagerService;
+    this.brandingService = brandingService;
   }
 
   @Override
@@ -53,54 +42,36 @@ public class BrandingUpgradePlugin extends UpgradeProductPlugin {
     SessionProvider sProvider = SessionProvider.createSystemProvider();
 
     if (!hasDataToMigrate()) {
-      LOG.info("== No Branding data to migrate from JCR to File Storage");
+      LOG.info("== No Branding data to migrate from JCR");
     } else {
-      LOG.info("== Start migration of Branding data from JCR to File Storage");
+      LOG.info("== Start migration of Branding data from JCR");
       try {
         Session session = getSession(sProvider);
         Node rootNode = session.getRootNode();
-        String logoCreator = "";
-        long fileSize = 0;
         if (rootNode.hasNode(path)) {
           Node logosNode = rootNode.getNode(path);
           if (logosNode.hasNode(logo_name)) {
             Node logoNode = logosNode.getNode(logo_name);
-            // && logoNode.isNodeType("nt:file")
             if (logoNode.hasNode("jcr:content")) {
               Node logoContent = logoNode.getNode("jcr:content");
               Property data = logoContent.getProperty("jcr:data");
-              if (logoNode.hasProperty("exo:lastModifier")) {
-                logoCreator = logoNode.getProperty("exo:lastModifier").getString();
-              }
-              // should use branding service instead!
-              entityManagerService.startRequest(ExoContainerContext.getCurrentContainer());
-              InputStream inputStream = data.getStream();
-              fileSize = data.getLength();
-              FileItem fileItem = new FileItem(null,
-                                               logo_name,
-                                               "image/png",
-                                               FILE_API_NAME_SPACE,
-                                               fileSize,
-                                               new Date(),
-                                               logoCreator,
-                                               false,
-                                               inputStream);
-              fileItem = fileService.writeFile(fileItem);
 
+              Logo logo = new Logo();
+              byte[] logoData = IOUtils.toByteArray(data.getStream());
+              logo.setData(logoData);
+              logo.setSize(logoData.length);
+              brandingService.uploadLogo(logo);
             }
             logoNode.remove();
             session.save();
           }
 
+          removeLogoHome();
         }
       } catch (Exception e) {
-        throw new RuntimeException("== Branding Data migration - Error when migrate logo to File Storage", e);
+        throw new RuntimeException("== Branding Data migration - Error when migrating logo from JCR", e);
       } finally {
-        entityManagerService.endRequest(ExoContainerContext.getCurrentContainer());
         sProvider.close();
-      }
-      if (!removeLogoHome()) {
-        throw new RuntimeException("== Branding Data migration - Error when remove logo from JCR");
       }
 
       LOG.info("==    Branding Data migration - Branding Data migration successfully !");
@@ -129,16 +100,15 @@ public class BrandingUpgradePlugin extends UpgradeProductPlugin {
         }
       }
     } catch (Exception e) {
-      LOG.error("Error while checking the existence of branding data", e);
+      LOG.error("Error while checking the existence of branding data in JCR", e);
     } finally {
       sProvider.close();
     }
     return hasDataToMigrate;
   }
 
-  private boolean removeLogoHome() {
+  private void removeLogoHome() throws Exception {
     SessionProvider sProvider = SessionProvider.createSystemProvider();
-    boolean status = true;
     try {
       Session session = getSession(sProvider);
       Node rootNode = session.getRootNode();
@@ -146,12 +116,8 @@ public class BrandingUpgradePlugin extends UpgradeProductPlugin {
       if (rootNode.hasNode(path)) {
         rootNode.getNode(path).remove();
       }
-    } catch (Exception e) {
-      LOG.error("Error while removing the logo home node", e);
-      status = false;
     } finally {
       sProvider.close();
-      return status;
     }
 
   }
