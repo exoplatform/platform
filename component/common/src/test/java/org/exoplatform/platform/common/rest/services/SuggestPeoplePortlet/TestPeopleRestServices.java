2c4d4d3525096490a5561269db25949ebc7d2c72
Ali HAMDI <ahamdi@exoplatform.com>
2019-11-08 11:51:13 +0100

    PLF-8554 : Suggestions should be continuously filled from last registered users and shared connections (#411)
    * PLF-8554 : Suggestions should be continuously filled from last registered users and shared connections

--- component/common/src/test/java/org/exoplatform/platform/common/rest/services/SuggestPeoplePortlet/TestPeopleRestServices.java
+++ component/common/src/test/java/org/exoplatform/platform/common/rest/services/SuggestPeoplePortlet/TestPeopleRestServices.java
@@ -47,6 +47,10 @@ public class TestPeopleRestServices extends BaseRestServicesTestCase {
         idFoo.setId("foo");
         Identity idBar = new Identity(OrganizationIdentityProvider.NAME, "bar");
         idBar.setId("bar");
+        Identity idBaz = new Identity(OrganizationIdentityProvider.NAME, "baz");
+        idBaz.setId("baz");
+        Identity idQux = new Identity(OrganizationIdentityProvider.NAME, "qux");
+        idBaz.setId("qux");
         envctx.put(SecurityContext.class, new MockSecurityContext(idFoo.getRemoteId()));
 
         Map<String, Object> imResults = new HashMap<String, Object>();
@@ -84,7 +88,7 @@ public class TestPeopleRestServices extends BaseRestServicesTestCase {
         assertTrue(json.has("items"));
         assertTrue(json.getJSONArray("items").length() == 0);
 
-        // The only suggestion is demo
+        // The only suggestion is bar
         rmResults.put("getConnections", new MockListAccess<Identity>(new Identity[]{idBar}));
         rmResults.put("getSuggestions", Collections.singletonMap(idRoot, 1));
         rmResults.remove("get");
@@ -94,8 +98,24 @@ public class TestPeopleRestServices extends BaseRestServicesTestCase {
         assertEquals("application/json", resp.getContentType().toString());
         json = new JSONObject(resp.getEntity().toString());
         assertTrue(json.has("items"));
-        assertTrue(json.getJSONArray("items").length() == 1);
+        assertEquals(1, json.getJSONArray("items").length());
         assertEquals(json.getJSONArray("items").getJSONObject(0).getString("username"), idBar.getRemoteId());
+
+        // There is one suggestion and one connection to a contact
+        // bar is the suggestion since he is a connection
+        // baz is a new suggestion as he is a new registered user
+        Map<Identity, Integer> suggestions = new HashMap<>();
+        suggestions.put(idRoot, 1);
+        suggestions.put(idBar, 2);
+        imResults.put("getLastIdentities", Arrays.asList(idRoot, idFoo, idBaz));
+        rmResults.put("getConnections", new MockListAccess<Identity>(new Identity[]{idQux}));
+        rmResults.put("getSuggestions", suggestions);
+        resp = launcher.service("GET", path, "", null, null, envctx);
+        assertEquals(200, resp.getStatus());
+        assertEquals("application/json", resp.getContentType().toString());
+        json = new JSONObject(resp.getEntity().toString());
+        assertTrue(json.has("items"));
+        assertEquals(2, json.getJSONArray("items").length());
 
         getContainer().unregisterComponent("UserACL");
         getContainer().unregisterComponent("RelationshipManager");
